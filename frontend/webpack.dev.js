5e63e375e0e0d38062cd9605ecde093847931a32
hoyyChoi <hoyeon8165@gmail.com>
2025-07-09 15:25:25 +0900

    chore: webpack 초기 세팅

---
+++ frontend/webpack.dev.js
@@ -0,0 +1,25 @@
+import { merge } from "webpack-merge";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  // CSS 처리 최적화 - 개발환경에서도 별도 파일로 추출
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  devServer: {
+    static: "./dist",
+    hot: true,
+    historyApiFallback: true, // SPA 라우팅
+    port: 3000,
+    open: true,
+  },
+});
---
+++ frontend/webpack.prod.js
@@ -0,0 +1,42 @@
+import { merge } from "webpack-merge";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        // CSS는 별도 파일로 뽑아야 캐싱에 유리
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  //   optimization: {
+  //     minimizer: [
+  //       `...`, // 기본 minimizer 유지
+  //       new CssMinimizerPlugin(), // CSS 압축 최적화
+  //     ],
+  //     splitChunks: {
+  //       chunks: "all",
+  //       cacheGroups: {
+  //         // CSS를 별도 청크로 분리
+  //         styles: {
+  //           name: "styles",
+  //           test: /\.css$/,
+  //           chunks: "all",
+  //           enforce: true,
+  //         },
+  //       },
+  //     },
+  //     runtimeChunk: "single",
+  //   },
+});
