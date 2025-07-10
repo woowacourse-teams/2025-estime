40a52f512475ef8e12ee58e57452a77fff511822
thgml05 <aisohii@naver.com>
2025-07-10 16:43:51 +0900

    chore: esLint 및 prettier 설정

---
+++ frontend/eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        document: 'readonly',
+      },
+    },
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    plugins: { react, 'react-hooks': reactHooks },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+
+  prettier,
+];
