0782dfb0d7e2fb5cd75f5f7c6f15b42ecfb37823
Yeonhu Lee <185307804+yeonnhuu@users.noreply.github.com>
2025-07-10 16:28:30 +0900

    TimeSlot 조회 엔드포인트 추가 (#12)
    * feat: TimeSlot 조회 API 엔드포인트 추가
    
    - TimeSlot 필드 추가
    - TimeSlot 조회 기능 수정
    
    * feat: Room 엔티티 추가
    
    * feat: TimeSlot API 엔드포인트 수정

--- backend/src/main/java/com/bether/bether/timeslot/infrastructure/TimeSlotJpaRepository.java
+++ backend/src/main/java/com/bether/bether/timeslot/infrastructure/TimeSlotJpaRepository.java
@@ -3,6 +3,10 @@ package com.bether.bether.timeslot.infrastructure;
 import com.bether.bether.timeslot.domain.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface TimeSlotJpaRepository extends JpaRepository<TimeSlot, Long> {
 
+    List<TimeSlot> findAllByRoomSession(UUID roomSession);
 }
--- backend/src/test/java/com/bether/bether/timeslot/application/service/TimeSlotServiceTest.java
+++ backend/src/test/java/com/bether/bether/timeslot/application/service/TimeSlotServiceTest.java
@@ -1,5 +1,6 @@
 package com.bether.bether.timeslot.application.service;
 
+import com.bether.bether.timeslot.application.dto.input.TimeSlotInput;
 import com.bether.bether.timeslot.domain.TimeSlot;
 import com.bether.bether.timeslot.domain.TimeSlotRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,16 +25,47 @@ class TimeSlotServiceTest {
     @Autowired
     private TimeSlotRepository timeSlotRepository;
 
-    @DisplayName("식별자로 타임슬롯을 가져올 수 있다")
+    @DisplayName("룸 세션으로 타임슬롯을 가져올 수 있다.")
     @Test
-    void getById() {
+    void getAllByRoomSession() {
         // given
-        final TimeSlot saved = timeSlotRepository.save(TimeSlot.withoutId(LocalDateTime.now()));
+        final UUID roomSession = UUID.randomUUID();
+        final TimeSlot saved1 = timeSlotRepository.save(TimeSlot.withoutId(roomSession, "user", LocalDateTime.now()));
+        final TimeSlot saved2 = timeSlotRepository.save(TimeSlot.withoutId(roomSession, "user", LocalDateTime.now()));
 
         // when
-        final TimeSlot found = timeSlotService.getById(saved.getId());
+        final List<TimeSlot> found = timeSlotService.getAllByRoomSession(roomSession);
 
         // then
-        assertThat(saved).isEqualTo(found);
+        assertThat(found)
+                .containsExactlyElementsOf(List.of(saved1, saved2));
+    }
+
+    @DisplayName("존재하지 않는 룸 세션 조회시 빈 리스트를 반환한다.")
+    @Test
+    void getAllByNonExistingRoomSession() {
+        // given
+        final UUID roomSession = UUID.randomUUID();
+
+        // when
+        final List<TimeSlot> found = timeSlotService.getAllByRoomSession(roomSession);
+
+        // then
+        assertThat(found)
+                .isEmpty();
+    }
+
+    @DisplayName("다수의 타임슬롯을 저장할 수 있다.")
+    @Test
+    void saveAll() {
+        // given
+        final TimeSlotInput input = new TimeSlotInput(UUID.randomUUID(), "user", List.of(LocalDateTime.now(), LocalDateTime.now()));
+
+        // when
+        final List<TimeSlot> saved = timeSlotService.saveAll(input);
+
+        // then
+        assertThat(saved)
+                .hasSize(2);
     }
 }
