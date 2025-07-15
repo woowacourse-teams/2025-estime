9f65370e0439b2cda9b6f3b0eb4af12629fc4324
deokbin <deokbin@naver.com>
2025-07-15 18:11:18 +0900

    design: 디자인 시스템 설정

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,70 @@
+export const theme = {
+  colors: {
+    light: {
+      primary: '#8052E1',
+      plum30: '#E6DAF3',
+      plum40: '#DBD1E6',
+      plum50: '#A87EFF',
+      red40: '#EF4444',
+      white: '#FFFFFF',
+      black: '#000000',
+      gray10: '#F4F4F4',
+      gray20: '#C6C6C6',
+      gray30: 'AEAEB2',
+      gray40: '#8E8E93',
+      gray50: '#636366',
+      gray60: '#48484A',
+      gray70: '#363639',
+      gray80: '#2C2C2E',
+      gray90: '#1C1C1E',
+    },
+    dark: {
+      primary: '#9E75EF',
+      plum30: '#A793BD',
+      plum40: '#5D4E84',
+      plum50: '#684BA5',
+      red40: '#7F1E1D',
+      white: '#FFFFFF',
+      black: '#000000',
+      gray10: '#F4F4F4',
+      gray20: '#C6C6C6',
+      gray30: 'AEAEB2',
+      gray40: '#8E8E93',
+      gray50: '#636366',
+      gray60: '#48484A',
+      gray70: '#363639',
+      gray80: '#2C2C2E',
+      gray90: '#1C1C1E',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '2.25rem',
+      fontWeight: 'medium',
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 'medium',
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+    },
+    h4: {
+      fontSize: '1rem',
+      fontWeight: 'regular',
+    },
+    body: {
+      fontSize: '0.875rem',
+      fontWeight: 'regular',
+    },
+    button: {
+      fontSize: '0.875rem',
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: '0.75rem',
+      fontWeight: 'regular',
+    },
+  },
+};
