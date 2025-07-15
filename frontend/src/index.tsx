c6fb978c1c98c219afe294c1450dc48052633dc2
Deokbin Kim <162319857+Db0111@users.noreply.github.com>
2025-07-15 18:18:24 +0900

    디자인 시스템 적용 (#27)
    * design: Pretendard 폰트 설정
    
    * chore: emotion/react 의존성 추가
    
    * design: 디자인 시스템 설정
    
    * chore: Theme 타입 설정
    
    * feat: ThemeProvider 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@/styles/reset.css';
+import '@/styles/index.css';
+import { theme } from './styles/theme';
+import { ThemeProvider } from '@emotion/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
