dfcc40922feaf745cd870c0416d51be066c08a9b
deokbin <deokbin@naver.com>
2025-07-16 18:31:42 +0900

    design: Button 컴포넌트 스타일 추가

---
+++ frontend/src/components/Button/Button.styled.ts
@@ -0,0 +1,24 @@
+import { LIGHT_THEME } from '@/styles/theme';
+import styled from '@emotion/styled';
+
+export const Container = styled.button<{
+  color: keyof typeof LIGHT_THEME.colors;
+  size: 'small' | 'medium' | 'large';
+}>`
+  width: ${({ size }) => {
+    switch (size) {
+      case 'small':
+        return '8rem';
+      case 'medium':
+        return '10rem';
+      case 'large':
+        return '12rem';
+      default:
+        return '100%';
+    }
+  }};
+  height: 50px;
+  border-radius: 8px;
+  background-color: ${({ theme, color }) => theme.colors[color]};
+  border: none;
+`;
