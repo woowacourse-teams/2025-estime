0dc31f190220003ac60424a2b2c6e5bdac54da5c
thgml05 <aisohii@naver.com>
2025-07-17 02:24:37 +0900

    design: Radio 컴포넌트 스타일 적용

---
+++ frontend/src/components/Radio/Radio.styled.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Label = styled.label`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  gap: 0.75rem;
+`;
+
+export const Input = styled.input`
+  width: 1.25rem;
+  height: 1.25rem;
+  accent-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
