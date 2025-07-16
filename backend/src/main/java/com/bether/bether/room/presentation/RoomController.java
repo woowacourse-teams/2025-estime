8b2f2537de0ddb66ddd302164641fcb6cc018166
AppleMint98 <applemint9821@gmail.com>
2025-07-16 14:40:43 +0900

    feat: swagger API 명세 추가

--- backend/src/main/java/com/bether/bether/room/presentation/RoomController.java
+++ backend/src/main/java/com/bether/bether/room/presentation/RoomController.java
@@ -1,6 +1,6 @@
 package com.bether.bether.room.presentation;
 
-import com.bether.bether.common.ApiResponse;
+import com.bether.bether.common.CustomApiResponse;
 import com.bether.bether.room.application.dto.RoomOutput;
 import com.bether.bether.room.application.service.RoomService;
 import com.bether.bether.room.presentation.dto.request.RoomCreateRequest;
@@ -13,54 +13,50 @@ import com.bether.bether.timeslot.domain.TimeSlot;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/rooms")
-public class RoomController {
+public class RoomController implements RoomControllerSpecification {
 
     private final RoomService roomService;
 
-    @PostMapping
-    public ApiResponse<RoomCreateResponse> create(@RequestBody final RoomCreateRequest request) {
+    @Override
+    public CustomApiResponse<RoomCreateResponse> create(@RequestBody final RoomCreateRequest request) {
         final RoomOutput saved = roomService.saveRoom(request.toInput());
-        return ApiResponse.ok(RoomCreateResponse.from(saved));
+        return CustomApiResponse.created(RoomCreateResponse.from(saved));
     }
 
-    @GetMapping("/{session}/time-slots/statistic")
-    public ApiResponse<TimeSlotStatisticResponse> getStatistic(@PathVariable("session") final UUID session) {
+    @Override
+    public CustomApiResponse<TimeSlotStatisticResponse> getStatistic(@PathVariable("session") final UUID session) {
         final TimeSlotStatisticOutput output = roomService.calculateStatistic(session);
-        return ApiResponse.ok(TimeSlotStatisticResponse.from(output));
+        return CustomApiResponse.ok(TimeSlotStatisticResponse.from(output));
     }
 
-    @GetMapping("/{session}/time-slots/recommendation")
-    public ApiResponse<TimeSlotStatisticResponse> getRank(@PathVariable("session") final UUID session) {
+    @Override
+    public CustomApiResponse<TimeSlotStatisticResponse> getRank(@PathVariable("session") final UUID session) {
         final TimeSlotStatisticOutput output = roomService.calculateStatistic(session);
-        return ApiResponse.ok(TimeSlotStatisticResponse.from(output));
+        return CustomApiResponse.ok(TimeSlotStatisticResponse.from(output));
     }
 
-    @PostMapping("/{session}/time-slots")
-    public ApiResponse<Void> create(
+    @Override
+    public CustomApiResponse<Void> createTimeSlots(
             @PathVariable("session") final UUID session,
             @RequestBody final TimeSlotCreateRequest request
     ) {
         roomService.saveTimeSlots(request.toInput(session));
-        return ApiResponse.ok();
+        return CustomApiResponse.ok();
     }
 
-    @GetMapping("/{session}/time-slots/user")
-    public ApiResponse<TotalTimeSlotResponse> getByUserName(
+    @Override
+    public CustomApiResponse<TotalTimeSlotResponse> getByUserName(
             @PathVariable("session") final UUID session,
             @RequestParam("name") final String userName
     ) {
         final List<TimeSlot> timeSlots = roomService.getTimeSlotsBySessionAndUserName(session, userName);
-        return ApiResponse.ok(TotalTimeSlotResponse.from(timeSlots));
+        return CustomApiResponse.ok(TotalTimeSlotResponse.from(timeSlots));
     }
 }
