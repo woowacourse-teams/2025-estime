c21c7314938404cbdd778756e4b377fc0cbb4ff4
deokbin <deokbin@naver.com>
2025-07-17 00:14:21 +0900

    design: input 컴포넌트 스타일 추가

---
+++ frontend/src/components/Input/Input.styled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.input<{ isError?: boolean }>`
+  width: 100%;
+  height: 2rem;
+  border-radius: var(--radius-4);
+  border: 1px solid ${({ theme, isError }) => (isError ? theme.colors.red40 : theme.colors.gray20)};
+  padding: var(--padding-3);
+  background-color: ${({ theme }) => theme.colors.gray10};
+
+  &:focus {
+    outline: none;
+    border: 2px solid ${({ theme }) => theme.colors.plum40};
+  }
+`;
