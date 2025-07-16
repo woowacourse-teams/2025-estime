86994f9fe99ae80ec1f6679fb04da78ff4223f1c
spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>
2025-07-16 17:03:17 +0900

    feat: 다크 및 라이트 테마 구현 (#51)
    다크 및 라이트 테마 구현

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@/styles/index.css';
-import { theme } from './styles/theme';
-import { ThemeProvider } from '@emotion/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
