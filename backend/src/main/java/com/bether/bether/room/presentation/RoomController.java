b126c7a5e75e724f5af6945223076b536d524f85
AppleMint98 <applemint9821@gmail.com>
2025-07-16 13:15:17 +0900

    feat: API 경로에 버전 추가

--- backend/src/main/java/com/bether/bether/room/presentation/RoomController.java
+++ backend/src/main/java/com/bether/bether/room/presentation/RoomController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/rooms")
+@RequestMapping("/api/v1/rooms")
 public class RoomController {
 
     private final RoomService roomService;
