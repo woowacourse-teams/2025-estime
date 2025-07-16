be32f359f4105710ae3cee7264102637f79a651b
hoyyChoi <hoyeon8165@gmail.com>
2025-07-16 18:07:58 +0900

    feat: Text 컴포넌트 제작

---
+++ frontend/src/components/Text/index.tsx
@@ -0,0 +1,18 @@
+import { TypographyKey, ColorsKey } from '@/styles/theme';
+import * as S from './Text.styled';
+
+export interface TextProps {
+  variant?: TypographyKey;
+  color?: ColorsKey;
+  children: React.ReactNode;
+}
+
+const Text = ({ variant = 'body', color = 'gray90', children }: TextProps) => {
+  return (
+    <S.Container variant={variant} color={color}>
+      {children}
+    </S.Container>
+  );
+};
+
+export default Text;
