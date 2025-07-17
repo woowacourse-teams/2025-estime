08f0878eceec5d320c8086e5bd55a9e174226ead
spoyodevelop <kjy9505@gmail.com>
2025-07-17 15:32:09 +0900

    feat: 주간 요일 상수 추가
    - 캘린더 상수 파일에 주간 요일을 나타내는 weekdays 배열을 추가하여 날짜 관련 기능을 개선했습니다.

--- frontend/src/constants/calender.ts
+++ frontend/src/constants/calender.ts
@@ -1,8 +1,9 @@
 const row = 6;
 const column = 7;
+const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
 const ERROR_MESSAGE = {
   INVALID_DATE: '유효한 날짜 객체를 제공해야 합니다.',
   INVALID_MONTH: '월은 0부터 11 사이의 값이어야 합니다.',
   INVALID_YEAR: '연도는 0 이상의 값이어야 합니다.',
 } as const;
-export { row, column, ERROR_MESSAGE };
+export { row, column, ERROR_MESSAGE, weekdays };
