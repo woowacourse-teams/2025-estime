9a92ef5bff949871060f51d313e74cd30b07893e
deokbin <deokbin@naver.com>
2025-07-17 20:25:44 +0900

    chore: public 정적 asset 설정

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,22 +1,21 @@
-import { merge } from "webpack-merge";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import common from "./webpack.common.js";
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
 export default merge(common, {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   // CSS 처리 최적화 - 개발환경에서도 별도 파일로 추출
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
 
   devServer: {
-    static: "./dist",
+    static: ['./dist', './public'],
     hot: true,
     historyApiFallback: true, // SPA 라우팅
     port: 3000,
