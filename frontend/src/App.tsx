4ecd234ca5a4d26e3ee99d952615629b5a904ab3
hoyyChoi <hoyeon8165@gmail.com>
2025-07-17 23:45:21 +0900

    feat: App 컴포넌트 라우팅 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { DARK_THEME, LIGHT_THEME } from './styles/theme';
 import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import CreateEventPage from './pages/CreateEventPage';
+import Layout from './components/Layout';
 
 const App = () => {
   const [isDark, setIsDark] = useState(false);
@@ -9,8 +12,13 @@ const App = () => {
   };
   return (
     <ThemeProvider theme={isDark ? DARK_THEME : LIGHT_THEME}>
-      <button onClick={toggleTheme} />
-      <div>hello</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout isDark={isDark} toggleTheme={toggleTheme} />}>
+            <Route index element={<CreateEventPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
