fcd44302223bbb85621f7e7a5dbc0c28a5b433f6
hoyyChoi <hoyeon8165@gmail.com>
2025-07-17 23:33:36 +0900

    feat: ThemeButton 컴포넌트 제작

---
+++ frontend/src/components/ThemeButton/ThemeButton.styled.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: var(--padding-2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
