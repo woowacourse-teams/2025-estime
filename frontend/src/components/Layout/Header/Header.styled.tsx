8b247b565c94d4e0641b2dea3b76e5f990548c1b
hoyyChoi <hoyeon8165@gmail.com>
2025-07-18 01:10:50 +0900

    fix: Header 컴포넌트 내부 content width 수정

--- frontend/src/components/Layout/Header/Header.styled.tsx
+++ frontend/src/components/Layout/Header/Header.styled.tsx
@@ -4,6 +4,12 @@ export const Container = styled.div`
   width: 100%;
   height: 4rem;
   background-color: ${({ theme }) => theme.colors.gray10};
+`;
+
+export const Content = styled.div`
+  max-width: 1280px;
+  height: inherit;
+  margin: 0 auto;
   display: flex;
   align-items: center;
   justify-content: space-between;
--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -5,10 +5,12 @@ import ThemeButton from '@/components/ThemeButton';
 const Header = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: () => void }) => {
   return (
     <S.Container>
-      <Text variant="h2" color="primary">
-        아인슈타인
-      </Text>
-      <ThemeButton isDark={isDark} onToggle={toggleTheme} />
+      <S.Content>
+        <Text variant="h2" color="primary">
+          아인슈타인
+        </Text>
+        <ThemeButton isDark={isDark} onToggle={toggleTheme} />
+      </S.Content>
     </S.Container>
   );
 };
