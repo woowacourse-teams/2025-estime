abcda6be4e6d3d245b9d54cbc220cce84a425c22
thgml05 <aisohii@naver.com>
2025-07-18 15:09:15 +0900

    fix: Accordion 컴포넌트 오픈 이벤트 핸들러 위치 헤더로 이동

--- frontend/src/components/Accordian/index.tsx
+++ frontend/src/components/Accordian/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import * as S from './Accodion.styled';
+import * as S from './Accordian.styled';
 import Text from '../Text';
 
-interface AccodianProps {
+interface AccordionProps {
   children: React.ReactNode;
   title: string;
   isOpen: boolean;
   onToggle: () => void;
 }
 
-const Accodion = ({ title, children, isOpen, onToggle }: AccodianProps) => {
+const Accordion = ({ title, children, isOpen, onToggle }: AccordionProps) => {
   return (
     <S.Container>
-      <S.Header>
+      <S.Header onClick={onToggle}>
         <Text variant={'h3'}>{title}</Text>
-        <S.Icon src="down-arrow.svg" alt="down-arrow-icon" isOpen={isOpen} onClick={onToggle} />
+        <S.Icon src="down-arrow.svg" alt="down-arrow-icon" isOpen={isOpen} />
       </S.Header>
       <S.Content isOpen={isOpen}>{children}</S.Content>
     </S.Container>
   );
 };
 
-export default Accodion;
+export default Accordion;
