7112bb831f4e05a4cf9df4bd3104d2fd86bad507
hoyyChoi <hoyeon8165@gmail.com>
2025-07-18 15:16:32 +0900

    feat: theme 테마 설정 변경

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -4,14 +4,18 @@ export type TypographyKey = keyof typeof LIGHT_THEME.typography;
 
 export const LIGHT_THEME = {
   colors: {
+    background: '#FFFFFF',
+    text: '#000000',
+
     primary: '#8052E1',
     secondary: '#E2A86A',
     plum30: '#E6DAF3',
     plum40: '#DBD1E6',
     plum50: '#A87EFF',
+    orange30: '#FFC64C',
+    orange40: '#FFAE00',
     red40: '#EF4444',
-    white: '#FFFFFF',
-    black: '#000000',
+
     gray10: '#F4F4F4',
     gray20: '#C6C6C6',
     gray30: '#AEAEB2',
@@ -55,23 +59,27 @@ export const LIGHT_THEME = {
 };
 export const DARK_THEME = {
   colors: {
+    background: '#000000',
+    text: '#FFFFFF',
+
     primary: '#9E75EF',
     secondary: '#F9C287',
     plum30: '#A793BD',
     plum40: '#5D4E84',
     plum50: '#684BA5',
+    orange30: '#FFC64C',
+    orange40: '#FFAE00',
     red40: '#7F1E1D',
-    white: '#FFFFFF',
-    black: '#000000',
-    gray10: '#F4F4F4',
-    gray20: '#C6C6C6',
-    gray30: '#AEAEB2',
-    gray40: '#8E8E93',
+
+    gray10: '#1C1C1E',
+    gray20: '#2C2C2E',
+    gray30: '#363639',
+    gray40: '#48484A',
     gray50: '#636366',
-    gray60: '#48484A',
-    gray70: '#363639',
-    gray80: '#2C2C2E',
-    gray90: '#1C1C1E',
+    gray60: '#8E8E93',
+    gray70: '#AEAEB2',
+    gray80: '#C6C6C6',
+    gray90: '#F4F4F4',
   },
   typography: {
     h1: {
