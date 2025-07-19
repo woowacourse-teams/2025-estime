0c342d1e337a29e0c9581be69824e3199312446a
thgml05 <aisohii@naver.com>
2025-07-19 21:16:53 +0900

    feat: Globe(지구본 아이콘) 컴포넌트 추가

---
+++ frontend/src/icons/Globe.tsx
@@ -0,0 +1,44 @@
+const Globe = ({
+  color = '#fff',
+  ...props
+}: {
+  color?: string;
+  props?: React.SVGProps<SVGSVGElement>;
+}) => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g xmlns="http://www.w3.org/2000/svg" clipPath="url(#clip0_395_101)">
+        <path
+          d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z"
+          stroke={color}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M1.5 9H16.5"
+          stroke={color}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9 1.5C10.876 3.55376 11.9421 6.21903 12 9C11.9421 11.781 10.876 14.4462 9 16.5C7.12404 14.4462 6.05794 11.781 6 9C6.05794 6.21903 7.12404 3.55376 9 1.5Z"
+          stroke={color}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default Globe;
