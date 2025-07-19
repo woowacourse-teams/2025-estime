5767f3c285cc932198508f1f4421c4847c4157a8
thgml05 <aisohii@naver.com>
2025-07-19 17:13:17 +0900

    design: DatePicker 컴포넌트 패딩 TimePicker 컴포넌트와 동일하게 수정

--- frontend/src/components/DatePicker/DatePicker.styled.ts
+++ frontend/src/components/DatePicker/DatePicker.styled.ts
@@ -5,7 +5,8 @@ export const Container = styled.input<{ isError: boolean }>`
   height: 2rem;
   border-radius: var(--radius-4);
   border: 1px solid ${({ theme, isError }) => (isError ? theme.colors.red40 : theme.colors.gray20)};
-  padding: var(--padding-3);
+  padding: var(--padding-4) var(--padding-6);
+
   background-color: ${({ theme }) => theme.colors.gray10};
   color: ${({ theme }) => theme.colors.gray50};
   outline: none;
