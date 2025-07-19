3c2c6414b74464950e2e260de0ef9fb9fbfcb456
jhan0121 <jhan0121@gmail.com>
2025-07-20 00:59:29 +0900

    test: 사용자가 입력한 모임 가능 시간 수정 기능 테스트 추가
    - 핵심 기능 동작 검증 추가
    - 멱등성 보장 검증 추가

--- backend/src/test/java/com/bether/bether/timeslot/application/service/TimeSlotServiceTest.java
+++ backend/src/test/java/com/bether/bether/timeslot/application/service/TimeSlotServiceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.groups.Tuple.tuple;
 import com.bether.bether.room.domain.Room;
 import com.bether.bether.room.domain.RoomRepository;
 import com.bether.bether.timeslot.application.dto.input.TimeSlotInput;
+import com.bether.bether.timeslot.application.dto.input.TimeSlotUpdateInput;
 import com.bether.bether.timeslot.application.dto.output.TimeSlotStatisticOutput;
 import com.bether.bether.timeslot.domain.TimeSlot;
 import com.bether.bether.timeslot.domain.TimeSlotRepository;
@@ -14,8 +15,13 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +38,32 @@ class TimeSlotServiceTest {
 
     @Autowired
     private RoomRepository roomRepository;
+
+    private static Stream<Arguments> provideUpdateTimeSlotsArguments() {
+        return Stream.of(
+                Arguments.of(
+                        "1번 케이스: 기존 데이터를 새 데이터로 교체",
+                        List.of(LocalDateTime.of(2026, 1, 1, 0, 0)),
+                        List.of(LocalDateTime.of(2026, 1, 2, 0, 0))
+                ),
+                Arguments.of(
+                        "2번 케이스: 기존 데이터 전체 삭제",
+                        List.of(LocalDateTime.of(2026, 1, 1, 0, 0)),
+                        List.of()
+                ),
+                Arguments.of(
+                        "3번 케이스: 데이터 부분 업데이트 (유지, 추가, 삭제 혼합)",
+                        List.of(
+                                LocalDateTime.of(2026, 1, 1, 0, 0),
+                                LocalDateTime.of(2026, 1, 2, 0, 0)
+                        ),
+                        List.of(
+                                LocalDateTime.of(2026, 1, 2, 0, 0),
+                                LocalDateTime.of(2026, 1, 3, 0, 0)
+                        )
+                )
+        );
+    }
 
     @DisplayName("룸 아이디로 타임슬롯을 가져올 수 있다.")
     @Test
@@ -170,5 +202,87 @@ class TimeSlotServiceTest {
                         tuple(dateTime1, List.of("user", "user2")),
                         tuple(dateTime2, List.of("user"))
                 );
+    }
+
+    @DisplayName("특정 사용자가 저장한 타임슬롯을 제시한 날짜로 변경할 수 있다.")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource(value = "provideUpdateTimeSlotsArguments")
+    void updateTimeSlots(final String testName, final List<LocalDateTime> existed, final List<LocalDateTime> updated) {
+        // given
+        final Room room = roomRepository.save(
+                Room.withoutId(
+                        "title",
+                        List.of(LocalDate.now()),
+                        LocalTime.of(7, 0),
+                        LocalTime.of(20, 0)
+                ));
+
+        final String userName = "user";
+        final List<TimeSlot> existedTimeSlot = existed.stream()
+                .map(dateTime -> TimeSlot.withoutId(room.getId(), "existed", dateTime))
+                .toList();
+        timeSlotRepository.saveAll(existedTimeSlot);
+
+        final TimeSlotUpdateInput input = new TimeSlotUpdateInput(UUID.randomUUID(), userName, updated);
+
+        // when
+        timeSlotService.updateTimeSlots(room.getId(), input);
+
+        // then
+        final List<TimeSlot> saved = timeSlotRepository.findAllByRoomIdAndUserName(room.getId(), userName);
+
+        assertThat(saved)
+                .hasSize(updated.size())
+                .extracting("startAt")
+                .containsExactlyInAnyOrderElementsOf(updated);
+    }
+
+    @Test
+    @DisplayName("타임슬롯 변경은 멱등성을 보장한다.")
+    void updateTimeSlotsIsIdempotent() {
+        // given
+        final Room room = roomRepository.save(
+                Room.withoutId(
+                        "title",
+                        List.of(LocalDate.now()),
+                        LocalTime.of(7, 0),
+                        LocalTime.of(20, 0)
+                ));
+        final String userName = "user";
+
+        timeSlotRepository.save(TimeSlot.withoutId(room.getId(), userName, LocalDateTime.of(2026, 1, 1, 0, 0)));
+
+        final List<LocalDateTime> updatedSlots = List.of(
+                LocalDateTime.of(2026, 1, 2, 10, 0),
+                LocalDateTime.of(2026, 1, 2, 11, 0)
+        );
+        final TimeSlotUpdateInput input = new TimeSlotUpdateInput(UUID.randomUUID(), userName, updatedSlots);
+
+        // when
+        timeSlotService.updateTimeSlots(room.getId(), input);
+        final List<TimeSlot> resultAfterFirstCall = timeSlotRepository.findAllByRoomIdAndUserName(room.getId(),
+                userName);
+
+        timeSlotService.updateTimeSlots(room.getId(), input);
+        final List<TimeSlot> resultAfterSecondCall = timeSlotRepository.findAllByRoomIdAndUserName(room.getId(),
+                userName);
+
+        // then
+        final SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThat(resultAfterFirstCall)
+                .as("첫 번째 호출 후, 타임슬롯 개수가 기대값과 일치하는지 검증")
+                .hasSize(updatedSlots.size());
+
+        softAssertions.assertThat(resultAfterFirstCall)
+                .extracting("startAt")
+                .as("첫 번째 호출 후, 타임슬롯 내용이 기대값과 일치하는지 검증")
+                .containsExactlyInAnyOrderElementsOf(updatedSlots);
+
+        softAssertions.assertThat(resultAfterSecondCall)
+                .as("두 번째 호출 결과가 첫 번째 호출 결과와 동일한지 멱등성 검증")
+                .containsExactlyInAnyOrderElementsOf(resultAfterFirstCall);
+
+        softAssertions.assertAll();
     }
 }
