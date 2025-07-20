8121960712d51baf636ed2da3c64c30fd10f97a7
spoyodevelop <kjy9505@gmail.com>
2025-07-20 18:39:57 +0900

    refactor: daycell 분리 독립

---
+++ frontend/src/components/Calendar/DayCell/DayCell.styled.tsx
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+const dayCellRadius = 36;
+
+export const DayCell = styled.div<{
+  isPast: boolean;
+  isSunday: boolean;
+  isSaturday: boolean;
+  isToday: boolean;
+  isSelected: boolean;
+  isEmpty: boolean;
+}>`
+  user-select: none;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ isPast, theme, isSunday, isSaturday, isSelected }) => {
+    if (isSelected) return theme.colors.background;
+    if (isPast) return theme.colors.gray20;
+    if (isSunday) return theme.colors.red40;
+    if (isSaturday) return theme.colors.text;
+    return theme.colors.text;
+  }};
+  font-weight: ${({ isSaturday }) => (isSaturday ? 600 : 400)};
+  border: ${({ isToday, isSelected, theme }) => {
+    if (isSelected) return 'none';
+    if (isToday) return `2px solid ${theme.colors.primary}`;
+
+    return 'none';
+  }};
+  border-radius: ${dayCellRadius}px;
+  background-color: ${({ isSelected, theme }) => {
+    if (isSelected) return theme.colors.primary;
+    return 'none';
+  }};
+  &:hover {
+    background-color: ${({ theme, isPast, isEmpty }) => {
+      if (isPast || isEmpty) return 'none';
+      return theme.colors.primary;
+    }};
+    color: ${({ theme, isPast, isEmpty }) => {
+      if (isPast || isEmpty) return 'inherit';
+      return theme.colors.background;
+    }};
+    cursor: ${({ isPast, isEmpty }) => (isPast || isEmpty ? 'not-allowed' : 'grab')};
+  }
+  transition: all 0.15s ease-in-out;
+  transform: ${({ isSelected }) => (isSelected ? 'scale(1.05)' : 'scale(1)')};
+`;
