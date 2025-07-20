88934e225113c4d05fc6897b14ef5d37477b4d5b
hoyyChoi <hoyeon8165@gmail.com>
2025-07-21 01:13:13 +0900

    feat: Header 컴포넌트에 logo 적용 (임시)

--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -1,14 +1,18 @@
 import Text from '@/components/Text';
 import * as S from './Header.styled';
 import ThemeButton from '@/components/ThemeButton';
+import Logo from '@/icons/Logo';
 
 const Header = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: () => void }) => {
   return (
     <S.Container>
       <S.Content>
-        <Text variant="h2" color="primary">
-          아인슈타인
-        </Text>
+        <S.Wrapper>
+          <Logo />
+          <Text variant="h2" color="primary">
+            아인슈타인
+          </Text>
+        </S.Wrapper>
         <ThemeButton isDark={isDark} onToggle={toggleTheme} />
       </S.Content>
     </S.Container>
