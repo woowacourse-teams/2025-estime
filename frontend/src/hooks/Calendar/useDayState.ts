8ad2277b7645333211169fa13ff81a9f98b43a87
spoyodevelop <kjy9505@gmail.com>
2025-07-20 18:40:26 +0900

    feat: useDayState 훅 생성

---
+++ frontend/src/hooks/Calendar/useDayState.ts
@@ -0,0 +1,32 @@
+import { isItPast, isToday } from '@/utils/Calendar/dateUtils';
+import { formatDateToString } from '@/utils/Calendar/format';
+
+interface UseDayStateProps {
+  day: Date | null;
+  today: Date;
+  selectedDates: Set<string>;
+}
+
+export const useDayState = ({ day, today, selectedDates }: UseDayStateProps) => {
+  if (!day) {
+    return {
+      isPast: false,
+      isSunday: false,
+      isSaturday: false,
+      isToday: false,
+      isSelected: false,
+      isEmpty: true,
+      dateString: '',
+    };
+  }
+
+  return {
+    isPast: isItPast(day, today),
+    isSunday: day.getDay() === 0,
+    isSaturday: day.getDay() === 6,
+    isToday: isToday(day, today),
+    isSelected: selectedDates.has(formatDateToString(day)),
+    isEmpty: false,
+    dateString: day.getDate().toString(),
+  };
+};
