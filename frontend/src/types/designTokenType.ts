37e847892ddc31927cea4fa58d2a02ddfbe28c46
hoyyChoi <hoyeon8165@gmail.com>
2025-07-21 03:21:26 +0900

    feat: designToken 타입 추가

---
+++ frontend/src/types/designTokenType.ts
@@ -0,0 +1,37 @@
+export type CSSPaddingVar =
+  | 'var(--padding-1)'
+  | 'var(--padding-2)'
+  | 'var(--padding-3)'
+  | 'var(--padding-4)'
+  | 'var(--padding-5)'
+  | 'var(--padding-6)'
+  | 'var(--padding-7)'
+  | 'var(--padding-8)'
+  | 'var(--padding-9)'
+  | 'var(--padding-10)'
+  | 'var(--padding-11)';
+
+export type CSSGapVar =
+  | 'var(--gap-1)'
+  | 'var(--gap-2)'
+  | 'var(--gap-3)'
+  | 'var(--gap-4)'
+  | 'var(--gap-5)'
+  | 'var(--gap-6)'
+  | 'var(--gap-7)'
+  | 'var(--gap-8)'
+  | 'var(--gap-9)'
+  | 'var(--gap-10)'
+  | 'var(--gap-11)'
+  | 'var(--gap-12)';
+
+export type CSSRadiusVar =
+  | 'var(--radius-1)'
+  | 'var(--radius-2)'
+  | 'var(--radius-3)'
+  | 'var(--radius-4)'
+  | 'var(--radius-5)'
+  | 'var(--radius-6)'
+  | 'var(--radius-25)';
+
+export type CSSShadowVar = 'var(--shadow-card)';
