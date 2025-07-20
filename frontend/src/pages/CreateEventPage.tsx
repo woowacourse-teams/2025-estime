32683ea19465eeb1f9729644ef38a8ddc6215bbd
hoyyChoi <hoyeon8165@gmail.com>
2025-07-21 02:35:31 +0900

    feat: createEventPage 제작

--- frontend/src/pages/CreateEventPage.tsx
+++ frontend/src/pages/CreateEventPage.tsx
@@ -1,5 +1,54 @@
+import { useState } from 'react';
+import Flex from '@/components/Layout/Flex';
+import Wrapper from '@/components/Layout/Wrapper';
+import BasicSettings from '@/components/Section/BasicSettings';
+import OptionSettings from '@/components/Section/OptionSettings';
+import CalendarSettings from '@/components/Section/CalendarSettings';
+import Button from '@/components/Button';
+import Text from '@/components/Text';
+import { useNavigate } from 'react-router';
+
 const CreateEventPage = () => {
-  return <div>CreateEventPage</div>;
+  const navigate = useNavigate();
+  // 상세 설정 상태
+  const [isOpenAccordion, setIsOpenAccordion] = useState(false);
+  const [isDeadlineEnable, setisDeadlineEnable] = useState(false);
+  const [date, setDate] = useState('2025-07-19');
+
+  return (
+    <Wrapper>
+      <Flex justify="space-between" gap={40}>
+        <Flex.Item flex={1}>
+          <CalendarSettings />
+        </Flex.Item>
+        <Flex.Item flex={1}>
+          <Flex direction="column" justify="space-between" gap={36}>
+            <BasicSettings />
+            <OptionSettings
+              isOpenAccordion={isOpenAccordion}
+              onToggleAccordion={() => setIsOpenAccordion((prev) => !prev)}
+              isDeadlineEnable={isDeadlineEnable}
+              onToggleDeadline={() => setisDeadlineEnable((prev) => !prev)}
+              date={date}
+              onDateChange={(e) => setDate(e.target.value)}
+            />
+            <Flex justify="flex-end">
+              <Button
+                color="primary"
+                selected={true}
+                size="small"
+                onClick={() => navigate(`/check?id=${124124124}`)}
+              >
+                <Text variant="button" color="background">
+                  방 만들기
+                </Text>
+              </Button>
+            </Flex>
+          </Flex>
+        </Flex.Item>
+      </Flex>
+    </Wrapper>
+  );
 };
 
 export default CreateEventPage;
