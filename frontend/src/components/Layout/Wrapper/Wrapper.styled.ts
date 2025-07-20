2da67470657fac540262c7d1e8b9a8b64704544f
hoyyChoi <hoyeon8165@gmail.com>
2025-07-21 03:21:49 +0900

    refactor: Wrapper 컴포넌트 리팩토링

--- frontend/src/components/Layout/Wrapper/Wrapper.styled.ts
+++ frontend/src/components/Layout/Wrapper/Wrapper.styled.ts
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled';
+import { WrapperProps } from '.';
 
-export const Container = styled.div<{ maxWidth: number }>`
+export const Container = styled.div<WrapperProps>`
   width: 100%;
-  max-width: ${({ maxWidth }) => `${maxWidth}px`};
-  margin: 0 auto;
-  padding-top: 50px;
+  margin: ${({ center }) => (center ? '0 auto' : '0')};
+  max-width: ${({ maxWidth }) => (typeof maxWidth === 'number' ? `${maxWidth}px` : maxWidth)};
+  height: ${({ fullHeight }) => (fullHeight ? '100vh' : 'auto')};
+  background-color: ${({ backgroundColor }) => backgroundColor ?? 'transparent'};
+
+  padding-top: ${({ paddingTop }) => paddingTop ?? '0'};
+  padding-right: ${({ paddingRight }) => paddingRight ?? '0'};
+  padding-bottom: ${({ paddingBottom }) => paddingBottom ?? '0'};
+  padding-left: ${({ paddingLeft }) => paddingLeft ?? '0'};
 `;
--- frontend/src/components/Layout/Wrapper/index.tsx
+++ frontend/src/components/Layout/Wrapper/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import * as S from './Wrapper.styled';
+import { CSSPaddingVar } from '@/types/designTokenType';
 
-interface WrapperProps {
-  maxWidth?: number;
+export interface WrapperProps {
+  maxWidth?: number | 'fit-content' | 'max-content' | 'min-content' | '100%' | 'auto';
+  fullHeight?: boolean;
+  backgroundColor?: string;
+
+  paddingTop?: CSSPaddingVar;
+  paddingRight?: CSSPaddingVar;
+  paddingBottom?: CSSPaddingVar;
+  paddingLeft?: CSSPaddingVar;
+
+  center?: boolean; // margin: 0 auto 로 가운데 정렬 여부
+
   children: React.ReactNode;
 }
 
-const Wrapper = ({ maxWidth = 1280, children }: WrapperProps) => {
-  return <S.Container maxWidth={maxWidth}>{children}</S.Container>;
+const Wrapper = ({ maxWidth = 'fit-content', center = true, children, ...rest }: WrapperProps) => {
+  return (
+    <S.Container maxWidth={maxWidth} center={center} {...rest}>
+      {children}
+    </S.Container>
+  );
 };
 export default Wrapper;
