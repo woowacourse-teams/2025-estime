3bdc154ad791f2842c798b11add350d85c77cc6b
spoyodevelop <kjy9505@gmail.com>
2025-07-21 19:09:20 +0900

    feat: LoginModal 컴포넌트 스토리 작성

---
+++ frontend/src/components/LoginModal/LoginModal.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import LoginModal from './index';
+import { ModalContext } from '@/contexts/ModalContext';
+import { useState } from 'react';
+import Button from '@/components/Button';
+import Flex from '@/components/Layout/Flex';
+import Text from '@/components/Text';
+
+const meta: Meta<typeof LoginModal> = {
+  title: 'Components/LoginModal',
+  component: LoginModal,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ModalContext.Provider value={{ onClose: () => {}, position: 'center' }}>
+        <Story />
+      </ModalContext.Provider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+      <Flex>
+        <Button onClick={() => setIsOpen(true)} color="primary" size="small" selected={true}>
+          <Text variant="body" color="background">
+            Open Modal
+          </Text>
+        </Button>
+        <LoginModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      </Flex>
+    );
+  },
+};
