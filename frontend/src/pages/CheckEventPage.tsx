08cb3a5a2cd863d3a39dc9fd6185cfaef86be8f3
spoyodevelop <kjy9505@gmail.com>
2025-07-21 17:29:36 +0900

    feat: CheckEventPage 컴포넌트 및 스타일 추가

--- frontend/src/pages/CheckEventPage.tsx
+++ frontend/src/pages/CheckEventPage.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import Flex from '@/components/Layout/Flex';
+import Wrapper from '@/components/Layout/Wrapper';
+import Text from '@/components/Text';
+import LoginModal from '@/components/LoginModal';
+import * as S from './CheckEventPage.styled';
+import Button from '@/components/Button';
+
 const CheckEventPage = () => {
-  return <div>CheckEventPage</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Wrapper maxWidth={1280} paddingTop="var(--padding-11)" paddingBottom="var(--padding-11)">
+      <Flex justify="center" align="center">
+        <Flex.Item flex={1}>
+          <S.Container>
+            <Text variant="h2">이벤트 확인 페이지</Text>
+            <Button onClick={() => setIsOpen(true)} color="plum40">
+              모달 열기
+            </Button>
+            <LoginModal isOpen={isOpen} setIsOpen={setIsOpen} />
+          </S.Container>
+        </Flex.Item>
+      </Flex>
+    </Wrapper>
+  );
 };
 
 export default CheckEventPage;
