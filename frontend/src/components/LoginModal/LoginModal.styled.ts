783d439b18fcd5dc6f569c923c6d8e5234b9bfaa
spoyodevelop <kjy9505@gmail.com>
2025-07-21 19:11:10 +0900

    style: required 약간 조정

--- frontend/src/components/LoginModal/LoginModal.styled.ts
+++ frontend/src/components/LoginModal/LoginModal.styled.ts
@@ -8,6 +8,7 @@ export const LoginLabel = styled.label<{ required?: boolean }>`
   &::after {
     content: ${({ required }) => (required ? "'*'" : "''")};
     color: ${({ theme }) => theme.colors.red40};
+    font-size: ${({ theme }) => theme.typography.h4.fontSize};
     margin-left: 0.25rem;
   }
 `;
