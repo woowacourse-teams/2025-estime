769eac97790460c52b5c6c094af97cb80a8de8e1
hoyyChoi <hoyeon8165@gmail.com>
2025-07-21 16:59:09 +0900

    feat: IArrow 아이콘 생성

---
+++ frontend/src/icons/IArrow.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const IArrow = ({ color, ...props }: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-chevron-down-icon lucide-chevron-down"
+      {...props}
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  );
+};
+
+export default IArrow;
