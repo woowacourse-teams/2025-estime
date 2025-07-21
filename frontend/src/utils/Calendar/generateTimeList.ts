2f3079da3ad5d3a30deab9e13609eb49d6fdd549
deokbin <deokbin@naver.com>
2025-07-22 01:10:50 +0900

    feat: 시간 배열 만드는 util 함수 추가

---
+++ frontend/src/utils/Calendar/generateTimeList.ts
@@ -0,0 +1,24 @@
+const generateTimeList = ({
+  startTime,
+  endTime,
+  interval,
+}: {
+  startTime: number;
+  endTime: number;
+  interval: number;
+}) => {
+  const time = [];
+  for (let i = startTime; i <= endTime; i += interval) {
+    const hour = Math.floor(i / 60)
+      .toString()
+      .padStart(2, '0');
+
+    const minuteValue = i % 60;
+    const minute = minuteValue.toString().padStart(2, '0');
+    const timeText = `${hour}:${minute}`;
+    time.push({ timeText, isHour: minuteValue === 0 });
+  }
+  return time;
+};
+
+export default generateTimeList;
