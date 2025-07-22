09e28fac1f00f0845a25e0bd139e66718ce4fd30
hoyyChoi <hoyeon8165@gmail.com>
2025-07-23 01:46:17 +0900

    fix: svg 컴포넌트 내부 속성 카멜케이스로 변경

--- frontend/src/icons/IArrow.tsx
+++ frontend/src/icons/IArrow.tsx
@@ -9,9 +9,9 @@ const IArrow = ({ color, ...props }: IconType) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke={color}
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-chevron-down-icon lucide-chevron-down"
       {...props}
     >
--- frontend/src/icons/IClock.tsx
+++ frontend/src/icons/IClock.tsx
@@ -10,20 +10,20 @@ const IClock = ({ color = '#fff', ...props }: IconType) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_395_24)">
+      <g clipPath="url(#clip0_395_24)">
         <path
           d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z"
           stroke={color}
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M9 4.5V9L12 10.5"
           stroke={color}
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
