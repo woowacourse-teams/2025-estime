589a4e703d4b69860389965809e16d85f9b90df8
AppleMint98 <applemint9821@gmail.com>
2025-07-22 13:57:00 +0900

    feat: service 계층 TimeSlots 일급컬렉션 변경

--- backend/src/main/java/com/bether/bether/timeslot/application/service/TimeSlotService.java
+++ backend/src/main/java/com/bether/bether/timeslot/application/service/TimeSlotService.java
@@ -7,6 +7,7 @@ import com.bether.bether.timeslot.application.dto.output.TimeSlotStatisticOutput
 import com.bether.bether.timeslot.domain.TimeSlot;
 import com.bether.bether.timeslot.domain.TimeSlotRepository;
 import com.bether.bether.timeslot.domain.TimeSlotStatistic;
+import com.bether.bether.timeslot.domain.TimeSlots;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -22,18 +23,18 @@ public class TimeSlotService {
     private final TimeSlotRepository timeSlotRepository;
 
     @Transactional(readOnly = true)
-    public List<TimeSlot> getAllByRoomId(final Long roomId) {
+    public TimeSlots getAllByRoomId(final Long roomId) {
         return timeSlotRepository.findAllByRoomId(roomId);
     }
 
     @Transactional(readOnly = true)
-    public List<TimeSlot> getAllByRoomIdAndUserName(final Long roomId, final String userName) {
+    public TimeSlots getAllByRoomIdAndUserName(final Long roomId, final String userName) {
         return timeSlotRepository.findAllByRoomIdAndUserName(roomId, userName);
     }
 
     @Transactional
-    public List<TimeSlot> saveAll(final Long roomId, final TimeSlotInput input) {
-        final List<TimeSlot> timeSlots = input.toEntity(roomId);
+    public TimeSlots saveAll(final Long roomId, final TimeSlotInput input) {
+        final TimeSlots timeSlots = input.toEntity(roomId);
         return timeSlotRepository.saveAll(timeSlots);
     }
 
@@ -50,7 +51,7 @@ public class TimeSlotService {
     @Transactional
     public void updateTimeSlots(final Long roomId, final TimeSlotUpdateInput input) {
         final List<TimeSlot> existingTimeSlots = timeSlotRepository.findAllByRoomIdAndUserName(roomId,
-                input.userName());
+                input.userName()).getTimeSlots(); //FIXME : 임시
 
         final Set<LocalDateTime> existingStartAts = existingTimeSlots.stream()
                 .map(TimeSlot::getStartAt)
@@ -72,16 +73,16 @@ public class TimeSlotService {
                 .toList();
 
         if (!timeSlotsToSave.isEmpty()) {
-            timeSlotRepository.saveAll(timeSlotsToSave);
+            timeSlotRepository.saveAll(TimeSlots.from(timeSlotsToSave));
         }
         if (!timeSlotsToDelete.isEmpty()) {
-            timeSlotRepository.deleteAll(timeSlotsToDelete);
+            timeSlotRepository.deleteAll(TimeSlots.from(timeSlotsToDelete));
         }
     }
 
     private TimeSlotStatistic calculateTimeSlotStatistic(final Long roomId) {
         // TODO List<TimeSlot> 일급 컬렉션 적용
-        final List<TimeSlot> timeSlots = getAllByRoomId(roomId);
+        final List<TimeSlot> timeSlots = getAllByRoomId(roomId).getTimeSlots(); // FIXME : 임시
         final TimeSlotStatistic statistic = TimeSlotStatistic.create();
         statistic.calculate(timeSlots);
         return statistic;
