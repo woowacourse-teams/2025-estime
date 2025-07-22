37f37802e91abbbb61188c95becbd94ddda11bbe
hoyyChoi <hoyeon8165@gmail.com>
2025-07-23 02:30:52 +0900

    test: storybook 에러 해결

--- frontend/src/components/Sections/BasicSettings/BasicSettings.stories.tsx
+++ frontend/src/components/Sections/BasicSettings/BasicSettings.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryFn } from '@storybook/react-webpack5';
 import BasicSettings from '.';
+import { useState } from 'react';
+import type { Field } from '@/types/field';
 
 export default {
   title: 'Components/BasicSettings',
@@ -7,7 +9,20 @@ export default {
   tags: ['autodocs'],
 } as Meta<typeof BasicSettings>;
 
-const Template: StoryFn<typeof BasicSettings> = (args) => <BasicSettings {...args} />;
+const Template: StoryFn = () => {
+  const [title, setTitle] = useState('');
+  const titleField: Field<string> = {
+    value: title,
+    set: setTitle,
+  };
+
+  const [time, setTime] = useState({ startTime: '', endTime: '' });
+  const timeField: Field<{ startTime: string; endTime: string }> = {
+    value: time,
+    set: setTime,
+  };
+
+  return <BasicSettings title={titleField} time={timeField} />;
+};
 
 export const Default = Template.bind({});
-Default.args = {};
--- frontend/src/components/Sections/OptionSettings/OptionSettings.stories.tsx
+++ frontend/src/components/Sections/OptionSettings/OptionSettings.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryFn } from '@storybook/react-webpack5';
 import OptionSettings from '.';
+import { useState } from 'react';
+import type { Field } from '@/types/field';
 
 export default {
   title: 'Components/OptionSettings',
@@ -7,7 +9,25 @@ export default {
   tags: ['autodocs'],
 } as Meta<typeof OptionSettings>;
 
-const Template: StoryFn<typeof OptionSettings> = (args) => <OptionSettings {...args} />;
+const Template: StoryFn = () => {
+  const [deadLine, setDeadLine] = useState<{ date: string; time: string }>({
+    date: '2025-07-23',
+    time: '16:00',
+  });
+
+  const deadLineField: Field<{ date: string; time: string }> = {
+    value: deadLine,
+    set: setDeadLine,
+  };
+
+  const [isPublic, setIsPublic] = useState<boolean>(true);
+
+  const isPublicField: Field<boolean> = {
+    value: isPublic,
+    set: setIsPublic,
+  };
+
+  return <OptionSettings deadLine={deadLineField} isPublic={isPublicField} />;
+};
 
 export const Default = Template.bind({});
-Default.args = {};
