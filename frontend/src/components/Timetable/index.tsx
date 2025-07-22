4e2be0eafd472d618dfe24cee28c4aa152409350
deokbin <deokbin@naver.com>
2025-07-22 21:03:33 +0900

    feat: Timetable header 추가

--- frontend/src/components/Timetable/index.tsx
+++ frontend/src/components/Timetable/index.tsx
@@ -7,12 +7,25 @@ import Text from '../Text';
 import { getDayOfWeek } from '@/utils/Calendar/getDayofWeek';
 import * as S from './Timetable.styled';
 import generateTimeList from '@/utils/Calendar/generateTimeList';
+import Button from '../Button';
 
 const response = {
   title: 'Bether 스터디',
-  availableDates: ['2026-07-15', '2026-07-16', '2026-07-17', '2026-07-18', '2026-07-19'],
+  availableDates: [
+    '2026-07-15',
+    '2026-07-16',
+    '2026-07-17',
+    '2026-07-18',
+    '2026-07-19',
+    '2026-07-20',
+    '2026-07-21',
+    '2026-07-22',
+    '2026-07-23',
+    '2026-07-24',
+    '2026-07-25',
+  ],
   startTime: '09:00',
-  endTime: '18:00',
+  endTime: '23:00',
   deadLine: '2026-07-15T10:00',
   isPublic: true,
   roomSession: '4d1f3a7e-6b8c-4d9a-9e0b-2f7c1a5b9d8e',
@@ -38,38 +51,74 @@ const Timetable = () => {
   const theme = useTheme();
 
   return (
-    <Flex justify="center">
-      <Wrapper maxWidth={600}>
-        {timeList.map(({ timeText, isHour }) => (
-          <S.GridContainer key={timeText} availableDates={response.availableDates.length}>
-            <S.TimeLabel isHour={isHour}>
-              <Text variant="body">{isHour ? timeText : ''}</Text>
-            </S.TimeLabel>
-            {response.availableDates.map((date) => (
-              <S.HeaderCell
-                key={`${date}-${timeText}`}
-                onMouseDown={() => onMouseDown(`${date} ${timeText}`)}
-                // onMouseEnter={() => onMouseEnter(`${date} ${timeText}`)}
-                onMouseUp={() => onMouseUp()}
-                // onMouseLeave={() => onMouseLeave()}
-                selectedTimes={selectedTimes}
-                date={date}
-                timeText={timeText}
-              >
-                {timeText === 'Dates' && (
-                  <Text variant="body">
-                    <Flex direction="column" justify="center" align="center">
-                      <div>{date.split('-').slice(1).join('.')}</div>
-                      <div>({getDayOfWeek(date)})</div>
-                    </Flex>
-                  </Text>
+    <Wrapper
+      maxWidth={800}
+      backgroundColor={theme.colors.background}
+      paddingTop="var(--padding-9)"
+      paddingRight="var(--padding-9)"
+      paddingBottom="var(--padding-9)"
+      paddingLeft="var(--padding-9)"
+      borderRadius="var(--radius-6)"
+    >
+      <Flex direction="column" gap="var(--gap-6)">
+        <S.TimetableHeader>
+          <Flex direction="column" gap="var(--gap-4)">
+            <Text variant="h2" color="text">
+              내 시간 등록하기
+            </Text>
+            <Text variant="body" color="text">
+              가능한 시간을 아래 시간표에서 드래그 해주세요 !
+            </Text>
+          </Flex>
+          <Wrapper maxWidth={100} center={false}>
+            <Button color="primary">
+              <Text variant="button" color="text">
+                저장하기
+              </Text>
+            </Button>
+          </Wrapper>
+        </S.TimetableHeader>
+        <S.TimetableContent>
+          <S.TimeSlotColumn>
+            {timeList.map(({ timeText, isHour }) => (
+              <S.GridContainer key={timeText}>
+                {isHour && (
+                  <S.TimeLabel>
+                    <Text variant="body" color="text">
+                      {isHour ? timeText : ''}
+                    </Text>
+                  </S.TimeLabel>
                 )}
-              </S.HeaderCell>
+              </S.GridContainer>
             ))}
-          </S.GridContainer>
-        ))}
-      </Wrapper>
-    </Flex>
+          </S.TimeSlotColumn>
+          {response.availableDates.map((date) => (
+            <Wrapper key={date} center={false}>
+              {timeList.map(({ timeText }) => (
+                <S.HeaderCell
+                  key={`${date}-${timeText}`}
+                  onMouseDown={() => onMouseDown(`${date} ${timeText}`)}
+                  onMouseUp={() => onMouseUp()}
+                  onMouseMove={() => onMouseEnter(`${date} ${timeText}`)}
+                  selectedTimes={selectedTimes}
+                  date={date}
+                  timeText={timeText}
+                >
+                  {timeText === 'Dates' && (
+                    <Text variant="body" color="text">
+                      <Flex direction="column" justify="center" align="center">
+                        <div>{date.split('-').slice(1).join('.')}</div>
+                        <div>({getDayOfWeek(date)})</div>
+                      </Flex>
+                    </Text>
+                  )}
+                </S.HeaderCell>
+              ))}
+            </Wrapper>
+          ))}
+        </S.TimetableContent>
+      </Flex>
+    </Wrapper>
   );
 };
 export default Timetable;
