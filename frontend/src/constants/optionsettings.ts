c9c36fd89e2b4ec583518dccc65f471f605e6ca8
hoyyChoi <hoyeon8165@gmail.com>
2025-07-23 01:29:10 +0900

    feat: ACCESS_OPTIONS 상수 flag 값 추가

--- frontend/src/constants/optionsettings.ts
+++ frontend/src/constants/optionsettings.ts
@@ -4,12 +4,14 @@ import ILock from '@/icons/ILock';
 export const ACCESS_OPTIONS = [
   {
     value: 'public',
+    flag: true,
     Icon: IGlobe,
     label: '공개',
     description: '누구나 이 페이지에 접근할 수 있습니다.',
   },
   {
     value: 'private',
+    flag: false,
     Icon: ILock,
     label: '비공개',
     description: '링크를 가진 사람만 접근할 수 있습니다.',
