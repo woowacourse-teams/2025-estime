720c8bf244218ecaa5bd7afccda57340a0f8d63c
조재중 <9gle3492@gmail.com>
2025-07-22 17:33:39 +0900

    Room 서비스 계층 분리 (#160)
    * refactor: room service 계층 분리
    
    * chore: merge conflict 해결

--- backend/src/main/java/com/bether/bether/room/application/service/RoomApplicationService.java
+++ backend/src/main/java/com/bether/bether/room/application/service/RoomApplicationService.java
@@ -1,11 +1,9 @@
 package com.bether.bether.room.application.service;
 
-import com.bether.bether.common.NotFoundException;
 import com.bether.bether.room.application.dto.RoomCreateInput;
 import com.bether.bether.room.application.dto.RoomCreateOutput;
 import com.bether.bether.room.application.dto.RoomOutput;
 import com.bether.bether.room.domain.Room;
-import com.bether.bether.room.domain.RoomRepository;
 import com.bether.bether.timeslot.application.dto.input.TimeSlotInput;
 import com.bether.bether.timeslot.application.dto.input.TimeSlotUpdateInput;
 import com.bether.bether.timeslot.application.dto.output.TimeSlotRecommendationsOutput;
@@ -19,64 +17,57 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class RoomService {
+public class RoomApplicationService {
 
-    private final RoomRepository roomRepository;
+    private final RoomDomainService roomDomainService;
     private final TimeSlotService timeSlotService;
 
     @Transactional(readOnly = true)
     public RoomOutput getBySession(final UUID session) {
-        final Room room = roomRepository.findBySession(session)
-                .orElseThrow(() -> new NotFoundException(Room.class.getSimpleName()));
+        final Room room = roomDomainService.getBySession(session);
         return RoomOutput.from(room);
     }
 
     @Transactional
     public RoomCreateOutput saveRoom(final RoomCreateInput input) {
         final Room room = input.toEntity();
-        final Room saved = roomRepository.save(room);
+        final Room saved = roomDomainService.save(room);
         return RoomCreateOutput.from(saved);
     }
 
     @Transactional(readOnly = true)
     public TimeSlots getTimeSlotsBySession(final UUID session) {
-        final Long id = getIdBySession(session);
+        final Long id = roomDomainService.getIdBySession(session);
         return timeSlotService.getAllByRoomId(id);
     }
 
     @Transactional(readOnly = true)
     public TimeSlots getTimeSlotsBySessionAndUserName(final UUID session, final String userName) {
-        final Long id = getIdBySession(session);
+        final Long id = roomDomainService.getIdBySession(session);
         return timeSlotService.getAllByRoomIdAndUserName(id, userName);
     }
 
     @Transactional
     public TimeSlots saveTimeSlots(final TimeSlotInput input) {
-        final Long id = getIdBySession(input.roomSession());
+        final Long id = roomDomainService.getIdBySession(input.roomSession());
         return timeSlotService.saveAll(id, input);
     }
 
     @Transactional(readOnly = true)
     public TimeSlotStatisticOutput generateTimeSlotStatistic(final UUID session) {
-        final Long id = getIdBySession(session);
+        final Long id = roomDomainService.getIdBySession(session);
         return timeSlotService.generateTimeSlotStatistic(id);
     }
 
     @Transactional(readOnly = true)
     public TimeSlotRecommendationsOutput recommendTopTimeSlots(final UUID session) {
-        final Long id = getIdBySession(session);
+        final Long id = roomDomainService.getIdBySession(session);
         return timeSlotService.recommendTopTimeSlots(id);
     }
 
     @Transactional
     public void updateTimeSlots(final TimeSlotUpdateInput input) {
-        final Long roomId = getIdBySession(input.roomSession());
+        final Long roomId = roomDomainService.getIdBySession(input.roomSession());
         timeSlotService.updateTimeSlots(roomId, input);
-    }
-
-    private Long getIdBySession(final UUID session) {
-        return roomRepository.findBySession(session)
-                .orElseThrow(() -> new NotFoundException(Room.class.getSimpleName()))  // TODO 검색 대상 파라미터 명시 여부 논의 필요
-                .getId();
     }
 }
