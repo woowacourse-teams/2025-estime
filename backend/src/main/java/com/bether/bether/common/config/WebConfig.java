047a136cbc88cfb1318325ba1ffc18a0e259244d
AppleMint98 <applemint9821@gmail.com>
2025-07-23 14:36:23 +0900

    feat: CORS 설정 클래스 추가

---
+++ backend/src/main/java/com/bether/bether/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.bether.bether.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins) // TODO: 실제 배포 시 프론트엔드 URL로 변경
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(7200);
+    }
+}
