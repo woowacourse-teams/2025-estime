25f1013a5f29d8df49eb02cda8dbd9557a5d3a55
thgml05 <76714311+thgml05@users.noreply.github.com>
2025-07-23 14:21:43 +0900

    API 연동 로직 구현  (#164)
    * chore: webpack 환경 변수 설정 추가
    
    * feat: BASE_URL 추가
    
    * feat: API 요청 함수 추가
    
    * refactor: baseFetch 함수 props 객체화
    
    * refactor: api room 공통 주소 네이밍 수정
    
    * refactor: 공통 api porps 객체화
    
    * refactor: baseFetch에서 공통 api 함수 사용하도록 수정
    
    * refactor: api 호출 관련 타입 네이밍 수정 (뒤에 Type 추가)

---
+++ frontend/src/apis/common/constant.ts
@@ -0,0 +1,1 @@
+export const ROOM_API_PATH = '/api/v1/rooms';
---
+++ frontend/src/apis/time/time.ts
@@ -0,0 +1,41 @@
+import api from '../common';
+import { ROOM_API_PATH } from '../common/constant';
+import {
+  CombinedAvailableTimesResponseType,
+  UserAvailableTimeResponseType,
+  RecommendationTimeResponseType,
+  UserAvailableTimeRequestType,
+} from './type';
+
+export const updateUserAvailableTime = async (
+  session: string,
+  body: UserAvailableTimeRequestType
+) => {
+  return await api.put(`${ROOM_API_PATH}/${session}/time-slots`, body);
+};
+
+export const createUserAvailableTime = async (
+  session: string,
+  body: UserAvailableTimeRequestType
+) => {
+  return await api.post(`${ROOM_API_PATH}/${session}/time-slots`, body);
+};
+
+export const getUserAvailableTime = async (
+  session: string,
+  name: string
+): Promise<UserAvailableTimeResponseType> => {
+  return await api.get(`${ROOM_API_PATH}/${session}/time-slots/user`, { name });
+};
+
+export const getTimeSlotStatistics = async (
+  session: string
+): Promise<CombinedAvailableTimesResponseType> => {
+  return await api.get(`${ROOM_API_PATH}/${session}/time-slots/statistic`);
+};
+
+export const getRecommendedTime = async (
+  session: string
+): Promise<RecommendationTimeResponseType> => {
+  return api.get(`${ROOM_API_PATH}/${session}/time-slots/recommendation`);
+};
