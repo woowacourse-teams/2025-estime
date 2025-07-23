b9f6bdaefa4f8b1e6cfecd9b01ff6977a3f7c7ea
deokbin <deokbin@naver.com>
2025-07-23 17:08:53 +0900

    chore: info 아이콘 사이즈 수정

--- frontend/src/icons/IInfo.tsx
+++ frontend/src/icons/IInfo.tsx
@@ -4,8 +4,8 @@ const IInfo = ({ color = '#fff', ...props }: IconType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="18"
+      height="18"
       viewBox="0 0 24 24"
       fill="none"
       stroke={color}
