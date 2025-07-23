0338fd68a321744fd86b5ba04d6706640c89d93b
AppleMint98 <applemint9821@gmail.com>
2025-07-23 15:18:04 +0900

    feat: 허용 HTTP 메서드 OPTIONS 추가

--- backend/src/main/java/com/bether/bether/common/config/WebConfig.java
+++ backend/src/main/java/com/bether/bether/common/config/WebConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(allowedOrigins) // TODO: 실제 배포 시 프론트엔드 URL로 변경
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(7200);
