0faa5c3b13609aef9abfe02733aea1ff057b6874
hoyyChoi <hoyeon8165@gmail.com>
2025-07-23 20:32:02 +0900

    chore: getCreateRoomPayload jsDoc 추가

--- frontend/src/apis/transform/getCreateRoomPayload.ts
+++ frontend/src/apis/transform/getCreateRoomPayload.ts
@@ -1,6 +1,12 @@
 import { RoomInfo } from '@/hooks/useCreateRoom';
 import { CreateRoomRequestType } from '../room/type';
 
+/**
+ * 클라이언트 상태(RoomInfo)를 서버 요청 형식(CreateRoomRequestType)으로 변환합니다.
+ *
+ * @param roomInfo - 클라이언트에서 관리하는 방 생성 정보
+ * @returns 서버에 전송할 API 요청 페이로드 형식
+ */
 export const getCreateRoomPayload = (roomInfo: RoomInfo): CreateRoomRequestType => {
   const { title, availableDates, time, deadLine, isPublic } = roomInfo;
 
