14ac18d1b5f79e8e7834e86cf58077737f1e7bcd
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-07-23 22:50:17 +0900

    refactor: connectedRoom 생성 알림 메시지 로직 간단히 개선
    - 리버 코드와 병합 후 최종 개선 예정

--- backend/src/main/java/com/bether/bether/connection/application/ConnectedRoomApplicationService.java
+++ backend/src/main/java/com/bether/bether/connection/application/ConnectedRoomApplicationService.java
@@ -1,12 +1,13 @@
 package com.bether.bether.connection.application;
 
+import com.bether.bether.common.config.WebConfigProperties;
 import com.bether.bether.connection.application.dto.input.ConnectedRoomCreateInput;
 import com.bether.bether.connection.application.dto.output.ConnectedRoomCreateOutput;
+import com.bether.bether.connection.application.dto.output.OutboundMessage;
 import com.bether.bether.connection.domain.ConnectedRoom;
 import com.bether.bether.connection.domain.Platform;
 import com.bether.bether.room.application.service.RoomDomainService;
 import com.bether.bether.room.domain.Room;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,12 +21,14 @@ public class ConnectedRoomApplicationService {
     private final RoomDomainService roomDomainService;
     private final ConnectedRoomDomainService connectedRoomDomainService;
     private final Map<Platform, MessageSender> messageSenders;
+    private final WebConfigProperties webConfigProperties; // TODO rename
 
     @Transactional
     public ConnectedRoomCreateOutput save(final ConnectedRoomCreateInput input) {
         final Room room = roomDomainService.save(input.toRoomEntity());
         final ConnectedRoom connectedRoom = ConnectedRoom.withoutId(room, input.platform());
         connectedRoomDomainService.save(connectedRoom);
+
         sendConnectedRoomCreatedMessage(connectedRoom, input.channelId()); // 딜레이가 없는 경우 문제 발생
 
         return ConnectedRoomCreateOutput.from(connectedRoom);
@@ -38,34 +41,13 @@ public class ConnectedRoomApplicationService {
      * 3. 마감 기한 임박 알림 메시지
      * 4. 일정 확정 메시지
      */
-    private void sendConnectedRoomCreatedMessage(
-            final ConnectedRoom connectedRoom,
-            final String channelId
-    ) {
-        final Room room = connectedRoom.getRoom();
-        final String url = "https://estime.today/" + room.getSession();
+    private void sendConnectedRoomCreatedMessage(final ConnectedRoom connectRoom, final String channelId) {
+        final Room room = connectRoom.getRoom();
+        final String url = webConfigProperties.dev() + room.getSession();
 
-        sendMessage(connectedRoom.getPlatform(), channelId, generateCreatedMessage(room, url));
-    }
-
-    private String generateCreatedMessage(final Room room, final String url) {
-        final String title = "**" + room.getTitle() + "**";
-        final String line = "<" + url + "|🔗 바로가기>";
-        final String deadLine = String.format(
-                ":alarm_clock: 마감: %s",
-                room.getDeadLine().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-        return title + "\n" + line + "\n" + deadLine;
-    }
-
-    private void sendMessage(
-            final Platform platform,
-            final String channelId,
-            final String message
-    ) {
-        switch (platform) {
-            case DISCORD -> messageSenders.get(Platform.DISCORD).execute(channelId, message);
-            case SLACK -> messageSenders.get(Platform.SLACK).execute(channelId, message);
-        }
+        messageSenders.get(connectRoom.getPlatform()).execute(
+                channelId,
+                OutboundMessage.roomCreated(connectRoom, url));
     }
 }
 
--- backend/src/test/java/com/bether/bether/connection/application/ConnectedRoomApplicationServiceTest.java
+++ backend/src/test/java/com/bether/bether/connection/application/ConnectedRoomApplicationServiceTest.java
@@ -1,7 +1,7 @@
 package com.bether.bether.connection.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 
 import com.bether.bether.connection.application.dto.input.ConnectedRoomCreateInput;
@@ -50,7 +50,7 @@ class ConnectedRoomApplicationServiceTest {
                 "testChannelId"
         );
 
-        doNothing().when(discordMessageSender).execute(anyString(), anyString());
+        doNothing().when(discordMessageSender).execute(any(), any());
 
         // when
         final ConnectedRoomCreateOutput saved = connectedRoomApplicationService.save(input);
