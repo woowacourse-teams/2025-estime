8484be44dd2cc42f7577ccef77c360bd205c3fce
hoyyChoi <hoyeon8165@gmail.com>
2025-07-23 14:55:51 +0900

    fix: ACCESS_OPTIONS 상수 필드 변경 및 타입 추가

--- frontend/src/constants/optionsettings.ts
+++ frontend/src/constants/optionsettings.ts
@@ -1,17 +1,22 @@
 import IGlobe from '@/icons/IGlobe';
 import ILock from '@/icons/ILock';
 
-export const ACCESS_OPTIONS = [
+interface AccessOption {
+  value: 'public' | 'private';
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  label: '공개' | '비공개';
+  description: '누구나 이 페이지에 접근할 수 있습니다.' | '링크를 가진 사람만 접근할 수 있습니다.';
+}
+
+export const ACCESS_OPTIONS: AccessOption[] = [
   {
     value: 'public',
-    flag: true,
     Icon: IGlobe,
     label: '공개',
     description: '누구나 이 페이지에 접근할 수 있습니다.',
   },
   {
     value: 'private',
-    flag: false,
     Icon: ILock,
     label: '비공개',
     description: '링크를 가진 사람만 접근할 수 있습니다.',
