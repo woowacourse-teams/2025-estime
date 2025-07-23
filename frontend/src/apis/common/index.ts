82e21b78a52bbee0122cbd87a85f64cb623a67d6
thgml05 <aisohii@naver.com>
2025-07-23 13:43:34 +0900

    refactor: 공통 api porps 객체화

--- frontend/src/apis/common/index.ts
+++ frontend/src/apis/common/index.ts
@@ -2,14 +2,14 @@ import baseFetch from './baseFetch';
 import { QueryParams, RequestBody } from './type';
 
 const api = {
-  get: <T>(path: string, query?: QueryParams) => baseFetch<T>(path, 'GET', query),
+  get: <T>(path: string, query?: QueryParams) => baseFetch<T>({ path, method: 'GET', query }),
   post: <T>(path: string, body?: RequestBody, query?: QueryParams) =>
-    baseFetch<T>(path, 'POST', query, body),
+    baseFetch<T>({ path, method: 'POST', query, body }),
   put: <T>(path: string, body?: RequestBody, query?: QueryParams) =>
-    baseFetch<T>(path, 'PUT', query, body),
+    baseFetch<T>({ path, method: 'PUT', query, body }),
   patch: <T>(path: string, body?: RequestBody, query?: QueryParams) =>
-    baseFetch<T>(path, 'PATCH', query, body),
-  delete: <T>(path: string, query?: QueryParams) => baseFetch<T>(path, 'DELETE', query),
+    baseFetch<T>({ path, method: 'PATCH', query, body }),
+  delete: <T>(path: string, query?: QueryParams) => baseFetch<T>({ path, method: 'DELETE', query }),
 };
 
 export default api;
