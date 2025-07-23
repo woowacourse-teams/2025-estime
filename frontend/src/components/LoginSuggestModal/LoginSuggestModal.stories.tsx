1d15d09a261abe0c1ce8726c20a246708e016870
spoyodevelop <kjy9505@gmail.com>
2025-07-23 16:47:34 +0900

    feat: 로그인 suggestion 모달 스토리 작성

---
+++ frontend/src/components/LoginSuggestModal/LoginSuggestModal.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import LoginSuggestModal from '.';
+import { ModalContext } from '@/contexts/ModalContext';
+import { useEffect, useRef, useState } from 'react';
+
+const meta: Meta<typeof LoginSuggestModal> = {
+  title: 'Components/LoginSuggestModal',
+  component: LoginSuggestModal,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      story: { inline: false },
+    },
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ModalContext.Provider value={{ onClose: () => {}, position: 'inside' }}>
+        <Story />
+      </ModalContext.Provider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const modalTargetRef = useRef<HTMLDivElement>(null);
+    const [isSuggestModalOpen, setIsSuggestModalOpen] = useState(false);
+    useEffect(() => {
+      if (modalTargetRef.current) {
+        setIsSuggestModalOpen(true);
+      }
+    }, []);
+    return (
+      <div
+        style={{ position: 'relative', width: '800px' }}
+        id="login-suggest-modal"
+        ref={modalTargetRef}
+      >
+        <LoginSuggestModal
+          target={modalTargetRef.current}
+          isOpen={isSuggestModalOpen}
+          onLoginClick={() => {}}
+        />
+      </div>
+    );
+  },
+};
