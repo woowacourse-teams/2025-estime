af44b32266b58b4246992c19ef7e05727731dd1c
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-07-23 22:47:37 +0900

    feat: discord listener/registrar slash command 명세에 맞게 수정
    - 기존 test용 slash command 수정

--- backend/src/main/java/com/bether/bether/connection/discord/infrastructure/DiscordSlashCommandRegistrar.java
+++ backend/src/main/java/com/bether/bether/connection/discord/infrastructure/DiscordSlashCommandRegistrar.java
@@ -4,38 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class DiscordSlashCommandRegistrar extends ListenerAdapter {
 
-    // 특정 길드 설정 (n초 후 갱신)
+    // 특정 테스트용 길드(서버) 설정 (n초 후 갱신), 삭제 예정
     @Override
     public void onReady(final ReadyEvent e) {
         final Guild guild = e.getJDA().getGuildById("1393585305713512478");
-        log.info("guild = {}", guild);
         if (guild != null) {
-            guild.upsertCommand("schedule", "스케줄 방 생성")
-                    .addOption(OptionType.STRING, "room_name", "방 이름", true)
-                    .addOption(OptionType.INTEGER, "max_participants", "최대 참여자 수", false)
+            guild.upsertCommand("estime-create", "일정 조율 시작하기")
                     .queue();
         }
     }
 
-    // 글로벌 설정 (n시간 후 갱신)
+//    // 글로벌 설정 (n시간 후 갱신)
 //    @Override
 //    public void onReady(final ReadyEvent event) {
 //        final JDA jda = event.getJDA();
-//        jda.upsertCommand("schedule", "스케줄 방을 생성합니다.")
-//                .addOption(OptionType.STRING, "room_name", "방 이름", true)
-//                .addOption(OptionType.INTEGER, "max_participants", "최대 참여자 수", false)
+//        jda.upsertCommand("estime-create", "일정 조율 시작하기")
 //                .queue();
-//
-//        event.getJDA().getGuilds().forEach(g ->
-//                log.info("✔ Bot joined guild: {} / ID={}", g.getName(), g.getId())
-//        );
 //
 //        // 불필요한 추가 등록 방지
 //        jda.removeEventListener(this);
