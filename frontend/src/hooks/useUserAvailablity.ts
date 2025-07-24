39dea681afa275fe28905c474326eaeaea373379
Deokbin Kim <162319857+Db0111@users.noreply.github.com>
2025-07-24 14:01:27 +0900

    타임테이블 구현 (#165)
    * feat: 시간 배열 만드는 util 함수 추가
    
    * feat: 요일 반환 util 함수 추가
    
    * feat: 시간 선택 커스텀 훅 추가
    
    * feat: 타임테이블 컴포넌트 추가
    
    * design: 타임테이블 컴포넌트 디자인 추가
    
    * feat: 타임테이블 컴포넌트 스토리북 추가
    
    * feat: endTime 이전 시간까지만 timeList 만들도록 수정
    
    * feat: onMouseEnter 로직 보완
    
    * design: borderRadius 속성 추가
    
    * design: Timetable 디자인 추가
    
    * feat: Timetable header 추가
    
    * feat: props 타입 및 변수명 변경
    
    * feat: case 별 타임테이블 스토리북 추가
    
    * chore: 서버 전달 형태에 맞게 수정
    
    * fix: 드래드 안되는 오류 수정
    
    * feat: 사용자 가능시간 서버 요청형식으로 바꾸는 포맷 함수 추가
    
    * feat: UserAvailability 타입 추가
    
    * feat: session type 임시 변경
    
    * refactor: useUserAvailability 훅으로 내부 상태 분리
    
    * fix: 스토리북 props 오류 수정
    
    * feat: Timetable 페이지에 추가
    
    * fix: roomSession 타입 및 값 변경
    
    * chore: 불필요 undefined 타입 제거
    
    * design: padding props 수정
    
    * design: div 대신 Text 컴포넌트 사용
    
    * remove: 타임테이블 스토리북 제거
    
    * feat: session, name 관련 타입 수정
    
    * feat: Wrapper 컴포넌트 타입 확장
    
    * feat: Timetable에 ref 추가

---
+++ frontend/src/hooks/useUserAvailablity.ts
@@ -0,0 +1,50 @@
+import { createUserAvailableTime } from '@/apis/time/time';
+import { toCreateUserAvailability } from '@/apis/transform/toCreateUserAvailablity';
+import { UserAvailability } from '@/types/userAvailability';
+import { useState } from 'react';
+
+export const useUserAvailability = ({
+  name,
+  session,
+}: {
+  name: string;
+  session: string | null;
+}) => {
+  const initialUserAvailability = {
+    userName: '메이토',
+    selectedTimes: new Set<string>(),
+  };
+  const [userAvailability, setUserAvailability] =
+    useState<UserAvailability>(initialUserAvailability);
+
+  const userName = {
+    value: name,
+    set: (userName: string) => setUserAvailability((prev) => ({ ...prev, userName })),
+  };
+
+  const selectedTimes = {
+    value: userAvailability.selectedTimes,
+    set: (selectedTimes: Set<string>) =>
+      setUserAvailability((prev) => ({ ...prev, selectedTimes })),
+  };
+
+  const userAvailabilitySubmit = async () => {
+    try {
+      const payload = toCreateUserAvailability(userAvailability);
+      const response = await createUserAvailableTime(session, payload);
+      return response;
+    } catch (err) {
+      const e = err as Error;
+      alert(e.message);
+      console.error(err);
+    }
+  };
+
+  return {
+    userName,
+    selectedTimes,
+    userAvailabilitySubmit,
+  };
+};
+
+export default useUserAvailability;
