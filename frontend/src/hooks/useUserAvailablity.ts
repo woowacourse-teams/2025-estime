14567c791a4bdb80dd5b4b8d0a692edad0233290
deokbin <deokbin@naver.com>
2025-07-24 18:22:49 +0900

    fix: useUserAvailability 훅 내부 fetch 수정

--- frontend/src/hooks/useUserAvailablity.ts
+++ frontend/src/hooks/useUserAvailablity.ts
@@ -1,11 +1,12 @@
-import {
-  createUserAvailableTime,
-  getUserAvailableTime,
-  updateUserAvailableTime,
-} from '@/apis/time/time';
+import { getUserAvailableTime, updateUserAvailableTime } from '@/apis/time/time';
 import { toCreateUserAvailability } from '@/apis/transform/toCreateUserAvailablity';
 import { UserAvailability } from '@/types/userAvailability';
 import { useEffect, useState } from 'react';
+
+const initialUserAvailability = {
+  userName: '앙부일구',
+  selectedTimes: new Set<string>(),
+};
 
 export const useUserAvailability = ({
   name,
@@ -15,25 +16,23 @@ export const useUserAvailability = ({
   session: string | null;
 }) => {
   useEffect(() => {
-    const fetchUserAvailableTime = async () => {
-      if (!session) {
-        alert('세션이 없습니다. 다시 시도해주세요.');
-        return;
-      }
-
-      const userAvailableTimeInfo = await getUserAvailableTime(session, name);
-      if (userAvailableTimeInfo.timeSlots.length > 0) {
-        initialUserAvailability.selectedTimes = new Set(userAvailableTimeInfo.timeSlots);
-      }
-    };
-
     fetchUserAvailableTime();
   }, []);
 
-  const initialUserAvailability = {
-    userName: '메이토',
-    selectedTimes: new Set<string>(),
+  const fetchUserAvailableTime = async () => {
+    if (!session) {
+      alert('세션이 없습니다. 다시 시도해주세요.');
+      return;
+    }
+
+    const userAvailableTimeInfo = await getUserAvailableTime(session, name);
+
+    if (userAvailableTimeInfo.timeSlots.length > 0) {
+      const selectedTimes = new Set(userAvailableTimeInfo.timeSlots.map((item) => item.dateTime));
+      setUserAvailability((prev) => ({ ...prev, selectedTimes }));
+    }
   };
+
   const [userAvailability, setUserAvailability] =
     useState<UserAvailability>(initialUserAvailability);
 
