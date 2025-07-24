196c64a88de7b39d725680c1853e47aded7f3dc6
jhan0121 <jhan0121@gmail.com>
2025-07-24 17:32:32 +0900

    style: Room 엔티티 코드 컨벤션 수정

--- backend/src/main/java/com/bether/bether/room/domain/Room.java
+++ backend/src/main/java/com/bether/bether/room/domain/Room.java
@@ -43,7 +43,6 @@ public class Room extends BaseEntity {
     @Column(name = "start_time", nullable = false)
     private LocalTime startTime;
 
-
     @Column(name = "end_time", nullable = false)
     private LocalTime endTime;
 
