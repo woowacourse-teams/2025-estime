6a00d0fda1f88d6287f5551f58059d196afd1359
spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>
2025-07-24 22:11:05 +0900

    링크 복사 추가 (#220)
    * feat: 링크 복사 추가
    
    * feat: CopyLinkButton 컴포넌트에 sessionId prop 추가 및 링크 복사 기능 개선
    
    * fix: CopyLink URL 파싱 방법 수정

---
+++ frontend/src/components/CopyLinkButton/index.tsx
@@ -0,0 +1,22 @@
+import ILink from '@/icons/ILink';
+import Button from '../Button';
+import { useTheme } from '@emotion/react';
+import Text from '@/components/Text';
+
+const CopyLinkButton = ({ sessionId }: { sessionId: string }) => {
+  const handleCopyLink = () => {
+    const link = `${process.env.DOMAIN_URL}/check?id=${sessionId}`;
+    navigator.clipboard.writeText(link);
+  };
+  const theme = useTheme();
+  return (
+    <Button color="primary" size="small" onClick={handleCopyLink}>
+      <ILink color={theme.colors.primary} />
+      <Text variant="body" color="primary">
+        {'링크 복사'}
+      </Text>
+    </Button>
+  );
+};
+
+export default CopyLinkButton;
