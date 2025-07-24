c599b8e25ae3e9d3fa57bae3980dd508ca998315
spoyodevelop <kjy9505@gmail.com>
2025-07-24 15:52:39 +0900

    feat: useUserLogin에 validation 함수 추가

--- frontend/src/hooks/Login /useUserLogin.ts
+++ frontend/src/hooks/Login /useUserLogin.ts
@@ -10,26 +10,30 @@ export function useUserLogin(session: string | null, handleCloseAllModal: () => 
     throw new Error('Session ID is required for user login');
   }
   const [userData, setUserData] = useState<LoginData>({ userid: '', password: '' });
-
+  const [responseUserName, setResponseUserName] = useState<string | null>(null);
   const handleModalLogin = async () => {
-    if (!session) {
-      return;
-    }
     try {
+      if (!session) {
+        throw new Error('세션이 없습니다. 로그인에 실패했습니다.');
+      }
+      if (userData.userid.trim().length === 0 || userData.password.trim().length === 0) {
+        throw new Error('아이디와 비밀번호를 입력해주세요.');
+      }
       const response = await joinPerson(session, {
         name: userData.userid,
         password: userData.password,
       });
-      console.log(response);
+      setResponseUserName(response.name);
       handleCloseAllModal();
     } catch (e) {
       console.error('Error closing suggest modal:', e);
-      alert(`${e}로그인 실패!  꺄약!`);
+      alert(`${e}로그인 실패! 다시 시도해주세요.`);
     }
   };
   return {
     userData,
     setUserData,
     handleModalLogin,
+    responseUserName,
   };
 }
