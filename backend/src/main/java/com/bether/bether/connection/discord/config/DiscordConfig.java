5fe1140ea893e99bfe52085e76a9e48e217bf54b
조재중 <9gle3492@gmail.com>
2025-07-25 10:50:17 +0900

    fix: JDA listener, registrar 등록되도록 수정 (#233)
    - 순환 참조 문제 해결

--- backend/src/main/java/com/bether/bether/connection/discord/config/DiscordConfig.java
+++ backend/src/main/java/com/bether/bether/connection/discord/config/DiscordConfig.java
@@ -1,6 +1,9 @@
 package com.bether.bether.connection.discord.config;
 
+import com.bether.bether.connection.discord.infrastructure.DiscordSlashCommandListener;
+import com.bether.bether.connection.discord.infrastructure.DiscordSlashCommandRegistrar;
 import com.bether.bether.connection.domain.PlatformMessage;
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -9,12 +12,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class DiscordConfig {
 
+    private final DiscordSlashCommandRegistrar registrar;
+    private final DiscordSlashCommandListener listener;
+
     @Bean(destroyMethod = "shutdown")
-    public JDA jda(@Value("${discord.bot.token}") String token) throws InterruptedException {
+    public JDA jda(@Value("${discord.bot.token}") final String token) throws InterruptedException {
         return JDABuilder.createDefault(token)
                 .setActivity(Activity.playing(PlatformMessage.SERVICE_SLOGAN))
+                .addEventListeners(registrar, listener)
                 .build()
                 .awaitReady();
     }
