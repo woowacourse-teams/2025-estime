14e168c4b12abe643b02c8350fd576d1660d473c
hoyyChoi <hoyeon8165@gmail.com>
2025-07-25 15:41:05 +0900

    hotfix: loginModal 아이디 에러 메시지 opacity 조절

--- frontend/src/components/Text/index.tsx
+++ frontend/src/components/Text/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { TypographyKey, ColorsKey } from '@/styles/theme';
 import * as S from './Text.styled';
+import { ComponentProps } from 'react';
 
-export interface TextProps {
+export interface TextProps extends ComponentProps<'span'> {
   variant?: TypographyKey;
   color?: ColorsKey;
+  opacity?: boolean;
   children: React.ReactNode;
 }
 
-const Text = ({ variant = 'body', color = 'gray90', children }: TextProps) => {
+const Text = ({ variant = 'body', color = 'gray90', opacity = true, children }: TextProps) => {
   return (
-    <S.Container variant={variant} color={color}>
+    <S.Container variant={variant} color={color} opacity={opacity}>
       {children}
     </S.Container>
   );
