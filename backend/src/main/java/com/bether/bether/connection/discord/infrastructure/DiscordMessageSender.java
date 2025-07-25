fee92a9a786429df9f6f602d0ea971364b8c1ae9
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-07-25 10:31:26 +0900

    fix: JDA listener, registrar 등록되도록 수정
    - 순환 참조 문제 해결

--- backend/src/main/java/com/bether/bether/connection/discord/infrastructure/DiscordMessageSender.java
+++ backend/src/main/java/com/bether/bether/connection/discord/infrastructure/DiscordMessageSender.java
@@ -1,14 +1,12 @@
 package com.bether.bether.connection.discord.infrastructure;
 
 import com.bether.bether.connection.application.MessageSender;
-import com.bether.bether.connection.application.dto.input.ConnectedRoomCreateMessageInput;
 import com.bether.bether.connection.application.dto.input.ConnectedRoomCreatedMessageInput;
 import com.bether.bether.connection.discord.application.util.DiscordMessageBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import org.springframework.stereotype.Component;
 
@@ -39,16 +37,6 @@ public class DiscordMessageSender implements MessageSender {
 
         final MessageCreateData message = discordMessageBuilder.buildConnectedRoomCreatedMessage(input);
         sendDiscordMessage(channel, message, "Connected room created message");
-    }
-
-    public void sendConnectedRoomCreateEphemeralMessage(
-            final SlashCommandInteractionEvent event,
-            final ConnectedRoomCreateMessageInput input
-    ) {
-        final MessageCreateData messageData = discordMessageBuilder.buildConnectedRoomCreateMessage(input);
-        event.reply(messageData)
-                .setEphemeral(true)
-                .queue();
     }
 
     private TextChannel getChannel(final String channelId) {
