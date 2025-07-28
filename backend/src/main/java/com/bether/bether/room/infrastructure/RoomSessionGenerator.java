20f35c8418484b56b8b640b1132529f898b8cc8b
AppleMint98 <applemint9821@gmail.com>
2025-07-28 20:14:56 +0900

    feat: 유틸클래스 외부 객체생성 불가하도록 접근제어자 수정

--- backend/src/main/java/com/bether/bether/room/infrastructure/RoomSessionGenerator.java
+++ backend/src/main/java/com/bether/bether/room/infrastructure/RoomSessionGenerator.java
@@ -1,7 +1,10 @@
 package com.bether.bether.room.infrastructure;
 
 import com.github.f4b6a3.tsid.TsidCreator;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RoomSessionGenerator {
 
     public static String generateTsid() {
