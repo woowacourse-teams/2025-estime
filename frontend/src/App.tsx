722c3dc2569d5553c788193bf50bcd24b2d3d22e
spoyodevelop <kjy9505@gmail.com>
2025-07-29 18:09:17 +0900

    feat: error boundary와 error page 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import CreateEventPage from './pages/CreateEventPage';
 import CheckEventPage from './pages/CheckEventPage';
 import Layout from './components/Layout';
+import ErrorBoundary from './ErrorBoundary';
+import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   const [isDark, setIsDark] = useState(false);
@@ -16,8 +18,22 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout isDark={isDark} toggleTheme={toggleTheme} />}>
-            <Route index element={<CreateEventPage />} />
-            <Route path="check" element={<CheckEventPage />} />
+            <Route
+              index
+              element={
+                <ErrorBoundary fallback={<ErrorPage />}>
+                  <CreateEventPage />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="check"
+              element={
+                <ErrorBoundary fallback={<ErrorPage />}>
+                  <CheckEventPage />
+                </ErrorBoundary>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
