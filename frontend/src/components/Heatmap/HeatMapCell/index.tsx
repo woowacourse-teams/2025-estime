ee1b434fa681a3f1dbbd73abaedd0c43184add87
spoyodevelop <kjy9505@gmail.com>
2025-08-01 22:19:05 +0900

    feat: HeatmapCell 작성

---
+++ frontend/src/components/Heatmap/HeatMapCell/index.tsx
@@ -0,0 +1,41 @@
+import { getDayOfWeek } from '@/utils/Calendar/getDayofWeek';
+import { getHeatMapCellBackgroundColor } from '@/utils/getBackgroundColor';
+import { useTheme } from '@emotion/react';
+import Flex from '@/components/Layout/Flex';
+import Text from '@/components/Text';
+import * as S from './HeatMapCell.styled';
+import type { DateCellInfo } from '@/hooks/useRoomStatistics';
+
+interface HeatMapCellProps {
+  date: string;
+  timeText: string;
+  roomStatistics: Map<string, DateCellInfo>;
+}
+
+const HeatMapCell = ({ date, timeText, roomStatistics }: HeatMapCellProps) => {
+  const theme = useTheme();
+  const isHeader = timeText === 'Dates';
+  const cellInfo = roomStatistics.get(`${date}T${timeText}`);
+  const weight = isHeader ? 0 : Math.min(cellInfo?.weight ?? 0, 1);
+
+  const backgroundColor = getHeatMapCellBackgroundColor({
+    theme,
+    isHeader,
+    weight,
+  });
+
+  return (
+    <S.Container isHeader={isHeader} backgroundColor={backgroundColor}>
+      {isHeader && (
+        <Text variant="body" color="text">
+          <Flex direction="column" justify="center" align="center">
+            <Text>{date.split('-').slice(1).join('.')}</Text>
+            <Text>({getDayOfWeek(date)})</Text>
+          </Flex>
+        </Text>
+      )}
+    </S.Container>
+  );
+};
+
+export default HeatMapCell;
