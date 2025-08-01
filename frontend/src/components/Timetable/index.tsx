20a41c1a33b2223583c19741c5cee4cb865bedcd
spoyodevelop <kjy9505@gmail.com>
2025-08-01 14:21:09 +0900

    refactor: Timetable 컴포넌트에서 불필요한 props 및 스타일 제거

--- frontend/src/components/Timetable/index.tsx
+++ frontend/src/components/Timetable/index.tsx
@@ -16,18 +16,9 @@ interface TimetableProps {
   };
   availableDates: Set<string>;
   selectedTimes: Field<Set<string>>;
-  userAvailabilitySubmit: () => void;
-  ref: React.RefObject<HTMLDivElement | null>;
 }
 
-const Timetable = ({
-  name,
-  time,
-  availableDates,
-  selectedTimes,
-  userAvailabilitySubmit,
-  ref,
-}: TimetableProps) => {
+const Timetable = ({ time, availableDates, selectedTimes }: TimetableProps) => {
   const { startTime, endTime } = time;
 
   const startTimeInMinutes = startTime.split(':').reduce((acc, time) => acc * 60 + +time, 0);
@@ -46,68 +37,47 @@ const Timetable = ({
   });
 
   return (
-    <Wrapper maxWidth={780} ref={ref}>
-      <S.Container>
-        <Flex direction="column" gap="var(--gap-6)">
-          <S.TimetableHeader>
-            <Flex direction="column" gap="var(--gap-4)">
-              <Text variant="h2" color="text">
-                {name}의 시간 등록하기
-              </Text>
-              <Text variant="body" color="text">
-                가능한 시간을 아래 시간표에서 드래그 해주세요 !
-              </Text>
-            </Flex>
-            <Wrapper maxWidth={100} center={false}>
-              <Button color="primary" onClick={userAvailabilitySubmit}>
-                <Text variant="button" color="text">
-                  저장하기
-                </Text>
-              </Button>
-            </Wrapper>
-          </S.TimetableHeader>
-          <S.TimetableContent onMouseLeave={onMouseLeave}>
-            <S.TimeSlotColumn>
-              {timeList.map(({ timeText, isHour }) => (
-                <S.GridContainer key={timeText}>
-                  {isHour && (
-                    <S.TimeLabel>
-                      <Text variant="body" color="text">
-                        {isHour ? timeText : ''}
-                      </Text>
-                    </S.TimeLabel>
-                  )}
-                </S.GridContainer>
-              ))}
-            </S.TimeSlotColumn>
-            {[...availableDates].map((date) => (
-              <Wrapper key={date} center={false}>
-                {timeList.map(({ timeText }) => (
-                  <S.HeaderCell
-                    key={`${date} ${timeText}`}
-                    onMouseDown={() => onMouseDown(`${date}T${timeText}`)}
-                    onMouseUp={() => onMouseUp()}
-                    onMouseMove={() => onMouseEnter(`${date}T${timeText}`)}
-                    selectedTimes={selectedTimes.value}
-                    date={date}
-                    timeText={timeText}
-                  >
-                    {timeText === 'Dates' && (
-                      <Text variant="body" color="text">
-                        <Flex direction="column" justify="center" align="center">
-                          <Text>{date.split('-').slice(1).join('.')}</Text>
-                          <Text>({getDayOfWeek(date)})</Text>
-                        </Flex>
-                      </Text>
-                    )}
-                  </S.HeaderCell>
-                ))}
-              </Wrapper>
+    <Flex direction="column" gap="var(--gap-6)">
+      <S.TimetableContent onMouseLeave={onMouseLeave}>
+        <S.TimeSlotColumn>
+          {timeList.map(({ timeText, isHour }) => (
+            <S.GridContainer key={timeText}>
+              {isHour && (
+                <S.TimeLabel>
+                  <Text variant="body" color="text">
+                    {isHour ? timeText : ''}
+                  </Text>
+                </S.TimeLabel>
+              )}
+            </S.GridContainer>
+          ))}
+        </S.TimeSlotColumn>
+        {[...availableDates].map((date) => (
+          <Wrapper key={date} center={false}>
+            {timeList.map(({ timeText }) => (
+              <S.HeaderCell
+                key={`${date} ${timeText}`}
+                onMouseDown={() => onMouseDown(`${date}T${timeText}`)}
+                onMouseUp={() => onMouseUp()}
+                onMouseMove={() => onMouseEnter(`${date}T${timeText}`)}
+                selectedTimes={selectedTimes.value}
+                date={date}
+                timeText={timeText}
+              >
+                {timeText === 'Dates' && (
+                  <Text variant="body" color="text">
+                    <Flex direction="column" justify="center" align="center">
+                      <Text>{date.split('-').slice(1).join('.')}</Text>
+                      <Text>({getDayOfWeek(date)})</Text>
+                    </Flex>
+                  </Text>
+                )}
+              </S.HeaderCell>
             ))}
-          </S.TimetableContent>
-        </Flex>
-      </S.Container>
-    </Wrapper>
+          </Wrapper>
+        ))}
+      </S.TimetableContent>
+    </Flex>
   );
 };
 export default Timetable;
