1fa19f771cf5e86f17a8c991b2375eaef77c27d8
Yeonhu Lee <185307804+yeonnhuu@users.noreply.github.com>
2025-08-01 11:35:21 +0900

    Slack/Discord 메시지 URL 생성 리팩터링 (#274)
    * refactor: Connection 관련 URL 생성을 ConnectionUrlBuilder으로 분리
    
    - 생성할 ConnectedRoom URL 메서드 분리
    - 생성된 ConnectedRoom URL 메서드 분리
    
    * style: 코드 정리
    
    - 불필요한 import 제거 및 들여쓰기 수정
    
    * refactor: Slack Slash Command 처리 로직 단순화 및 enum 기반 검증 추가
    
    - PlatformCommand.exists() 메서드 추가로 커맨드 존재 여부 판별 지원
    - 지원하지 않는 커맨드에 대해 early return 방식 적용하여 중첩 제거
    
    * refactor: WebConfigProperties를 직접 주입받도록 ConnectionUrlBuilder 개선
    
    - 생성자에서 baseUrl을 초기화하던 방식 제거
    - WebConfigProperties를 필드로 직접 주입받아 메서드 호출 시 즉시 사용
    
    * refactor: ConnectionUrlBuilder 패키지 위치 및 클래스명 개선
    
    - 패키지 util에서 support로 변경
    - 클래스명 ConnectionUrlBuilder에서 ConnectionUrlSupport로 변경

--- estime-api/src/main/java/com/estime/estime/connection/slack/application/service/SlackService.java
+++ estime-api/src/main/java/com/estime/estime/connection/slack/application/service/SlackService.java
@@ -1,14 +1,13 @@
 package com.estime.estime.connection.slack.application.service;
 
-import com.estime.estime.common.config.WebConfigProperties;
 import com.estime.estime.connection.application.dto.input.ConnectedRoomCreateMessageInput;
 import com.estime.estime.connection.domain.Platform;
 import com.estime.estime.connection.domain.PlatformCommand;
 import com.estime.estime.connection.slack.application.dto.SlackSlashCommandInput;
 import com.estime.estime.connection.slack.infrastructure.SlackMessageSender;
+import com.estime.estime.connection.support.ConnectionUrlHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @RequiredArgsConstructor
@@ -16,25 +15,34 @@ public class SlackService {
 
     private static final String UNSUPPORTED_COMMAND_MESSAGE = "지원하지 않는 아인슈타임 슬랙 커맨드입니다.";
 
-    private final WebConfigProperties webConfigProperties;
+    private final ConnectionUrlHelper connectionUrlHelper;
     private final SlackMessageSender slackMessageSender;
 
     public void handleSlashCommand(final SlackSlashCommandInput input) {
-        if (PlatformCommand.CREATE.getCommandWithSlash().equals(input.command())) {
-            final String url = generateConnectedRoomCreateUrl(input);
-            final ConnectedRoomCreateMessageInput messageInput = new ConnectedRoomCreateMessageInput(url);
-            slackMessageSender.sendConnectedRoomCreateEphemeralMessage(input.channelId(), input.userId(), messageInput);
+        final String command = input.command();
+        if (!PlatformCommand.exists(command)) {
+            slackMessageSender.sendTextMessage(input.channelId(), UNSUPPORTED_COMMAND_MESSAGE);
+            return;
+        }
+
+        if (PlatformCommand.CREATE.getCommandWithSlash().equals(command)) {
+            handleCreateCommand(input);
         } else {
             slackMessageSender.sendTextMessage(input.channelId(), UNSUPPORTED_COMMAND_MESSAGE);
         }
     }
 
-    private String generateConnectedRoomCreateUrl(final SlackSlashCommandInput input) {
-        // TODO refactor with UTIL class
-        return UriComponentsBuilder.fromUriString(webConfigProperties.dev())
-                .queryParam("platform", Platform.SLACK.name())
-                .queryParam("channelId", input.channelId())
-                .build()
-                .toUriString();
+    private void handleCreateCommand(final SlackSlashCommandInput input) {
+        final String shortcut = getConnectedRoomCreateUrl(input);
+        final ConnectedRoomCreateMessageInput messageInput = new ConnectedRoomCreateMessageInput(shortcut);
+        slackMessageSender.sendConnectedRoomCreateEphemeralMessage(
+                input.channelId(),
+                input.userId(),
+                messageInput
+        );
+    }
+
+    private String getConnectedRoomCreateUrl(final SlackSlashCommandInput input) {
+        return connectionUrlHelper.buildConnectedRoomCreateUrl(Platform.SLACK, input.channelId());
     }
 }
