4a3de35d496b04539dd1e3e3c6ea2042a2b81a60
yeonnhuu <yeonnhuu@gmail.com>
2025-08-01 11:31:03 +0900

    refactor: WebConfigProperties를 직접 주입받도록 ConnectionUrlBuilder 개선
    - 생성자에서 baseUrl을 초기화하던 방식 제거
    - WebConfigProperties를 필드로 직접 주입받아 메서드 호출 시 즉시 사용

--- estime-api/src/main/java/com/estime/estime/connection/util/ConnectionUrlBuilder.java
+++ estime-api/src/main/java/com/estime/estime/connection/util/ConnectionUrlBuilder.java
@@ -2,20 +2,18 @@ package com.estime.estime.connection.util;
 
 import com.estime.estime.common.config.WebConfigProperties;
 import com.estime.estime.connection.domain.Platform;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
+@RequiredArgsConstructor
 public class ConnectionUrlBuilder {
 
-    private final String baseUrl;
-
-    public ConnectionUrlBuilder(final WebConfigProperties properties) {
-        this.baseUrl = properties.prod();
-    }
+    private final WebConfigProperties webConfigProperties;
 
     public String buildConnectedRoomCreateUrl(final Platform platform, final String channelId) {
-        return UriComponentsBuilder.fromUriString(baseUrl)
+        return UriComponentsBuilder.fromUriString(webConfigProperties.prod())
                 .queryParam("platform", platform.name())
                 .queryParam("channelId", channelId)
                 .build()
@@ -23,7 +21,7 @@ public class ConnectionUrlBuilder {
     }
 
     public String buildConnectedRoomCreatedUrl(final String session) {
-        return UriComponentsBuilder.fromUriString(baseUrl + "check")
+        return UriComponentsBuilder.fromUriString(webConfigProperties.prod() + "check")
                 .queryParam("id", session)
                 .build()
                 .toUriString();
