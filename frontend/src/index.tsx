69ce45952cf4c39e004b5a8aa94cbfb8f2a9a22a
Deokbin Kim <162319857+Db0111@users.noreply.github.com>
2025-08-02 20:02:59 +0900

    Sentry 초기 세팅 (#295)
    * chore: sentry 설치
    
    * chore: webpack sentry 설정
    
    * feat: Sentry 초기화 설정 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@/styles/index.css';
+import * as Sentry from '@sentry/react';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+
+    sendDefaultPii: true,
+    enableLogs: true,
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
