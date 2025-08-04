3465258ff94a4cc6acaa2347caa0af5bd0097526
hoyyChoi <hoyeon8165@gmail.com>
2025-08-05 02:02:00 +0900

    test: date 관련 test 코드 수정

--- frontend/src/utils/common/DateManager.ts
+++ frontend/src/utils/common/DateManager.ts
@@ -15,6 +15,9 @@ export const DateManager = {
    * @returns 해당 월의 총 일 수
    */
   daysInMonth(year: number, month: number): number {
+    if (month < 0 || month > 11) throw new Error('월은 0부터 11 사이의 값이어야 합니다.');
+    if (year < 0) throw new Error('연도는 0 이상의 값이어야 합니다.');
+
     return new Date(year, month + 1, 0).getDate();
   },
 
