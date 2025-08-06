8d4035ba220747357ce17794142799419e5bbd7d
hoyyChoi <hoyeon8165@gmail.com>
2025-08-06 17:38:35 +0900

    feat: ToastZone 컴포넌트 제작

---
+++ frontend/src/components/Layout/ToastZone/ToastZone.styled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: fixed;
+  top: 100px;
+  left: 50%;
+  z-index: 10;
+`;
+
+export const Wrapper = styled.div<{ index: number }>`
+  width: fit-content;
+  position: absolute;
+  top: ${({ index }) => `${index * 20}px`};
+  transform: translateX(-50%);
+`;
---
+++ frontend/src/components/Layout/ToastZone/index.tsx
@@ -0,0 +1,22 @@
+import { type ToastStateType } from '@/types/toastType';
+import * as S from './ToastZone.styled';
+import Toast from '@/components/Toast';
+
+interface ToastZoneProps {
+  toasts: ToastStateType[];
+  removeToast: (id: string) => void;
+}
+
+const ToastZone = ({ toasts, removeToast }: ToastZoneProps) => {
+  return (
+    <S.Container>
+      {toasts.map(({ id, type, message }, index) => (
+        <S.Wrapper index={index} key={id}>
+          <Toast id={id} type={type} message={message} onClose={removeToast} />
+        </S.Wrapper>
+      ))}
+    </S.Container>
+  );
+};
+
+export default ToastZone;
