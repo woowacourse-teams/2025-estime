c2630999ee4385873711b75948bc9acd081c3a58
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-08-06 15:52:59 +0900

    test: 도메인 테스트, 서비스 테스트 작성
    - 리팩터링 후 부족했던 테스트 채움.

---
+++ estime-api/src/test/java/com/estime/room/domain/vo/DateSlotTest.java
@@ -0,0 +1,61 @@
+package com.estime.room.domain.vo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateSlotTest {
+
+    @Test
+    @DisplayName("정적 팩토리 메소드 from으로 DateSlot을 생성한다.")
+    void from() {
+        // given
+        final LocalDate now = LocalDate.now();
+
+        // when
+        final DateSlot dateSlot = DateSlot.from(now);
+
+        // then
+        assertThat(dateSlot.getStartAt()).isEqualTo(now);
+    }
+
+    @Test
+    @DisplayName("from 메소드에 null을 전달하면 예외가 발생한다.")
+    void from_withNull() {
+        // given
+        final LocalDate nullDate = null;
+
+        // when & then
+        assertThatThrownBy(() -> DateSlot.from(nullDate))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("startAt cannot be null");
+    }
+
+    @Test
+    @DisplayName("과거 날짜로 DateSlot을 생성하면 예외가 발생한다.")
+    void from_withPastDate() {
+        // given
+        final LocalDate pastDate = LocalDate.now().minusDays(1);
+
+        // when & then
+        assertThatThrownBy(() -> DateSlot.from(pastDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("dateSlot cannot be past date: " + pastDate);
+    }
+
+    @Test
+    @DisplayName("compareTo 메소드로 날짜를 비교한다.")
+    void compareTo() {
+        // given
+        final DateSlot today = DateSlot.from(LocalDate.now());
+        final DateSlot tomorrow = DateSlot.from(LocalDate.now().plusDays(1));
+
+        // when & then
+        assertThat(today.compareTo(tomorrow)).isNegative();
+        assertThat(tomorrow.compareTo(today)).isPositive();
+        assertThat(today.compareTo(today)).isZero();
+    }
+}
