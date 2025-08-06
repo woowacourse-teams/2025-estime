e10a97decb86b4679801b00eaa8607fe00b744b3
thgml05 <aisohii@naver.com>
2025-08-06 23:29:45 +0900

    refactor: sentry init 옵션 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,9 +7,9 @@ import * as Sentry from '@sentry/react';
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-
     sendDefaultPii: true,
     enableLogs: true,
+    debug: true,
   });
 }
 
