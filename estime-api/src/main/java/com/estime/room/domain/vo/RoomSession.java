188c8795ac814c42df556dcc0af1ad2b27d58faa
AppleMint98 <applemint9821@gmail.com>
2025-08-06 16:40:36 +0900

    refactor: 값 객체 생성로직 축소 & 불필요한 유효성 검증 제거

--- estime-api/src/main/java/com/estime/room/domain/vo/RoomSession.java
+++ estime-api/src/main/java/com/estime/room/domain/vo/RoomSession.java
@@ -3,7 +3,6 @@ package com.estime.room.domain.vo;
 import com.estime.room.infrastructure.RoomSessionGenerator;
 import com.github.f4b6a3.tsid.Tsid;
 import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,36 +13,17 @@ public class RoomSession implements Serializable {
 
     private final Tsid tsid;
 
-    public RoomSession(final Tsid tsid) {
+    private RoomSession(final Tsid tsid) {
         Objects.requireNonNull(tsid, "tsid cannot be null");
         this.tsid = tsid;
     }
 
-    public static RoomSession of(final String tsid) {
-        validate(tsid);
-        return new RoomSession(Tsid.from(tsid));
+    public static RoomSession from(final Tsid tsid) {
+        return new RoomSession(tsid);
     }
 
     public static RoomSession generate() {
         final Tsid tsid = RoomSessionGenerator.generateTsid();
-        validate(tsid.toString());
         return new RoomSession(tsid);
-    }
-
-    private static void validate(final String tsid) {
-        validateLength(tsid);
-        validateASCII(tsid);
-    }
-
-    private static void validateLength(final String tsid) {
-        if (tsid.length() != 13) {
-            throw new IllegalArgumentException("session id must be exactly 13 characters.");
-        }
-    }
-
-    private static void validateASCII(final String tsid) {
-        if (!StandardCharsets.US_ASCII.newEncoder().canEncode(tsid)) {
-            throw new IllegalArgumentException("session id must be ASCII characters only.");
-        }
     }
 }
