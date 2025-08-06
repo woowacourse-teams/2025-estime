1aa4aff7bcd057043a71b5b77031ac126955d69b
AppleMint98 <applemint9821@gmail.com>
2025-08-06 17:47:17 +0900

    refactor: 검증 위치 수정

--- estime-api/src/main/java/com/estime/room/domain/vo/RoomSession.java
+++ estime-api/src/main/java/com/estime/room/domain/vo/RoomSession.java
@@ -4,26 +4,29 @@ import com.estime.room.infrastructure.RoomSessionGenerator;
 import com.github.f4b6a3.tsid.Tsid;
 import java.io.Serializable;
 import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @EqualsAndHashCode
 public class RoomSession implements Serializable {
 
     private final Tsid tsid;
 
-    private RoomSession(final Tsid tsid) {
-        Objects.requireNonNull(tsid, "tsid cannot be null");
-        this.tsid = tsid;
-    }
-
     public static RoomSession from(final Tsid tsid) {
+        validate(tsid);
         return new RoomSession(tsid);
     }
 
     public static RoomSession generate() {
         final Tsid tsid = RoomSessionGenerator.generateTsid();
         return new RoomSession(tsid);
+    }
+
+    private static void validate(final Tsid tsid) {
+        Objects.requireNonNull(tsid, "tsid cannot be null");
     }
 }
