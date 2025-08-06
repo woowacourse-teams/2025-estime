417cf935612b47306fe714682ffbff677e846060
hoyyChoi <hoyeon8165@gmail.com>
2025-08-06 19:28:24 +0900

    feat: Toast 컴포넌트 title 데이터 추가

--- frontend/src/components/Toast/index.tsx
+++ frontend/src/components/Toast/index.tsx
@@ -4,6 +4,12 @@ import * as S from './Toast.styled';
 import Text from '../Text';
 import { type ToastType } from '@/types/toastType';
 
+const TOAST_TITLE = {
+  success: '성공',
+  error: '오류',
+  warning: '위험',
+};
+
 export type ToastPhase = 'idle' | 'visible' | 'hidden';
 
 interface ToastProps {
@@ -36,7 +42,7 @@ const Toast = ({ id, type, message, onClose }: ToastProps) => {
       <S.Container phase={phase}>
         <S.Header>
           <StatusButton type={type} />
-          <Text color={`${type}Text`}>{type}</Text>
+          <Text color={`${type}Text`}>{TOAST_TITLE[type]}</Text>
         </S.Header>
         <S.Body>
           <Text color={`${type}Text`}>{message}</Text>
