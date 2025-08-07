a8a298e06589893cbfa8751fde12e5bb918c10eb
AppleMint98 <applemint9821@gmail.com>
2025-08-07 19:20:51 +0900

    refactor: 미사용 생성자 제거

--- estime-api/src/main/java/com/estime/common/exception/BaseException.java
+++ estime-api/src/main/java/com/estime/common/exception/BaseException.java
@@ -9,30 +9,9 @@ public abstract class BaseException extends RuntimeException {
     private final String userMessage;
 
     protected BaseException(final String logMessage,
-                            final String userMessage,
-                            final Throwable cause) {
-        super(logMessage, cause);
-        this.logMessage = logMessage;
-        this.userMessage = userMessage;
-    }
-
-    protected BaseException(final String logMessage,
-                            final Throwable cause) {
-        super(logMessage, cause);
-        this.logMessage = logMessage;
-        this.userMessage = "";
-    }
-
-    protected BaseException(final String logMessage,
                             final String userMessage) {
         super(logMessage);
         this.logMessage = logMessage;
         this.userMessage = userMessage;
-    }
-
-    protected BaseException(final String logMessage) {
-        super(logMessage);
-        this.logMessage = logMessage;
-        this.userMessage = "";
     }
 }
