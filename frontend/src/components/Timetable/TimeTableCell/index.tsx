591045039b9d57edc2b10966075657826a99b3a9
spoyodevelop <kjy9505@gmail.com>
2025-08-07 14:49:01 +0900

    fix: unused import 삭제

--- frontend/src/components/Timetable/TimeTableCell/index.tsx
+++ frontend/src/components/Timetable/TimeTableCell/index.tsx
@@ -1,9 +1,6 @@
 import * as S from './TimeTableCell.styled';
 import type { Field } from '@/types/field';
-import Text from '@/components/Text';
-import Flex from '@/components/Layout/Flex';
 import { getHeaderCellBackgroundColor } from '@/utils/getBackgroundColor';
-import { DateManager } from '@/utils/common/DateManager';
 import { useTheme } from '@emotion/react';
 
 interface TimeTableCellProps {
