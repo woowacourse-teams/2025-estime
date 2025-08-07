81b8d14e402baccff5e9436bed7c7a3968306837
AppleMint98 <applemint9821@gmail.com>
2025-08-07 19:20:51 +0900

    refactor: 미사용 생성자 제거

--- estime-api/src/main/java/com/estime/common/exception/application/ApplicationException.java
+++ estime-api/src/main/java/com/estime/common/exception/application/ApplicationException.java
@@ -4,20 +4,8 @@ import com.estime.common.exception.BaseException;
 
 public abstract class ApplicationException extends BaseException {
 
-    protected ApplicationException(final String logMessage, final String userMessage, final Throwable cause) {
-        super(logMessage, userMessage, cause);
-    }
-
-    protected ApplicationException(final String logMessage, final Throwable cause) {
-        super(logMessage, cause);
-    }
-
     protected ApplicationException(final String logMessage, final String userMessage) {
         super(logMessage, userMessage);
-    }
-
-    protected ApplicationException(final String logMessage) {
-        super(logMessage);
     }
 }
 
--- estime-api/src/main/java/com/estime/common/exception/domain/DomainException.java
+++ estime-api/src/main/java/com/estime/common/exception/domain/DomainException.java
@@ -4,19 +4,7 @@ import com.estime.common.exception.BaseException;
 
 public abstract class DomainException extends BaseException {
 
-    protected DomainException(final String logMessage, final String userMessage, final Throwable cause) {
-        super(logMessage, userMessage, cause);
-    }
-
-    protected DomainException(final String logMessage, final Throwable cause) {
-        super(logMessage, cause);
-    }
-
     protected DomainException(final String logMessage, final String userMessage) {
         super(logMessage, userMessage);
-    }
-
-    protected DomainException(final String logMessage) {
-        super(logMessage);
     }
 }
