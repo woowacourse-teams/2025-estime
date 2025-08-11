c3fbdd501e2579c017eaacf2ddbbcd3970137e13
spoyodevelop <kjy9505@gmail.com>
2025-08-11 16:49:49 +0900

    chore: 크레딧 작성

---
+++ frontend/src/constants/credits.ts
@@ -0,0 +1,51 @@
+import dummyImage from '@/assets/images/dummy.jpg';
+export const credits = [
+  {
+    name: '해삐',
+    role: 'Frontend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '메이토',
+    role: 'Frontend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '호이초이',
+    role: 'Frontend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '마빈',
+    role: 'Frontend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '리버',
+    role: 'Backend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '제프리',
+    role: 'Backend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '강산',
+    role: 'Backend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+  {
+    name: '플린트',
+    role: 'Backend Developer',
+    github: '',
+    imageUrl: dummyImage,
+  },
+] as const;
