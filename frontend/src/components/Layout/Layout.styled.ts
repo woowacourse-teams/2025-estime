2c7399f653b7ea2b0d8a76323401a50febfb69b2
spoyodevelop <kjy9505@gmail.com>
2025-08-11 16:49:22 +0900

    feat: 레이아웃에 푸터 병합

--- frontend/src/components/Layout/Layout.styled.ts
+++ frontend/src/components/Layout/Layout.styled.ts
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   width: 100vw;
-  min-height: calc(100vh - 5rem);
+  min-height: calc(100vh - 5rem - 100px);
   background-color: ${({ theme }) => ` ${theme.colors.gray10}`};
 `;
