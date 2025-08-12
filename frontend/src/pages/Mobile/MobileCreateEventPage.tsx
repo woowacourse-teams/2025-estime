d596d3e18566e2648ca0da7bc62d2255262f8ddd
thgml05 <aisohii@naver.com>
2025-08-12 20:11:59 +0900

    feat: 모바일 첫 페이지 구현

---
+++ frontend/src/pages/Mobile/MobileCreateEventPage.tsx
@@ -0,0 +1,38 @@
+import IEstime from '@/icons/IEstime';
+import Logo from '@/icons/Logo';
+import Button from '@/components/Button';
+import Text from '@/components/Text';
+import Wrapper from '@/components/Layout/Wrapper';
+import { ButtonWrapper, LogoWrapper } from '../styles/MobileCreateEventPage.styled';
+import Flex from '@/components/Layout/Flex';
+
+const MobileCreatePage = () => {
+  return (
+    <Wrapper
+      paddingTop="var(--padding-11)"
+      paddingLeft="var(--padding-7)"
+      paddingRight="var(--padding-7)"
+    >
+      <Flex direction="column" align="center" justify="space-between" gap="var(--gap-11)">
+        <IEstime />
+        <LogoWrapper>
+          <Logo />
+        </LogoWrapper>
+        <ButtonWrapper>
+          <Button color="primary" selected={true}>
+            <Text variant="button" color="background">
+              방 만들기
+            </Text>
+          </Button>
+          <Button color="primary" selected={true}>
+            <Text variant="button" color="background">
+              방 참가하기
+            </Text>
+          </Button>
+        </ButtonWrapper>
+      </Flex>
+    </Wrapper>
+  );
+};
+
+export default MobileCreatePage;
