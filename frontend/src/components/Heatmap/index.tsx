15f1b7cc859fa2edaee166d25fbf5f972e1cd378
spoyodevelop <kjy9505@gmail.com>
2025-08-12 18:24:57 +0900

    feat: Heatmap 컴포넌트에 모바일 지원 추가 및 툴팁 로직 개선

--- frontend/src/components/Heatmap/index.tsx
+++ frontend/src/components/Heatmap/index.tsx
@@ -5,34 +5,59 @@ import * as S from './Heatmap.styled';
 import HeatMapDataCell from './HeatMapDataCell';
 import type { DateCellInfo } from '@/hooks/useRoomStatistics';
 import TimeTableDay from '@/components/Timetable/TimeTableDay';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import TableTooltip from '../TableTooltip';
-import Flex from '../Layout/Flex';
-import IPerson from '@/icons/IPerson';
 import { useHoverTooltip } from '@/hooks/useHoverTooltip';
+
 interface HeatmapProps {
   dateTimeSlots: string[];
   availableDates: Set<string>;
   roomStatistics: Map<string, DateCellInfo>;
+  isMobile: boolean;
 }
 export interface TooltipInfo {
   date: string;
   timeText: string;
   participantList: string[];
 }
-const Heatmap = ({ dateTimeSlots, availableDates, roomStatistics }: HeatmapProps) => {
+const Heatmap = ({
+  dateTimeSlots,
+  availableDates,
+  roomStatistics,
+  isMobile = true,
+}: HeatmapProps) => {
   const [tooltipInfo, setTooltipInfo] = useState<TooltipInfo | null>(null);
 
-  const { position, onEnter, onLeave } = useHoverTooltip();
+  const { position, onEnter, onLeave } = useHoverTooltip(isMobile);
 
   const handleMouseEnter = (tooltipInfo: TooltipInfo, event: React.PointerEvent) => {
+    if (isMobile) return;
     setTooltipInfo(tooltipInfo);
     onEnter(event);
   };
 
   const handleMouseLeave = () => {
+    if (isMobile) return;
     setTooltipInfo(null);
     onLeave();
+  };
+
+  // 모바일 클릭 핸들러
+  const handleMobileClick = useCallback(
+    (tooltipInfo: TooltipInfo) => {
+      if (!isMobile) return;
+      setTooltipInfo(tooltipInfo);
+    },
+    [isMobile]
+  );
+
+  const isClicked = (date: string, timeText: string) => {
+    return (
+      isMobile &&
+      tooltipInfo?.date === date &&
+      tooltipInfo?.timeText === timeText &&
+      !!tooltipInfo.participantList.length
+    );
   };
   return (
     <S.HeatMapContent>
@@ -60,19 +85,20 @@ const Heatmap = ({ dateTimeSlots, availableDates, roomStatistics }: HeatmapProps
               roomStatistics={roomStatistics}
               onEnter={handleMouseEnter}
               onLeave={handleMouseLeave}
+              onMobileClick={handleMobileClick}
+              isClicked={isClicked(date, timeText)}
             />
           ))}
         </Wrapper>
       ))}
-      {!!tooltipInfo?.participantList?.length && (
-        <TableTooltip position={position}>
-          <Flex direction="row" gap="var(--gap-4)" align="center">
-            <IPerson />
-            <Text variant="body" color="text">
-              {tooltipInfo.participantList.join(', ')} 가능!
-            </Text>
-          </Flex>
-        </TableTooltip>
+      {!isMobile && !!tooltipInfo?.participantList?.length && (
+        <TableTooltip
+          position={position}
+          positioning="mouse-follow"
+          participantList={tooltipInfo.participantList}
+          date={tooltipInfo.date}
+          timeText={tooltipInfo.timeText}
+        />
       )}
     </S.HeatMapContent>
   );
