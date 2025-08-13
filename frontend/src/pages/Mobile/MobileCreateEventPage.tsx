4d5d93101a11c670f9265dec72f9cf2ce9eda8d2
deokbin <deokbin@naver.com>
2025-08-13 12:43:07 +0900

    feat: MobileCreateEventPage 추가

--- frontend/src/pages/Mobile/MobileCreateEventPage.tsx
+++ frontend/src/pages/Mobile/MobileCreateEventPage.tsx
@@ -1,17 +1,14 @@
 import Flex from '@/components/Layout/Flex';
 import Wrapper from '@/components/Layout/Wrapper';
-import BasicSettings from '@/components/Sections/BasicSettings';
 import Button from '@/components/Button';
 import Text from '@/components/Text';
 import { useNavigate } from 'react-router';
 import useCreateRoom from '@/hooks/useCreateRoom';
-import { useTheme } from '@emotion/react';
 import useShakeAnimation from '@/hooks/CreateRoom/useShakeAnimation';
 import { useRef, useState } from 'react';
 import { useToastContext } from '@/contexts/ToastContext';
-import * as S from './MobileCreateEventPage.styled';
-import Calender from '@/components/Calendar';
-import { useDateSelection } from '@/hooks/Calendar/useDateSelection';
+import MobileBasicSettings from '@/components/Mobile/MobileBasicSettings';
+import MobileCalendarSettings from '@/components/Mobile/MobileCalendarSettings';
 
 const MobileCreateEventPage = () => {
   //  Todo: funnel 반영하기 (임의로 step 정보)
@@ -35,31 +32,35 @@ const MobileCreateEventPage = () => {
 
   const { addToast } = useToastContext();
 
-  const handleCreateRoom = async () => {
-    if (!isCalendarReady && !isBasicReady) {
+  const handleNextStep = () => {
+    if (step === 2 && !isCalendarReady) {
       addToast({
         type: 'warning',
-        message: '날짜와 약속 정보를 입력해주세요.',
+        message: '날짜를 선택해주세요.',
       });
       showValidation.current = true;
       handleShouldShake();
       return;
     }
-    if (!isCalendarReady) {
-      addToast({
-        type: 'warning',
-        message: '날짜를 선택해주세요.',
-      });
-      return;
-    }
-    if (!isBasicReady) {
-      addToast({
-        type: 'warning',
-        message: '약속 정보를 입력해주세요.',
-      });
+
+    if (step === 3) {
+      if (!isBasicReady) {
+        addToast({
+          type: 'warning',
+          message: '약속 정보를 입력해주세요.',
+        });
+        showValidation.current = true;
+        handleShouldShake();
+        return;
+      }
+      handleCreateRoom();
       return;
     }
 
+    setStep((prev) => prev + 1);
+  };
+
+  const handleCreateRoom = async () => {
     const session = await roomInfoSubmit();
     addToast({
       type: 'success',
@@ -71,21 +72,8 @@ const MobileCreateEventPage = () => {
     }
   };
 
-  const today = new Date();
-  const dateSelection = useDateSelection({
-    selectedDates: availableDateSlots.value,
-    setSelectedDates: availableDateSlots.set,
-    today,
-  });
-  const mouseHandlers = {
-    onMouseDown: dateSelection.onMouseDown,
-    onMouseEnter: dateSelection.onMouseEnter,
-    onMouseUp: dateSelection.onMouseUp,
-    onMouseLeave: dateSelection.onMouseLeave,
-  };
-
   return (
-    <Wrapper maxWidth={430} padding="var(--padding-8)">
+    <Wrapper maxWidth={430} padding="var(--padding-6)" borderRadius="var(--radius-4)">
       {step === 1 && (
         <Flex direction="column" gap="var(--gap-6)" align="center" justify="center">
           <Text variant="h3" color="primary">
@@ -106,14 +94,41 @@ const MobileCreateEventPage = () => {
       )}
       {step === 2 && (
         <Flex direction="column" justify="space-between" gap="var(--gap-8)">
-          <S.TextWrapper>
-            <Text variant="h3">날짜 선택</Text>
-            <Text variant="h4">가능한 날짜를 드래그해서 선택해주세요!</Text>
-          </S.TextWrapper>
-          <Calender
-            today={today}
-            selectedDates={availableDateSlots.value}
-            mouseHandlers={mouseHandlers}
+          <MobileCalendarSettings
+            availableDateSlots={availableDateSlots}
+            isValid={!showValidation.current || isCalendarReady}
+            shouldShake={shouldShake}
+          />
+
+          <Flex justify="space-between" gap="var(--gap-4)">
+            <Button
+              color="primary"
+              selected={false}
+              size="large"
+              // 이전 단계 돌아가야 함
+              onClick={() => setStep((prev) => prev - 1)}
+            >
+              <Text variant="button" color="primary">
+                이전
+              </Text>
+            </Button>
+            <Button color="primary" selected={true} size="large" onClick={handleNextStep}>
+              <Text variant="button" color="background">
+                다음
+              </Text>
+            </Button>
+          </Flex>
+        </Flex>
+      )}
+
+      {step === 3 && (
+        <Flex direction="column" justify="space-between" gap="var(--gap-8)">
+          <MobileBasicSettings
+            title={title}
+            time={time}
+            deadline={deadline}
+            isValid={!showValidation.current || isBasicReady}
+            shouldShake={shouldShake}
           />
           <Flex justify="space-between" gap="var(--gap-4)">
             <Button
@@ -131,42 +146,7 @@ const MobileCreateEventPage = () => {
               color="primary"
               selected={true}
               size="large"
-              onClick={() => setStep((prev) => prev + 1)}
-            >
-              <Text variant="button" color="background">
-                다음
-              </Text>
-            </Button>
-          </Flex>
-        </Flex>
-      )}
-
-      {step === 3 && (
-        <Flex direction="column" justify="space-between" gap="var(--gap-8)">
-          <BasicSettings
-            title={title}
-            time={time}
-            deadline={deadline}
-            isValid={!showValidation.current || isBasicReady}
-            shouldShake={shouldShake}
-          />
-          <Flex justify="flex-end">
-            <Button
-              color="primary"
-              selected={false}
-              size="small"
-              // 이전 단계 돌아가야 함
-              onClick={() => setStep((prev) => prev - 1)}
-            >
-              <Text variant="button" color="background">
-                이전
-              </Text>
-            </Button>
-            <Button
-              color="primary"
-              selected={true}
-              size="small"
-              onClick={handleCreateRoom}
+              onClick={handleNextStep}
               data-ga-id="create-event-button"
             >
               <Text variant="button" color="background">
