2af061d01bd71c124c2efa25aee6f4b20730f08d
thgml05 <76714311+thgml05@users.noreply.github.com>
2025-08-13 17:52:49 +0900

    모바일 시작 페이지 작업 (#387)
    * design: Header 양옆 padding 적용
    
    * refactor: Header Logo 제거
    
    * feat: icon 추가
    
    * feat: 모바일 첫 페이지 구현
    
    * design: IEstime, Logo 색상 theme 사용하도록 수정
    
    * refactor: IEstime 로고 및 아이콘 네이밍 변경
    
    * refactor: 헤더 로고 svg로 변경

---
+++ frontend/src/pages/Mobile/MobileCreateEventPage.tsx
@@ -0,0 +1,41 @@
+import IEstimeLogo from '@/icons/IEstimeLogo';
+import IEstimeIcon from '@/icons/IEstimeIcon';
+import Button from '@/components/Button';
+import Text from '@/components/Text';
+import Wrapper from '@/components/Layout/Wrapper';
+import * as S from '../styles/MobileCreateEventPage.styled';
+import Flex from '@/components/Layout/Flex';
+import { useTheme } from '@emotion/react';
+
+const MobileCreatePage = () => {
+  const theme = useTheme();
+
+  return (
+    <Wrapper
+      paddingTop="var(--padding-11)"
+      paddingLeft="var(--padding-7)"
+      paddingRight="var(--padding-7)"
+    >
+      <Flex direction="column" align="center" justify="space-between" gap="var(--gap-11)">
+        <IEstimeLogo color={theme.colors.primary} />
+        <S.LogoWrapper>
+          <IEstimeIcon color={theme.colors.primary} />
+        </S.LogoWrapper>
+        <S.ButtonWrapper>
+          <Button color="primary" selected={true}>
+            <Text variant="button" color="background">
+              방 만들기
+            </Text>
+          </Button>
+          <Button color="primary" selected={true}>
+            <Text variant="button" color="background">
+              방 참가하기
+            </Text>
+          </Button>
+        </S.ButtonWrapper>
+      </Flex>
+    </Wrapper>
+  );
+};
+
+export default MobileCreatePage;
---
+++ frontend/src/pages/styles/MobileCreateEventPage.styled.ts
@@ -0,0 +1,23 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const rotateAnimation = keyframes`
+  0%{
+     transform: rotate(0deg);
+    }
+ 100%{
+     transform: rotate(360deg);
+    }
+`;
+
+export const LogoWrapper = styled.div`
+  width: 100%;
+  animation: ${rotateAnimation} 15s linear infinite;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap-4);
+`;
