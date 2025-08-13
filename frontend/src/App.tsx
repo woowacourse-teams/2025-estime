d03eb57ba643ebd847c3d40883fca6ab62b1527a
deokbin <deokbin@naver.com>
2025-08-13 12:43:21 +0900

    feat: isMobile에 따른 라우팅 분기 처리 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,36 @@
 import { useState } from 'react';
 import { DARK_THEME, LIGHT_THEME } from './styles/theme';
 import { ThemeProvider } from '@emotion/react';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import CreateEventPage from './pages/CreateEventPage';
-import CheckEventPage from './pages/CheckEventPage';
-import Layout from './components/Layout';
+import { BrowserRouter } from 'react-router';
+import DesktopRoutes from './routes/DesktopRoutes';
+import MobileRoutes from './routes/MobileRoutes';
+import { useMobileContext } from './contexts/MobileContext';
+import MobileProvider from './providers/MobileProvider';
 
 const App = () => {
   const [isDark, setIsDark] = useState(false);
   const toggleTheme = () => {
     setIsDark((prev) => !prev);
   };
+
   return (
     <ThemeProvider theme={isDark ? DARK_THEME : LIGHT_THEME}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout isDark={isDark} toggleTheme={toggleTheme} />}>
-            <Route index element={<CreateEventPage />} />
-            <Route path="check" element={<CheckEventPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <MobileProvider>
+        <BrowserRouter>
+          <RoutesWrapper isDark={isDark} toggleTheme={toggleTheme} />
+        </BrowserRouter>
+      </MobileProvider>
     </ThemeProvider>
+  );
+};
+
+const RoutesWrapper = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: () => void }) => {
+  const { isMobile } = useMobileContext();
+
+  return isMobile ? (
+    <MobileRoutes isDark={isDark} toggleTheme={toggleTheme} />
+  ) : (
+    <DesktopRoutes isDark={isDark} toggleTheme={toggleTheme} />
   );
 };
 
