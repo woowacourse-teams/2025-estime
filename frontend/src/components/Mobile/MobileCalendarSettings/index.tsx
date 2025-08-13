d7b3de6ecb27bcf4218b201a1ec5cc2dca932e52
deokbin <deokbin@naver.com>
2025-08-13 12:42:53 +0900

    feat: MobileCalendarSettings 컴포넌트 추가

---
+++ frontend/src/components/Mobile/MobileCalendarSettings/index.tsx
@@ -0,0 +1,46 @@
+import Calender from '@/components/Calendar';
+import * as S from './MobileCalendarSettings.styled';
+import { useDateSelection } from '@/hooks/Calendar/useDateSelection';
+import Text from '@/components/Text';
+import { Field } from '@/types/field';
+
+type CalendarSettingsProps = {
+  availableDateSlots: Field<Set<string>>;
+  isValid: boolean;
+  shouldShake: boolean;
+};
+
+const MobileCalendarSettings = ({
+  availableDateSlots,
+  isValid,
+  shouldShake,
+}: CalendarSettingsProps) => {
+  const today = new Date();
+  const dateSelection = useDateSelection({
+    selectedDates: availableDateSlots.value,
+    setSelectedDates: availableDateSlots.set,
+    today,
+  });
+  const mouseHandlers = {
+    onMouseDown: dateSelection.onMouseDown,
+    onMouseEnter: dateSelection.onMouseEnter,
+    onMouseUp: dateSelection.onMouseUp,
+    onMouseLeave: dateSelection.onMouseLeave,
+  };
+
+  return (
+    <S.Container isValid={isValid} shouldShake={shouldShake}>
+      <S.TextWrapper>
+        <Text variant="h3">날짜 선택</Text>
+        <Text variant="h4">가능한 날짜를 드래그해서 선택해주세요!</Text>
+      </S.TextWrapper>
+      <Calender
+        today={today}
+        selectedDates={availableDateSlots.value}
+        mouseHandlers={mouseHandlers}
+      />
+    </S.Container>
+  );
+};
+
+export default MobileCalendarSettings;
