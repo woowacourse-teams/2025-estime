88aa1dfcb77e8e8300c8dd7ce59c322c5cf36b63
AppleMint98 <applemint9821@gmail.com>
2025-08-13 16:15:28 +0900

    feat: 데드라인 도메인 예외 구현

---
+++ estime-api/src/main/java/com/estime/common/exception/domain/DeadlineOverdueException.java
@@ -0,0 +1,22 @@
+package com.estime.common.exception.domain;
+
+import com.estime.common.DomainTerm;
+import com.estime.common.exception.util.ExceptionMessageFormatter;
+
+public class DeadlineOverdueException extends DomainException {
+
+    public DeadlineOverdueException(final DomainTerm term, final Object... params) {
+        super(
+                buildLogMessage(term, params),
+                buildUserMessage()
+        );
+    }
+
+    private static String buildLogMessage(final DomainTerm term, final Object... params) {
+        return ExceptionMessageFormatter.format("Deadline exceeded for %s".formatted(term), params);
+    }
+
+    private static String buildUserMessage() {
+        return "마감시간이 경과되어 더이상 수정하실 수 없습니다. 결과를 확인해주세요.";
+    }
+}
