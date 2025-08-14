577bc146ed05e5ce16e4ad25f91ff9798e38fdff
spoyodevelop <kjy9505@gmail.com>
2025-08-14 13:40:58 +0900

    fix: 모바일 대응 분기 처리

--- frontend/src/hooks/useTooltipBehavior.ts
+++ frontend/src/hooks/useTooltipBehavior.ts
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react';
 import { useHoverTooltip } from './useHoverTooltip';
+import { useTheme } from '@emotion/react';
 
 export interface TooltipInfo {
   date: string;
@@ -10,25 +11,25 @@ export interface TooltipInfo {
 export default function useTooltipBehavior() {
   const [tooltipInfo, setTooltipInfo] = useState<TooltipInfo | null>(null);
   const { position, onEnter, onMobileTap, onLeave } = useHoverTooltip();
+  const theme = useTheme();
 
-  const handlePointerEnter = useCallback(
-    (tooltipInfo: TooltipInfo, event: React.PointerEvent) => {
-      setTooltipInfo(tooltipInfo);
-      onEnter(event);
-    },
-    [tooltipInfo]
-  );
+  const handlePointerEnter = useCallback((tooltipInfo: TooltipInfo, event: React.PointerEvent) => {
+    setTooltipInfo(tooltipInfo);
+    onEnter(event);
+  }, []);
 
   const handlePointerLeave = useCallback(() => {
+    if (theme.isMobile) return;
     onLeave();
     setTooltipInfo(null);
-  }, []);
+  }, [theme.isMobile]);
 
   const handleMobileTap = useCallback(
     (element: HTMLDivElement) => {
+      if (!theme.isMobile) return;
       onMobileTap(element);
     },
-    [tooltipInfo]
+    [theme.isMobile]
   );
 
   const isTooltipVisible: boolean = !!tooltipInfo?.participantList.length;
