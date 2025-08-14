3173b47eaf6306500fd6a9aa347af12f93fbf3d1
thgml05 <aisohii@naver.com>
2025-08-14 12:56:42 +0900

    refactor: checkEventPage 마감시간 정각으로 표시되도록 수정

--- frontend/src/components/CheckEventPageHeader/index.tsx
+++ frontend/src/components/CheckEventPageHeader/index.tsx
@@ -4,6 +4,7 @@ import Text from '@/components/Text';
 import { useTheme } from '@emotion/react';
 import type { RoomInfo } from '@/types/roomInfo';
 import CopyLinkButton from '../CopyLinkButton';
+import { TimeManager } from '@/utils/common/TimeManager';
 
 type CheckEventPageHeaderProps = Pick<RoomInfo, 'deadline' | 'title'> & {
   roomSession: string;
@@ -11,6 +12,9 @@ type CheckEventPageHeaderProps = Pick<RoomInfo, 'deadline' | 'title'> & {
 
 const CheckEventPageHeader = ({ deadline, title, roomSession }: CheckEventPageHeaderProps) => {
   const theme = useTheme();
+
+  const deadLineTime = TimeManager.addMinutes(deadline.time, 30);
+
   return (
     <Flex gap="var(--gap-5)" justify="space-between" direction="column">
       <Flex gap="var(--gap-6)" align="center">
@@ -24,7 +28,7 @@ const CheckEventPageHeader = ({ deadline, title, roomSession }: CheckEventPageHe
           <IClock color={theme.colors.text} />
 
           <Text variant="h4" color="text">
-            마감일 : {deadline ? `${deadline.date} ${deadline.time}` : '설정되지 않음'}
+            마감일 : {deadline ? `${deadline.date} ${deadLineTime}` : '설정되지 않음'}
           </Text>
         </Flex>
       </Flex>
