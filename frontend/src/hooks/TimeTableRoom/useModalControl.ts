e0ba1adbefc0603c582703eab59fc8ba1d8ef725
deokbin <deokbin@naver.com>
2025-08-15 00:42:49 +0900

    feat: CopyLink 모달 타입 추가

--- frontend/src/hooks/TimeTableRoom/useModalControl.ts
+++ frontend/src/hooks/TimeTableRoom/useModalControl.ts
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 
-type modalTypeKey = 'Login' | 'EntryConfirm';
+type modalTypeKey = 'Login' | 'EntryConfirm' | 'CopyLink';
 type Modals = Record<modalTypeKey, boolean>;
 export function useModalControl() {
   const [modals, setModals] = useState<Modals>({
     Login: false,
     EntryConfirm: false,
+    CopyLink: false,
   });
 
   const handleOpenModal = (key: modalTypeKey) => {
