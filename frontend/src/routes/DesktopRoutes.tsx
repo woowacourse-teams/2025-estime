111a28b3069a4a874c16baae7e1d1c5254605d05
deokbin <deokbin@naver.com>
2025-08-14 11:33:09 +0900

    feat: creditPage 및 ErrorPage 추가

--- frontend/src/routes/DesktopRoutes.tsx
+++ frontend/src/routes/DesktopRoutes.tsx
@@ -2,6 +2,8 @@ import Layout from '@/components/Layout';
 import { Route, Routes } from 'react-router';
 import CreateEventPage from '../pages/CreateEventPage';
 import CheckEventPage from '../pages/CheckEventPage';
+import CreditsPage from '@/pages/CreditsPage';
+import Error404Page from '@/pages/404Page';
 
 const DesktopRoutes = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: () => void }) => {
   return (
@@ -9,6 +11,8 @@ const DesktopRoutes = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: 
       <Route path="/" element={<Layout isDark={isDark} toggleTheme={toggleTheme} />}>
         <Route index element={<CreateEventPage />} />
         <Route path="check" element={<CheckEventPage />} />
+        <Route path="credits" element={<CreditsPage />} />
+        <Route path="*" element={<Error404Page />} />
       </Route>
     </Routes>
   );
--- frontend/src/routes/MobileRoutes.tsx
+++ frontend/src/routes/MobileRoutes.tsx
@@ -2,6 +2,8 @@ import Layout from '@/components/Layout';
 import { Route, Routes } from 'react-router';
 import CheckEventPage from '../pages/CheckEventPage';
 import MobileCreateEventPage from '@/pages/Mobile/MobileCreateEventPage';
+import CreditsPage from '@/pages/CreditsPage';
+import Error404Page from '@/pages/404Page';
 
 const MobileRoutes = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: () => void }) => {
   return (
@@ -9,6 +11,8 @@ const MobileRoutes = ({ isDark, toggleTheme }: { isDark: boolean; toggleTheme: (
       <Route path="/" element={<Layout isDark={isDark} toggleTheme={toggleTheme} />}>
         <Route index element={<MobileCreateEventPage />} />
         <Route path="check" element={<CheckEventPage />} />
+        <Route path="credits" element={<CreditsPage />} />
+        <Route path="*" element={<Error404Page />} />
       </Route>
     </Routes>
   );
