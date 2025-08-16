82a2f626c2e6c17d8398e617a81ab7c97b02c3a9
deokbin <deokbin@naver.com>
2025-08-16 21:21:47 +0900

    chore: kakao 색상 추가

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -16,6 +16,7 @@ export const LIGHT_THEME = {
     orange40: '#FFAE00',
     red40: '#EF4444',
     green30: '#27874D',
+    kakao: '#FEE500',
 
     successBackground: '#D6F0E0',
     successBorder: '#C0E7D0',
@@ -85,6 +86,7 @@ export const DARK_THEME = {
     orange40: '#FFAE00',
     red40: '#7F1E1D',
     green30: '#27874D',
+    kakao: '#FEE500',
 
     successBackground: '#D6F0E0',
     successBorder: '#C0E7D0',
