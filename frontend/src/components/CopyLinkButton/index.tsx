c32e9e90d3697f85227f25489117609a43343522
deokbin <deokbin@naver.com>
2025-08-16 21:21:36 +0900

    feat: CopyLinkButton 변경

--- frontend/src/components/CopyLinkButton/index.tsx
+++ frontend/src/components/CopyLinkButton/index.tsx
@@ -1,37 +1,30 @@
-import ILink from '@/icons/ILink';
 import Button from '../Button';
 import { useTheme } from '@emotion/react';
-import Text from '@/components/Text';
-import { useState } from 'react';
-import MiniPopup from '../MiniPopup/MiniPopup';
 import Flex from '@/components/Layout/Flex';
+import ICopy from '@/icons/ICopy';
+import * as S from './CopyLinkButton.styled';
+import ICheck from '@/icons/ICheck';
 
-const CopyLinkButton = ({ sessionId }: { sessionId: string }) => {
-  const [isCopied, setIsCopied] = useState(false);
-  const handleCopyLink = () => {
-    const link = `${process.env.DOMAIN_URL}/check?id=${sessionId}`;
-    navigator.clipboard.writeText(link);
-    setIsCopied(true);
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 2000);
-  };
+const CopyLinkButton = ({ isCopied, onClick }: { isCopied: boolean; onClick: () => void }) => {
   const theme = useTheme();
+
   return (
     <>
       <Flex gap="var(--gap-6)" align="center">
-        <Button color="primary" size="small" onClick={handleCopyLink}>
-          <ILink color={theme.colors.primary} />
-          <Text variant="button" color="primary">
-            링크 복사
-          </Text>
+        <Button color={isCopied ? 'green30' : 'primary'} size="x-small" onClick={onClick}>
+          <S.ImageWrapper>
+            {isCopied ? (
+              <ICheck color={theme.colors.green30} />
+            ) : (
+              <ICopy color={theme.colors.primary} />
+            )}
+          </S.ImageWrapper>
         </Button>
-
-        <MiniPopup isVisible={isCopied}>
+        {/* <MiniPopup isVisible={isCopied}>
           <Text variant="caption" color="background">
             링크 복사되었습니다!
           </Text>
-        </MiniPopup>
+        </MiniPopup> */}
       </Flex>
     </>
   );
