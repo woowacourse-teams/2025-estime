84dcef5d8fe5daa57ba95d4144516f2d5145873e
hoyyChoi <hoyeon8165@gmail.com>
2025-08-17 18:20:53 +0900

    feat: 빌드시, 자동으로 index.html에 버전 정보 script로 삽입

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import { readFileSync } from 'fs';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import { fileURLToPath } from 'url';
@@ -8,7 +9,31 @@ import ForkTsCheckerPlugin from 'fork-ts-checker-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// package.json 파일 객체로 만들기
+const pkg = JSON.parse(readFileSync(path.resolve('./package.json'), 'utf-8'));
+
 dotenv.config();
+
+// 빌드시, index.html에 버전 정보 넣기
+class InjectVersionConsolePlugin {
+  constructor(version) {
+    this.version = version;
+  }
+  apply(compiler) {
+    // "컴파일" 객체가 만들어질 때마다 한번씩 호출되는 훅에 구독
+    compiler.hooks.compilation.tap('InjectVersionConsolePlugin', (compilation) => {
+      // HtmlWebpackPlugin이 노출한 훅 세트 가져오기
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tap(
+        'InjectVersionConsolePlugin',
+        (data) => {
+          const banner = `<script>console.info('v${this.version}');</script>`;
+          // 최종 HTML 문자열을 바꿔치기
+          data.html = data.html.replace('</body>', `${banner}\n</body>`);
+        }
+      );
+    });
+  }
+}
 
 export default {
   entry: './src/index.tsx',
@@ -54,6 +79,7 @@ export default {
       favicon: './src/assets/images/logo.svg',
     }),
     new ForkTsCheckerPlugin(),
+    new InjectVersionConsolePlugin(pkg.version),
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env),
     }),
