f1dd29e6645fa9c6dd8c1376eed2588e17819186
deokbin <deokbin@naver.com>
2025-08-18 01:05:27 +0900

    feat: Kakao 타입 상세 추가

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -2,6 +2,29 @@ export {};
 
 declare global {
   interface Window {
-    Kakao: any;
+    Kakao: KakaoSDK;
   }
+}
+
+interface KakaoSDK {
+  init: (appKey: string | undefined) => void;
+  isInitialized: () => boolean;
+  Link: {
+    sendDefault: (options: KakaoLinkDefaultOptions) => void;
+  };
+}
+
+interface KakaoLinkDefaultOptions {
+  objectType: 'feed' | 'list' | 'location' | 'commerce' | 'text';
+  content: {
+    title: string;
+    description?: string;
+    imageUrl: string;
+    imageWidth?: number;
+    imageHeight?: number;
+    link: {
+      mobileWebUrl: string;
+      webUrl: string;
+    };
+  };
 }
