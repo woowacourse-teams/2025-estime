f8eb41300a6ab6a7fed45d8debbbb842329b0894
AppleMint98 <applemint9821@gmail.com>
2025-08-20 14:56:35 +0900

    test: 길이 제한 테스트 추가

---
+++ estime-api/src/test/java/com/estime/room/domain/participant/ParticipantTest.java
@@ -0,0 +1,35 @@
+package com.estime.room.domain.participant;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.estime.common.DomainTerm;
+import com.estime.common.exception.domain.InvalidLengthException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParticipantTest {
+
+    @DisplayName("이름이 최대 길이(10)를 초과하면 InvalidLengthException이 발생한다")
+    @Test
+    void validateName_exceedMaxLength_throwsException() {
+        // given
+        String invalidName = "이름이너무길어서예외가발생합니다";
+
+        // when & then
+        assertThatThrownBy(() -> Participant.withoutId(1L, invalidName))
+                .isInstanceOf(InvalidLengthException.class)
+                .hasMessageContaining(DomainTerm.PARTICIPANT.name());
+    }
+
+    @DisplayName("이름이 최대 길이(10)와 같으면 예외가 발생하지 않는다")
+    @Test
+    void validateName_exactMaxLength_success() {
+        // given
+        String exactLengthName = "열글자이름입니다";
+
+        // when & then
+        assertThatCode(() -> Participant.withoutId(1L, exactLengthName))
+                .doesNotThrowAnyException();
+    }
+}
