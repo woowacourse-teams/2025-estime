c594bd213678e52eec65a74fa323bab8bf1c4ad3
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-08-21 13:01:32 +0900

    feat: EnableScheduling 추가

--- estime-api/src/main/java/com/estime/EstimeApiApplication.java
+++ estime-api/src/main/java/com/estime/EstimeApiApplication.java
@@ -2,8 +2,10 @@ package com.estime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EstimeApiApplication {
 
     public static void main(final String[] args) {
