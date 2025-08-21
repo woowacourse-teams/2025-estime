cda036240170229047ad85ccf8a9b39c63dfa770
조재중 <9gle3492@gmail.com>
2025-08-21 13:08:00 +0900

    스케줄링 동작 설정 (#496)
    * feat: EnableScheduling 추가
    
    * refactor: 불필요한 개행 삭제

--- estime-api/src/main/java/com/estime/EstimeApiApplication.java
+++ estime-api/src/main/java/com/estime/EstimeApiApplication.java
@@ -2,12 +2,13 @@ package com.estime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EstimeApiApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(EstimeApiApplication.class, args);
     }
-
 }
