bd78a63c606cc642460bda2821ab0e4463b36907
AppleMint98 <141808008+AppleMint98@users.noreply.github.com>
2025-08-25 02:18:06 +0900

    hotfix: session.getValue 에러로그 수정 (#509)

--- estime-api/src/main/java/com/estime/room/infrastructure/converter/RoomSessionConverter.java
+++ estime-api/src/main/java/com/estime/room/infrastructure/converter/RoomSessionConverter.java
@@ -4,17 +4,37 @@ import com.estime.room.domain.vo.RoomSession;
 import com.github.f4b6a3.tsid.Tsid;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Converter(autoApply = true)
 public class RoomSessionConverter implements AttributeConverter<RoomSession, String> {
 
     @Override
     public String convertToDatabaseColumn(final RoomSession session) {
-        return session == null ? null : session.getValue().toString();
+        if (session == null) {
+            return null;
+        }
+
+        final Tsid value = session.getValue();
+        if (value == null) {
+            log.error("RoomSession's internal value(Tsid) is null! RoomSession Object: {}", session);
+            return null;
+        }
+
+        return value.toString();
     }
 
     @Override
     public RoomSession convertToEntityAttribute(final String dbData) {
-        return dbData == null ? null : RoomSession.from(Tsid.from(dbData));
+        if (dbData == null) {
+            return null;
+        }
+        try {
+            return RoomSession.from(Tsid.from(dbData));
+        } catch (Exception e) {
+            log.error("Failed to convert String '{}' to RoomSession.", dbData, e);
+            throw e;
+        }
     }
 }
