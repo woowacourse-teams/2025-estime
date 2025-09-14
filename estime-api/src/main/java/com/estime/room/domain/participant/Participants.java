5a492950ca96e092b3463a69dade90f37c80c2c9
jhan0121 <jhan0121@gmail.com>
2025-09-14 15:31:15 +0900

    refactor: room 전체 투표 현황 조회 기능 response에 전체 참가자 현황 정보 추가
    - participants 일급 컬렉션 추가

---
+++ estime-api/src/main/java/com/estime/room/domain/participant/Participants.java
@@ -0,0 +1,42 @@
+package com.estime.room.domain.participant;
+
+import com.estime.common.util.Validator;
+import com.estime.room.domain.participant.vo.ParticipantName;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Participants {
+
+    List<Participant> values;
+
+    private Participants(final List<Participant> values) {
+        this.values = values;
+    }
+
+    public static Participants from(final List<Participant> participants) {
+        validateNull(participants);
+        return new Participants(List.copyOf(participants));
+    }
+
+    private static void validateNull(final List<Participant> participants) {
+        Validator.builder()
+                .add("participants", participants)
+                .validateNull();
+    }
+
+    public int getSize() {
+        return values.size();
+    }
+
+    public List<ParticipantName> getAllNames() {
+        return values.stream()
+                .map(Participant::getName)
+                .toList();
+    }
+
+    public Map<Long, ParticipantName> getIdToName() {
+        return values.stream()
+                .collect(Collectors.toMap(Participant::getId, Participant::getName));
+    }
+}
