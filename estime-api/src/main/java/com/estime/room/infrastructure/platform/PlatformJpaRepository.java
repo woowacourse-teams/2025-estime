d12c1032c1ee8627a6de99915c743aacf06ee428
Yeonhu Lee <185307804+yeonnhuu@users.noreply.github.com>
2025-09-16 15:52:01 +0900

    Repository RUD(Read, Update, Delete) 로직을 QueryDSL로 전환 (#534)
    * refactor: JPA 로직을 QueryDSL로 전환
    
    * refactor: QueryDsl로 전환한 JPARepository 메서드 제거
    
    * chore: 불필요한 import 제거
    
    * refactor: Q클래스 중복 코드 개선
    
    * feat: VoteId 리스트 반환 메서드 추가

--- estime-api/src/main/java/com/estime/room/infrastructure/platform/PlatformJpaRepository.java
+++ estime-api/src/main/java/com/estime/room/infrastructure/platform/PlatformJpaRepository.java
@@ -1,10 +1,7 @@
 package com.estime.room.infrastructure.platform;
 
 import com.estime.room.domain.platform.Platform;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlatformJpaRepository extends JpaRepository<Platform, Long> {
-
-    Optional<Platform> findByRoomIdAndActiveTrue(Long roomId);
 }
