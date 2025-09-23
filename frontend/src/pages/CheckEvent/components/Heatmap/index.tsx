f5ec7e438a219d23a41086b59fc577956ff3198a
deokbin <deokbin@naver.com>
2025-09-23 11:51:05 +0900

    refactor: Tooltip 위치 이동

--- frontend/src/pages/CheckEvent/components/Heatmap/index.tsx
+++ frontend/src/pages/CheckEvent/components/Heatmap/index.tsx
@@ -1,55 +1,63 @@
 import Wrapper from '@/shared/layout/Wrapper';
 import Text from '@/shared/components/Text';
 import * as S from './Heatmap.styled';
-
 import HeatMapDataCell from './HeatMapDataCell';
-import type { DateCellInfo } from '@/pages/CheckEvent/hooks/useHeatmapStatistics';
 import TimeTableDay from '@/pages/CheckEvent/components/Timetable/TimeTableDay';
-import { RefObject } from 'react';
+import { RefObject, useMemo } from 'react';
+import useEventDelegation from '../../hooks/useEventDelegation';
+import Tooltip from '../Tooltip';
 
 interface HeatmapProps {
   timeColumnRef: RefObject<HTMLDivElement | null>;
   dateTimeSlots: string[];
   availableDates: Set<string>;
-  roomStatistics: Map<string, DateCellInfo>;
   handleBeforeEdit?: (e: React.PointerEvent<HTMLDivElement>) => void;
 }
+
 const Heatmap = ({
   timeColumnRef,
   dateTimeSlots,
   availableDates,
-  roomStatistics,
   handleBeforeEdit,
 }: HeatmapProps) => {
+  const { currentCellId, position, isVisible, containerRef } = useEventDelegation();
+
+  // 시간 라벨 불필요한 리렌더링 방지를 위해 분리
+  const timeSlotNodes = useMemo(
+    () =>
+      dateTimeSlots.map((timeText) => (
+        <S.GridContainer key={timeText}>
+          {timeText.endsWith(':00') && (
+            <S.TimeLabel>
+              <Text variant="body" color="text">
+                {timeText}
+              </Text>
+            </S.TimeLabel>
+          )}
+        </S.GridContainer>
+      )),
+    [dateTimeSlots]
+  );
+
   return (
-    <S.HeatMapContent onPointerDown={handleBeforeEdit}>
-      <S.TimeSlotColumn ref={timeColumnRef}>
-        {dateTimeSlots.map((timeText) => (
-          <S.GridContainer key={timeText}>
-            {timeText.endsWith(':00') && (
-              <S.TimeLabel>
-                <Text variant="body" color="text">
-                  {timeText}
-                </Text>
-              </S.TimeLabel>
-            )}
-          </S.GridContainer>
+    <>
+      <S.HeatMapContent ref={containerRef} onPointerDown={handleBeforeEdit}>
+        <S.TimeSlotColumn ref={timeColumnRef}>{timeSlotNodes}</S.TimeSlotColumn>
+        {[...availableDates].map((date) => (
+          <Wrapper key={date} center={false} maxWidth="100%">
+            <TimeTableDay date={date} />
+            {dateTimeSlots.map((timeText) => (
+              <HeatMapDataCell key={`${date}T${timeText}`} date={date} timeText={timeText} />
+            ))}
+          </Wrapper>
         ))}
-      </S.TimeSlotColumn>
-      {[...availableDates].map((date) => (
-        <Wrapper key={date} center={false} maxWidth="100%">
-          <TimeTableDay date={date} key={`${date}-day`} />
-          {dateTimeSlots.map((timeText) => (
-            <HeatMapDataCell
-              key={`${date}T${timeText}`}
-              date={date}
-              timeText={timeText}
-              roomStatistics={roomStatistics}
-            />
-          ))}
-        </Wrapper>
-      ))}
-    </S.HeatMapContent>
+      </S.HeatMapContent>
+
+      {currentCellId && (
+        <Tooltip currentCellId={currentCellId} position={position} visible={isVisible} />
+      )}
+    </>
   );
 };
+
 export default Heatmap;
