b097ad35efa22c347dbc1f6e0bccceaa19e32005
hoyyChoi <hoyeon8165@gmail.com>
2025-09-23 14:24:17 +0900

    chore: 주석제거

--- frontend/src/pages/CheckEvent/hooks/useCheckRoomSession.ts
+++ frontend/src/pages/CheckEvent/hooks/useCheckRoomSession.ts
@@ -5,11 +5,9 @@ import type { RoomInfo } from '@/pages/CreateEvent/types/roomInfo';
 import { initialCheckRoomInfo } from '@/constants/initialRoomInfo';
 import { fromParseRoomInfo } from '@/apis/transform/fromParseRoomInfo';
 import * as Sentry from '@sentry/react';
-// import { useToastContext } from '@/contexts/ToastContext';
 import { useNavigate } from 'react-router';
 
 const useCheckRoomSession = () => {
-  // const { addToast } = useToastContext();
   const navigate = useNavigate();
 
   const session = useExtractQueryParams('id');
@@ -25,18 +23,6 @@ const useCheckRoomSession = () => {
       const parseData = fromParseRoomInfo(response);
       setRoomInfo(parseData);
     } catch (err) {
-      // const e = err as Error;
-      // TODO: 이렇게 에러를 처리하기
-      // if (e.message === 'Room not found') {
-      //   addToast({
-      //     type: 'warning',
-      //     message: '존재하지 않는 방입니다.',
-      //   });
-      //   router('/');
-
-      //   return;
-      // }
-      // 임시 방편으로 모든 에러를 500이든 400이든 404로 처리.
       Sentry.captureException(err, {
         level: 'error',
       });
