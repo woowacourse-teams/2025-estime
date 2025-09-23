1b4d97f05696fedf70a12f32338790c22d31146d
hoyyChoi <hoyeon8165@gmail.com>
2025-09-23 14:23:57 +0900

    refactor: 공통store를 이용해서 toastStore 구현

--- frontend/src/shared/store/toastStore.ts
+++ frontend/src/shared/store/toastStore.ts
@@ -1,11 +1,4 @@
-type Listener = () => void;
-
-type Store<T> = {
-  getSnapshot: () => T;
-  subscribe: (listener: Listener) => () => void;
-  publish: () => void;
-  addToast: (toast: Omit<ToastStateType, 'id'>) => void;
-};
+import createStore from './createStore';
 
 export type ToastType = 'success' | 'error' | 'warning';
 
@@ -15,50 +8,37 @@ export interface ToastStateType {
   message: string;
 }
 
-let toasts: ToastStateType[] = [];
-let listen: Listener[] = [];
-let timer: ReturnType<typeof setTimeout> | null = null;
-
 const MAX_SAME_TOAST_REPEAT = 3;
 
-const toastStore: Store<ToastStateType[]> = {
-  getSnapshot: () => toasts,
+const createToastStore = () => {
+  const store = createStore<ToastStateType[]>([]);
+  let timer: ReturnType<typeof setTimeout> | null = null;
 
-  subscribe: (listener: Listener) => {
-    listen.push(listener);
-    return () => {
-      listen = listen.filter((li) => li !== listener);
-    };
-  },
+  const addToast = (toast: Omit<ToastStateType, 'id'>) => {
+    store.setState((prev) => {
+      const isOverLimit =
+        prev.length >= MAX_SAME_TOAST_REPEAT &&
+        prev
+          .slice(-MAX_SAME_TOAST_REPEAT)
+          .every((t) => t.message === toast.message && t.type === toast.type);
 
-  publish: () => {
-    listen.forEach((listener) => {
-      listener();
+      if (isOverLimit) return prev;
+
+      const id = `${Date.now()}-${Math.random()}`;
+      const next = [...prev, { ...toast, id }];
+
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => store.setState([]), 1600);
+
+      return next;
     });
-  },
+  };
 
-  addToast: (toast: Omit<ToastStateType, 'id'>) => {
-    // 마지막 3개 잘라서 현재 들어온 메시지와 모두 동일한지 체크하기 (동일하면 limit 걸기)
-    const isOverLimit =
-      toasts.length >= MAX_SAME_TOAST_REPEAT &&
-      toasts
-        .slice(-MAX_SAME_TOAST_REPEAT)
-        .every((t) => t.message === toast.message && t.type === toast.type);
-
-    if (isOverLimit) return;
-
-    const id = `${Date.now()}-${Math.random()}`;
-    toasts = [...toasts, { ...toast, id }];
-
-    if (timer) clearTimeout(timer);
-
-    timer = setTimeout(() => {
-      toasts = [];
-    }, 1600);
-
-    //리렌더링
-    toastStore.publish();
-  },
+  return {
+    ...store, // getSnapshot, subscribe, setState 재사용
+    addToast,
+  };
 };
 
-export default toastStore;
+export const toastStore = createToastStore();
+export const showToast = toastStore.addToast;
