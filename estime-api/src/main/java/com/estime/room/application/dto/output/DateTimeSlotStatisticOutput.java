95379f291382bb63173d433b05d20129cdf2c3f3
jhan0121 <56645802+jhan0121@users.noreply.github.com>
2025-09-24 17:36:57 +0900

    방 전체 투표 현황 조회 기능 참가자 리스트 추가 (#536)
    * refactor: room 전체 투표 현황 조회 기능 response에 전체 참가자 현황 정보 추가
    
    - participants 일급 컬렉션 추가
    
    * test: ParticipantsTest 추가
    
    * refactor: Participants 필드 접근 제한자 수정
    
    * test: ParticipantsTest#getIdToName 테스트용 로직 제거 및 리플렉션 적용

--- estime-api/src/main/java/com/estime/room/application/dto/output/DateTimeSlotStatisticOutput.java
+++ estime-api/src/main/java/com/estime/room/application/dto/output/DateTimeSlotStatisticOutput.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public record DateTimeSlotStatisticOutput(
         int participantCount,
+        List<ParticipantName> participants,
         List<DateTimeParticipantsOutput> statistic
 ) {
 
