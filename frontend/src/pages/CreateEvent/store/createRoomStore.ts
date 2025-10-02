9c2ff1e87ddc4ab64f68cd9a02d848ce1f5a9363
spoyodevelop <kjy9505@gmail.com>
2025-10-02 16:27:59 +0900

    chore: createRoomStore 파일 위치 변경
    Co-authored-by: thgml05 <thgml05@users.noreply.github.com>

--- frontend/src/pages/CreateEvent/store/createRoomStore.ts
+++ frontend/src/pages/CreateEvent/store/createRoomStore.ts
@@ -1,15 +1,10 @@
-import type { RoomInfo } from '@/pages/CreateEvent/types/roomInfo';
-import createStore from './createStore';
-import { initialCreateRoomInfo } from '@/constants/initialRoomInfo';
 import { useSyncExternalStore } from 'react';
-import { TimeManager } from '../utils/common/TimeManager';
-
-interface CreateRoomStoreType extends RoomInfo {
-  time: { startTime: string; endTime: string };
-}
+import type { CreateRoomInfoType } from '@/pages/CreateEvent/types/roomInfo';
+import { initialCreateRoomInfo } from '@/constants/initialRoomInfo';
+import createStore from '@/shared/store/createStore';
 
 const createRoomStore = () => {
-  const store = createStore<CreateRoomStoreType>(initialCreateRoomInfo);
+  const store = createStore<CreateRoomInfoType>(initialCreateRoomInfo);
 
   const onChangeTitle = (title: string) => {
     store.setState((prev) => ({ ...prev, title }));
@@ -22,6 +17,7 @@ const createRoomStore = () => {
   const onChangeTime = (time: { startTime: string; endTime: string }) => {
     store.setState((prev) => ({ ...prev, time }));
   };
+
   const onChangeDeadline = (deadline: { date: string; time: string }) => {
     store.setState((prev) => ({ ...prev, deadline }));
   };
@@ -37,13 +33,13 @@ const createRoomStore = () => {
 
 const roomStore = createRoomStore();
 
-export const getRoomInfo = () => roomStore.getSnapshot();
+export const getRoomInfo = roomStore.getSnapshot;
 
 export const onChangeTitle = roomStore.onChangeTitle;
 export const onChangeTime = roomStore.onChangeTime;
 export const onChangeAvailableDateSlots = roomStore.onChangeAvailableDateSlots;
 export const onChangeDeadline = roomStore.onChangeDeadline;
 
-export const useRoomSelector = <T extends keyof CreateRoomStoreType>(selector: T) => {
+export const useRoomSelector = <T extends keyof CreateRoomInfoType>(selector: T) => {
   return useSyncExternalStore(roomStore.subscribe, () => getRoomInfo()[selector]);
 };
