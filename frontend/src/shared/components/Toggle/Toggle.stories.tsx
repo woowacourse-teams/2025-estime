d540d7d7246dcc51ce826501c09a8102d2931498
spoyodevelop <kjy9505@gmail.com>
2025-10-07 20:07:24 +0900

    feat: 토글 컴포넌트 추가

---
+++ frontend/src/shared/components/Toggle/Toggle.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import Toggle from '.';
+
+const meta: Meta<typeof Toggle> = {
+  title: 'Components/Toggle',
+  component: Toggle,
+  tags: ['autodocs'],
+
+  decorators: [
+    (Story) => (
+      <div style={{ width: '50px', padding: '10px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toggle>;
+
+export const OnToggle: Story = {
+  args: {
+    isOn: true,
+    onToggle: () => {},
+  },
+};
+
+export const OffToggle: Story = {
+  args: {
+    isOn: false,
+    onToggle: () => {},
+  },
+};
