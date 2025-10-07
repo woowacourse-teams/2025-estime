e68ffb8be2e85297c141adea4f3110ff8f65590b
spoyodevelop <kjy9505@gmail.com>
2025-10-07 20:18:56 +0900

    feat: 히트맵 프리뷰 토글 적용

--- frontend/src/pages/CheckEvent/components/Timetable/index.tsx
+++ frontend/src/pages/CheckEvent/components/Timetable/index.tsx
@@ -3,23 +3,36 @@ import TimeTableColumn from './TimeTableColumn';
 import TimeSlotColumn from './TimeSlotColumn';
 import useLocalTimeSelection from '@/pages/CheckEvent/hooks/useLocalTimeSelection';
 import { RefObject } from 'react';
+import HeatmapPreview from '../HeatMapPreview';
+import Wrapper from '@/shared/layout/Wrapper';
 
 interface TimetableProps {
   timeColumnRef: RefObject<HTMLDivElement | null>;
   dateTimeSlots: string[];
   availableDates: Set<string>;
+  showHeatmapPreview: boolean;
 }
 
-const Timetable = ({ timeColumnRef, dateTimeSlots, availableDates }: TimetableProps) => {
+const Timetable = ({
+  timeColumnRef,
+  dateTimeSlots,
+  availableDates,
+  showHeatmapPreview,
+}: TimetableProps) => {
   const { containerRef, pointerHandlers } = useLocalTimeSelection();
 
   return (
-    <S.TimetableContent ref={containerRef} {...pointerHandlers}>
-      <TimeSlotColumn timeColumnRef={timeColumnRef} dateTimeSlots={dateTimeSlots} />
-      {[...availableDates].map((date) => (
-        <TimeTableColumn key={date} date={date} dateTimeSlots={dateTimeSlots} />
-      ))}
-    </S.TimetableContent>
+    <>
+      <S.TimetableContent ref={containerRef} {...pointerHandlers}>
+        <TimeSlotColumn timeColumnRef={timeColumnRef} dateTimeSlots={dateTimeSlots} />
+        {[...availableDates].map((date) => (
+          <Wrapper key={date} center={false} maxWidth="100%">
+            <HeatmapPreview date={date} dateTimeSlots={dateTimeSlots} show={showHeatmapPreview} />
+            <TimeTableColumn date={date} dateTimeSlots={dateTimeSlots} />
+          </Wrapper>
+        ))}
+      </S.TimetableContent>
+    </>
   );
 };
 
