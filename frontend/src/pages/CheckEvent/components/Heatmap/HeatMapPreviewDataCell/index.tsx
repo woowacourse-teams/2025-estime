2db34240b0354c5ed6775b9631886092336a28fd
spoyodevelop <kjy9505@gmail.com>
2025-10-07 20:18:26 +0900

    feat: 히트맵셀에서 빤짝이는 애니메이션을 뺀 HeatMapPreviewCell 추가

---
+++ frontend/src/pages/CheckEvent/components/Heatmap/HeatMapPreviewDataCell/index.tsx
@@ -0,0 +1,25 @@
+import * as S from './HeatMapPreviewDataCell.styled';
+import { memo } from 'react';
+import { useRoomStatistics } from '@/pages/CheckEvent/stores/roomStatisticsStore';
+
+interface HeatMapDataCellProps {
+  date: string;
+  timeText: string;
+}
+
+const HeatmapPreviewDataCell = ({ date, timeText }: HeatMapDataCellProps) => {
+  const roomStatistics = useRoomStatistics();
+  const cellInfo = roomStatistics.get(`${date}T${timeText}`);
+  const isRecommended = cellInfo?.isRecommended;
+
+  const weight = (cellInfo?.weight ?? 0) / 2;
+  return (
+    <S.Container
+      data-cell-id={`${date}T${timeText}`}
+      weight={weight}
+      isRecommended={isRecommended}
+    />
+  );
+};
+
+export default memo(HeatmapPreviewDataCell);
