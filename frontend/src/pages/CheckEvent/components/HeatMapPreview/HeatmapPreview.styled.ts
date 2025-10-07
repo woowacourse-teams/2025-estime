c6ad993e9a1f45fbe9b6ad7e6d8e37f689de2724
spoyodevelop <kjy9505@gmail.com>
2025-10-07 22:17:27 +0900

    refactor: 셀 각각에서 투명도 조절을 하지 않고 전체적으로 투명도를 조정하게 수정

--- frontend/src/pages/CheckEvent/components/HeatMapPreview/HeatmapPreview.styled.ts
+++ frontend/src/pages/CheckEvent/components/HeatMapPreview/HeatmapPreview.styled.ts
@@ -7,6 +7,6 @@ export const HeatmapContainer = styled.div<{ show: boolean }>`
   width: 100%;
   height: 100%;
   pointer-events: none;
-  opacity: ${({ show }) => (show ? 1 : 0)};
+  opacity: ${({ show }) => (show ? 0.5 : 0)};
   transition: opacity 0.3s ease;
 `;
