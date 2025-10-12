b82ca16d8c6d6c95bb85d58002467b262e17700e
hoyyChoi <hoyeon8165@gmail.com>
2025-10-12 19:02:07 +0900

    feat: 히트맵 셀에 이벤트 핸들러 적용 (hover, leave)

--- frontend/src/pages/CheckEvent/components/Heatmap/HeatMapDataCell/index.tsx
+++ frontend/src/pages/CheckEvent/components/Heatmap/HeatMapDataCell/index.tsx
@@ -1,6 +1,9 @@
 import * as S from './HeatMapDataCell.styled';
 import { memo } from 'react';
 import { useRoomStatistics } from '@/pages/CheckEvent/stores/roomStatisticsStore';
+import { cellDataStore } from '@/pages/CheckEvent/stores/CellDataStore';
+import { TimeManager } from '@/shared/utils/common/TimeManager';
+import { FormatManager } from '@/shared/utils/common/FormatManager';
 
 interface HeatMapDataCellProps {
   date: string;
@@ -11,13 +14,25 @@ const HeatMapDataCell = ({ date, timeText }: HeatMapDataCellProps) => {
   const roomStatistics = useRoomStatistics();
   const cellInfo = roomStatistics.get(`${date}T${timeText}`);
   const isRecommended = cellInfo?.isRecommended;
-
   const weight = cellInfo?.weight ?? 0;
   return (
     <S.Container
       data-cell-id={`${date}T${timeText}`}
       weight={weight}
       isRecommended={isRecommended}
+      onMouseOver={() => {
+        if (!cellInfo) {
+          cellDataStore.initialStore();
+        } else {
+          cellDataStore.setState({
+            ...cellInfo,
+            date: FormatManager.formatKoreanDate(date),
+            startTime: timeText,
+            endTime: TimeManager.addMinutes(timeText, 30),
+          });
+        }
+      }}
+      onMouseLeave={() => cellDataStore.initialStore()}
     />
   );
 };
