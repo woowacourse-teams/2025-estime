a9366a2131ec2795bdc4b0b38d0bfc72e4482b91
조재중 <9gle3492@gmail.com>
2025-10-14 00:38:33 +0900

    feat: AvailableDateSlot, AvailableTimeSlot 인덱스 추가 (#678)

--- core/src/main/java/com/estime/room/slot/AvailableDateSlot.java
+++ core/src/main/java/com/estime/room/slot/AvailableDateSlot.java
@@ -4,6 +4,7 @@ import com.estime.shared.BaseEntity;
 import com.estime.shared.Validator;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AccessLevel;
@@ -13,7 +14,9 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
-@Table(name = "available_date_slot")
+@Table(indexes = {
+        @Index(name = "idx_available_date_slot_room_id", columnList = "room_id")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
