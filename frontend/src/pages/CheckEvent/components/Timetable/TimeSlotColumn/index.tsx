66d062e88cc2680040eb6d7384b51ff543dd3be1
deokbin <deokbin@naver.com>
2025-10-14 03:31:32 +0900

    feat: 보여줘야 할 시간 label에 active 하는 로직 추가

--- frontend/src/pages/CheckEvent/components/Timetable/TimeSlotColumn/index.tsx
+++ frontend/src/pages/CheckEvent/components/Timetable/TimeSlotColumn/index.tsx
@@ -1,28 +1,70 @@
 import * as S from '../Timetable.styled';
-import Text from '@/shared/components/Text';
-import { RefObject, memo } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
+import { HoveredTimeRef } from '../TimeTableColumn';
 
 interface TimeSlotColumnProps {
   timeColumnRef: RefObject<HTMLDivElement | null>;
   dateTimeSlots: string[];
+  hoveredTimeRef: RefObject<HoveredTimeRef>;
 }
 
-const TimeSlotColumn = ({ timeColumnRef, dateTimeSlots }: TimeSlotColumnProps) => {
+const TimeSlotColumn = ({ timeColumnRef, dateTimeSlots, hoveredTimeRef }: TimeSlotColumnProps) => {
+  const labelRefs = useRef<Record<string, HTMLDivElement | null>>({});
+  const hoverLabelRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    hoveredTimeRef.current.update = (hoveredTime: string | null) => {
+      Object.values(labelRefs.current).forEach((el) => el?.classList.remove('active'));
+
+      const hoverLabel = hoverLabelRef.current;
+      if (!hoverLabel) return;
+
+      if (!hoveredTime) {
+        hoverLabel.classList.remove('visible');
+        return;
+      }
+
+      const index = dateTimeSlots.indexOf(hoveredTime);
+      if (index === -1) return;
+
+      const isHalf = index % 2 !== 0;
+
+      const nextLabelTime = dateTimeSlots[!isHalf ? index + 1 : index];
+      const top = `calc(24px + ${(dateTimeSlots.indexOf(nextLabelTime) / 2) * 3}rem)`;
+
+      const endTime = dateTimeSlots[index + 1];
+
+      hoverLabel.textContent = nextLabelTime;
+      hoverLabel.style.top = top;
+      hoverLabel.classList.add('visible');
+
+      const startLabel = labelRefs.current[hoveredTime];
+      startLabel?.classList.add('active');
+      const endLabel = labelRefs.current[endTime];
+      endLabel?.classList.add('active');
+
+      hoverLabel.classList.add('active');
+    };
+  }, [dateTimeSlots, hoveredTimeRef]);
+
   return (
     <S.TimeSlotColumn ref={timeColumnRef}>
-      {dateTimeSlots.map((dateTimeSlot) => (
-        <S.GridContainer key={dateTimeSlot}>
-          {dateTimeSlot.endsWith(':00') && (
-            <S.TimeLabel>
-              <Text variant="body" color="text">
-                {dateTimeSlot}
-              </Text>
+      {dateTimeSlots.map(
+        (time) =>
+          time.endsWith(':00') && (
+            <S.TimeLabel
+              key={time}
+              ref={(el) => {
+                labelRefs.current[time] = el;
+              }}
+            >
+              <span>{time}</span>
             </S.TimeLabel>
-          )}
-        </S.GridContainer>
-      ))}
+          )
+      )}
+      <S.HoverLabel ref={hoverLabelRef} />
     </S.TimeSlotColumn>
   );
 };
 
-export default memo(TimeSlotColumn);
+export default TimeSlotColumn;
