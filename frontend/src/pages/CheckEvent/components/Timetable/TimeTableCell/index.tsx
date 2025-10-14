7c65668d54af273932ac39a64e8ae94ae3103061
spoyodevelop <kjy9505@gmail.com>
2025-10-14 13:50:03 +0900

    feat: Timetable-cell에 마우스 상호작용 도입

--- frontend/src/pages/CheckEvent/components/Timetable/TimeTableCell/index.tsx
+++ frontend/src/pages/CheckEvent/components/Timetable/TimeTableCell/index.tsx
@@ -1,3 +1,8 @@
+import { cellDataStore } from '@/pages/CheckEvent/stores/CellDataStore';
+import { useRoomStatistics } from '@/pages/CheckEvent/stores/roomStatisticsStore';
+import { FormatManager } from '@/shared/utils/common/FormatManager';
+import { TimeManager } from '@/shared/utils/common/TimeManager';
+
 interface TimeTableCellProps {
   date: string;
   timeText: string;
@@ -6,7 +11,30 @@ interface TimeTableCellProps {
 const TimeTableCell = ({ date, timeText }: TimeTableCellProps) => {
   const dateTimeKey = `${date}T${timeText}`;
 
-  return <div className="heat-map-cell" data-time={dateTimeKey} />;
+  const roomStatistics = useRoomStatistics();
+  const cellInfo = roomStatistics.statistics.get(dateTimeKey);
+  const isRecommended = cellInfo?.voteCount === roomStatistics.maxVoteCount;
+
+  return (
+    <div
+      className="heat-map-cell"
+      data-time={dateTimeKey}
+      onMouseOver={() => {
+        if (!cellInfo) {
+          cellDataStore.initialStore();
+        } else {
+          cellDataStore.setState({
+            ...cellInfo,
+            isRecommended,
+            date: FormatManager.formatKoreanDate(date),
+            startTime: timeText,
+            endTime: TimeManager.addMinutes(timeText, 30),
+          });
+        }
+      }}
+      onMouseLeave={() => cellDataStore.initialStore()}
+    />
+  );
 };
 
 export default TimeTableCell;
