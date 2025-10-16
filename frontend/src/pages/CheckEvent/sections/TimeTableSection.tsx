d93753ffad506278adf3cc5d8562bb069bf5e80e
spoyodevelop <kjy9505@gmail.com>
2025-10-16 23:06:27 +0900

    feat: 0명일때 프리뷰 버튼 숨김 기능 추가

--- frontend/src/pages/CheckEvent/sections/TimeTableSection.tsx
+++ frontend/src/pages/CheckEvent/sections/TimeTableSection.tsx
@@ -15,6 +15,7 @@ import { RoomInfo } from '@/pages/CreateEvent/types/roomInfo';
 
 import Toggle from '@/shared/components/Toggle';
 import { useGlassPreview } from '../stores/glassPreviewStore';
+import { useRoomStatistics } from '../stores/roomStatisticsStore';
 
 interface TimetableSectionProps {
   roomInfo: RoomInfo & { roomSession: string; availableTimeSlots: string[] };
@@ -35,17 +36,21 @@ const TimetableSection = ({
 
   const isExpired = DateManager.IsPastDeadline(roomInfo.deadline);
 
+  const { participantCount } = useRoomStatistics();
+
   return (
     <S.BackFace ref={pagination.timeTableContainerRef}>
       <Flex direction="column" gap="var(--gap-8)">
         <TimeTableHeader name={userNameStore.getSnapshot()} mode="save" isExpired={isExpired}>
           <Flex gap="var(--gap-8)" align="center" justify="flex-end">
-            <Flex gap="var(--gap-3)" align="center" justify="center" direction="column">
-              <Text variant="h4" color="text">
-                전체 시간표
-              </Text>
-              <Toggle isOn={glassPreview.isOn} onToggle={glassPreview.toggle} />
-            </Flex>
+            {participantCount > 0 && (
+              <Flex gap="var(--gap-3)" align="center" justify="center" direction="column">
+                <Text variant="h4" color="text">
+                  전체 시간표
+                </Text>
+                <Toggle isOn={glassPreview.isOn} onToggle={glassPreview.toggle} />
+              </Flex>
+            )}
             <Button color="primary" onClick={handleButtonClick} disabled={isExpired} size="small">
               <Text variant="button" color={isExpired ? 'gray50' : 'text'}>
                 {buttonName}
