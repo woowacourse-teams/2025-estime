8577c9b8c2c9dd0fe1a829f126f57c5be17f5a67
thgml05 <aisohii@naver.com>
2025-10-20 10:06:04 +0900

    feat: DatePicker 접근성 추가

--- frontend/src/shared/components/DatePicker/index.tsx
+++ frontend/src/shared/components/DatePicker/index.tsx
@@ -9,14 +9,20 @@ const DatePicker = ({ isError = false, ...props }: DatePickerProps) => {
   const date = props.value ?? new Date().toISOString().substring(0, 10);
 
   return (
-    <S.Container
-      type="date"
-      value={date}
-      onClick={(e) => e.currentTarget.showPicker()}
-      min={new Date().toISOString().split('T')[0]}
-      isError={isError}
-      {...props}
-    />
+    <>
+      <S.Container
+        type="date"
+        value={date}
+        onClick={(e) => e.currentTarget.showPicker()}
+        min={new Date().toISOString().split('T')[0]}
+        isError={isError}
+        tabIndex={0}
+        aria-describedby="dateHint"
+        aria-live="assertive"
+        {...props}
+      />
+      <S.HintA11y id="dateHint">방향키 위아래로 연, 월, 일을 변경할 수 있습니다.</S.HintA11y>
+    </>
   );
 };
 
