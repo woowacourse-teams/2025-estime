eb0ae9c38796449a1b916334acc8523b189611c6
thgml05 <aisohii@naver.com>
2025-10-21 19:39:09 +0900

    test: createEventPage E2E 테스트 작성

---
+++ frontend/cypress/e2e/createEventPage.cy.ts
@@ -0,0 +1,40 @@
+describe('방 생성 플로우', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000');
+  });
+
+  it('모든 필수 입력값이 주어지면 방이 정상적으로 생성된다.', () => {
+    const date = new Date();
+    const day = date.getDate();
+    cy.get(`[role="button"][aria-label="${day}일 선택 안되어 있음"]`).click();
+    cy.get('[aria-label="약속 제목 입력 필드"]').type('아인슈타임 회식');
+
+    cy.contains('약속 만들기').click();
+
+    cy.url().should('include', '/check');
+    cy.contains('방 생성이 완료되었습니다.').should('be.visible');
+  });
+
+  describe('필수 입력값이 누락되면 방이 생성되지 않는다.', () => {
+    it('날짜를 선택하지 않고 약속 제목을 입력하지 않으면 방 생성 버튼 클릭 시 경고 토스트가 표시된다.', () => {
+      cy.contains('약속 만들기').click();
+      cy.contains('날짜와 기본 설정을 선택해주세요!').should('be.visible');
+    });
+
+    it('날짜를 선택하지 않고 방 생성 버튼 클릭 시 경고 토스트가 표시된다.', () => {
+      cy.get('[aria-label="약속 제목 입력 필드"]').type('아인슈타임 회식');
+
+      cy.contains('약속 만들기').click();
+      cy.contains('날짜를 선택해주세요!').should('be.visible');
+    });
+
+    it('약속 제목을 입력하지 않고 방 생성 버튼 클릭 시 경고 토스트가 표시된다.', () => {
+      const date = new Date();
+      const day = date.getDate();
+      cy.get(`[role="button"][aria-label="${day}일 선택 안되어 있음"]`).click();
+
+      cy.contains('약속 만들기').click();
+      cy.contains('제목과 시간을 선택해주세요!').should('be.visible');
+    });
+  });
+});
