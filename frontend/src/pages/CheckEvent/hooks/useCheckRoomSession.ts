3fe051fc57a8ee061bd92f24b3b60fdf3ab68058
spoyodevelop <kjy9505@gmail.com>
2025-10-21 19:07:29 +0900

    feat: 방 정보에 aria-polite 기능 추가

--- frontend/src/pages/CheckEvent/hooks/useCheckRoomSession.ts
+++ frontend/src/pages/CheckEvent/hooks/useCheckRoomSession.ts
@@ -5,6 +5,7 @@ import type { RoomInfo } from '@/pages/CreateEvent/types/roomInfo';
 import { initialCheckRoomInfo } from '@/constants/initialRoomInfo';
 import { fromParseRoomInfo } from '@/apis/transform/fromParseRoomInfo';
 import useFetch from '@/shared/hooks/common/useFetch';
+import { useAnnounceContext } from '../providers/AnnounceProvider';
 
 const useCheckRoomSession = () => {
   const session = useExtractQueryParams('id');
@@ -17,11 +18,31 @@ const useCheckRoomSession = () => {
     RoomInfo & { roomSession: string; availableTimeSlots: string[] }
   >(initialCheckRoomInfo);
 
+  const { roomInfoAnnounce } = useAnnounceContext();
+
+  const announceRoomInfo = useCallback(
+    (title: string, deadline: { date: string; time: string }) => {
+      const deadlineString = new Date(deadline.date + 'T' + deadline.time).toLocaleString('ko-KR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+
+      roomInfoAnnounce.announce(
+        `${title} 방에 들어왔습니다. 투표 기간은 ${deadlineString}까지 입니다.`
+      );
+    },
+    [roomInfoAnnounce]
+  );
+
   const fetchSession = useCallback(async () => {
     const response = await getRoomSession();
     const parseData = fromParseRoomInfo(response);
     setRoomInfo(parseData);
-  }, [getRoomSession]);
+    announceRoomInfo(parseData.title, parseData.deadline);
+  }, [getRoomSession, announceRoomInfo]);
 
   useEffect(() => {
     if (!session) return;
