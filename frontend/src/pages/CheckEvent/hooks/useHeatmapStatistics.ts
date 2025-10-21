1275ec69e56fff61d56c02b40d0b8faa9cebad54
spoyodevelop <kjy9505@gmail.com>
2025-10-21 19:06:58 +0900

    feat: 히트맵 정보 갱신에 ariaPolite 훅 사용

--- frontend/src/pages/CheckEvent/hooks/useHeatmapStatistics.ts
+++ frontend/src/pages/CheckEvent/hooks/useHeatmapStatistics.ts
@@ -3,6 +3,7 @@ import useFetch from '@/shared/hooks/common/useFetch';
 import { useEffect, useCallback } from 'react';
 import { roomStatisticsStore, type StatisticItem } from '../stores/roomStatisticsStore';
 import type { GetRoomStatisticsResponseType } from '@/apis/room/type';
+import { useAnnounceContext } from '../providers/AnnounceProvider';
 
 const useHeatmapStatistics = ({ session }: { session: string }) => {
   const { triggerFetch: getStatistics } = useFetch({
@@ -10,26 +11,56 @@ const useHeatmapStatistics = ({ session }: { session: string }) => {
     requestFn: useCallback(() => getRoomStatistics(session), [session]),
   });
 
-  const storeRoomResponse = useCallback((response: GetRoomStatisticsResponseType) => {
-    const statisticsMap = new Map<string, StatisticItem>();
+  const { statisticsAnnounce } = useAnnounceContext();
 
-    const { participantCount, participants, maxVoteCount, statistic } = response;
-    for (const item of statistic) {
-      statisticsMap.set(item.dateTimeSlot, {
-        voteCount: item.voteCount,
-        weight: item.weight,
-        participantNames: item.participantNames,
+  const announceNewStatistics = useCallback(
+    (recommendedTime: string[]) => {
+      const recommendedTimeString = recommendedTime
+        .map((date) =>
+          new Date(date).toLocaleString('ko-kr', {
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+        )
+        .slice(0, 6)
+        .join(', ');
+
+      statisticsAnnounce.announce(`추천 시간대는 다음과 같습니다. ${recommendedTimeString}`);
+    },
+    [statisticsAnnounce]
+  );
+
+  const storeRoomResponse = useCallback(
+    (response: GetRoomStatisticsResponseType) => {
+      const statisticsMap = new Map<string, StatisticItem>();
+      const recommendedTime = [];
+      const { participantCount, participants, maxVoteCount, statistic } = response;
+
+      for (const item of statistic) {
+        if (item.participantNames.length === maxVoteCount) {
+          recommendedTime.push(item.dateTimeSlot);
+        }
+        statisticsMap.set(item.dateTimeSlot, {
+          voteCount: item.voteCount,
+          weight: item.weight,
+          participantNames: item.participantNames,
+        });
+      }
+
+      roomStatisticsStore.setState({
+        recommendedTime,
+        participantCount,
+        participants,
+        maxVoteCount,
+        statistics: statisticsMap,
       });
-    }
 
-    roomStatisticsStore.setState({
-      participantCount,
-      participants,
-      maxVoteCount,
-      statistics: statisticsMap,
-    });
-  }, []);
-
+      announceNewStatistics(recommendedTime);
+    },
+    [announceNewStatistics]
+  );
   const fetchRoomStatistics = useCallback(async () => {
     const response = await getStatistics();
     if (response) {
@@ -41,7 +72,7 @@ const useHeatmapStatistics = ({ session }: { session: string }) => {
     if (session) {
       fetchRoomStatistics();
     }
-  }, [session, fetchRoomStatistics]);
+  }, [fetchRoomStatistics, session]);
 
   return { fetchRoomStatistics };
 };
