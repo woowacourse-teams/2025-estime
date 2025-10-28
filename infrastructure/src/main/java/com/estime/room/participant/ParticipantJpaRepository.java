15aae6676ae4c5e943f0e0d327335a3de6862f48
jhan0121 <jhan0121@gmail.com>
2025-10-28 18:18:42 +0900

    fix: ParticipantJpaRepository `@Transactional` 제거

--- infrastructure/src/main/java/com/estime/room/participant/ParticipantJpaRepository.java
+++ infrastructure/src/main/java/com/estime/room/participant/ParticipantJpaRepository.java
@@ -1,6 +1,5 @@
 package com.estime.room.participant;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface ParticipantJpaRepository extends JpaRepository<Participant, Long> {
 
     @Modifying
-    @Transactional
     @Query(
             value = "INSERT IGNORE INTO participant (room_id, name, active) VALUES (?1, ?2, ?3)",
             nativeQuery = true
