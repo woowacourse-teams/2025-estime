fccd0697eafae002ddbb01da09124b575847d064
jhan0121 <jhan0121@gmail.com>
2025-10-29 11:19:05 +0900

    refactor: ParticipantJpaRepository#saveIfNotExists 파라미터 바인딩 변경
    - 순서 기반 -> 이름 기반 으로 변경

--- infrastructure/src/main/java/com/estime/room/participant/ParticipantJpaRepository.java
+++ infrastructure/src/main/java/com/estime/room/participant/ParticipantJpaRepository.java
@@ -3,13 +3,18 @@ package com.estime.room.participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ParticipantJpaRepository extends JpaRepository<Participant, Long> {
 
     @Modifying
     @Query(
-            value = "INSERT IGNORE INTO participant (room_id, name, active) VALUES (?1, ?2, ?3)",
+            value = "INSERT IGNORE INTO participant (room_id, name, active) VALUES (:room_id, :name, :active)",
             nativeQuery = true
     )
-    int saveIfNotExists(Long roomId, String name, boolean active);
+    int saveIfNotExists(
+            @Param("room_id") Long roomId,
+            @Param("name") String name,
+            @Param("active") boolean active
+    );
 }
