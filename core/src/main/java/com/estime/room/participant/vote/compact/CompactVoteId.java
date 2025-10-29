246a36382aabbd2fb52874cc017caef797294046
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-10-29 15:06:50 +0900

    feat: CompactVote Entity 추가
    - 복합키(CompactVoteId)로만 이루어진 CompactVote.
    - CompactDateTimeSlot을 위한 AttributeConverter.

---
+++ core/src/main/java/com/estime/room/participant/vote/compact/CompactVoteId.java
@@ -0,0 +1,36 @@
+package com.estime.room.participant.vote.compact;
+
+import com.estime.room.slot.CompactDateTimeSlot;
+import com.estime.shared.Validator;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode
+public class CompactVoteId implements Serializable {
+
+    private Long participantId;
+
+    private CompactDateTimeSlot dateTimeSlot;
+
+    public static CompactVoteId of(final Long participantId, final CompactDateTimeSlot dateTimeSlot) {
+        validateNull(participantId, dateTimeSlot);
+        return new CompactVoteId(participantId, dateTimeSlot);
+    }
+
+    private static void validateNull(final Long participantId, final CompactDateTimeSlot dateTimeSlot) {
+        Validator.builder()
+                .add("participantId", participantId)
+                .add("dateTimeSlot", dateTimeSlot)
+                .validateNull();
+    }
+}
