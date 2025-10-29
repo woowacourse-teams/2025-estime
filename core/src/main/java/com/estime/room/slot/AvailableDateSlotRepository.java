5ebb97446eace1b7d33afd669c563fc5840fad28
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-10-29 16:47:14 +0900

    refactor: 기존 Votes와의 일관성 개선

--- core/src/main/java/com/estime/room/slot/AvailableDateSlotRepository.java
+++ core/src/main/java/com/estime/room/slot/AvailableDateSlotRepository.java
@@ -3,6 +3,7 @@ package com.estime.room.slot;
 import java.util.List;
 
 public interface AvailableDateSlotRepository {
+
     List<AvailableDateSlot> findByRoomId(Long roomId);
 
     AvailableDateSlot save(AvailableDateSlot availableDateSlot);
