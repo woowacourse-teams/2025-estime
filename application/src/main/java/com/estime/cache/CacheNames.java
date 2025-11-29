de7d2e3ffeedf581b5440d82c34b637d084dc5a0
조재중 <9gle3492@gmail.com>
2025-11-29 20:12:54 +0900

    투표 현황 조회 캐싱 적용 (#789)
    * build: spring cache, caffeine 의존성 추가
    
    * feat: 투표 현황 조회 서비스 메서드 캐싱 적용
    
    - 투표 변경 시 캐시 무효화
    - 일반(v1)/압축(v2) 모두 적용.
    
    * refactor: 캐시 이름 상수 클래스 개선
    
    - 롬복 유틸리티 클래스 활용
    - 마지막 줄 개행 추가

---
+++ application/src/main/java/com/estime/cache/CacheNames.java
@@ -0,0 +1,10 @@
+package com.estime.cache;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class CacheNames {
+
+    public static final String VOTE_STATISTIC = "voteStatistic";
+    public static final String COMPACT_VOTE_STATISTIC = "compactVoteStatistic";
+}
