4849cb4e9f6ce75ef28519368327048a9716a370
조재중 <126754298+m-a-king@users.noreply.github.com>
2026-01-03 23:01:02 +0900

    refactor: 플랫폼별 MessageSender 전략 패턴 적용

--- application/src/main/java/com/estime/port/out/PlatformMessageSender.java
+++ application/src/main/java/com/estime/port/out/PlatformMessageSender.java
@@ -1,9 +1,12 @@
 package com.estime.port.out;
 
 import com.estime.room.RoomSession;
+import com.estime.room.platform.PlatformType;
 import java.time.LocalDateTime;
 
 public interface PlatformMessageSender {
+
+    PlatformType getPlatformType();
 
     void sendDeadlineAlertMessage(
             String channelId,
