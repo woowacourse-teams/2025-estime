e119b6e34698da328e05fc391fa98047ceed273a
jhan0121 <jhan0121@gmail.com>
2026-01-22 19:43:38 +0900

    fix: Vote/CompactVote에 Persistable 적용하여 n + 1 문제 수정

--- core/src/main/java/com/estime/room/participant/vote/Vote.java
+++ core/src/main/java/com/estime/room/participant/vote/Vote.java
@@ -4,6 +4,9 @@ import com.estime.room.slot.DateTimeSlot;
 import com.estime.shared.Validator;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.Transient;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -11,24 +14,39 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.domain.Persistable;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @ToString
-@EqualsAndHashCode
-public class Vote {
+@EqualsAndHashCode(of = "id")
+public class Vote implements Persistable<VoteId> {
 
     @EmbeddedId
     private VoteId id;
+
+    @Transient
+    private boolean isNew = true;
 
     public static Vote of(final Long participantId, final DateTimeSlot dateTimeSlot) {
         Validator.builder()
                 .add("participantId", participantId)
                 .add("dateTimeSlot", dateTimeSlot)
                 .validateNull();
-        return new Vote(VoteId.of(participantId, dateTimeSlot));
+        return new Vote(VoteId.of(participantId, dateTimeSlot), true);
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
+
+    @PostLoad
+    @PostPersist
+    void markNotNew() {
+        this.isNew = false;
     }
 
     public Long participantId() {
