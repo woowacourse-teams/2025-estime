df3c788323b25364daf059111d2a3af680f66d72
조재중 <9gle3492@gmail.com>
2026-02-14 18:34:14 +0900

    API 응답 ETag 캐싱 적용 (ShallowEtagHeaderFilter) (#831)
    * feat: ShallowEtagHeaderFilter 적용으로 API 응답 브라우저 캐싱 지원
    
    - sse 엔드포인트 명시적 제거
    
    * test: ShallowEtagHeaderFilter 적용으로 API 응답 브라우저 캐싱 지원

--- infrastructure/src/main/java/com/estime/config/WebConfig.java
+++ infrastructure/src/main/java/com/estime/config/WebConfig.java
@@ -1,8 +1,12 @@
 package com.estime.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +20,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new ShallowEtagHeaderFilter() {
+            @Override
+            protected boolean shouldNotFilter(final HttpServletRequest request) {
+                return request.getRequestURI().startsWith("/api/v1/sse/");
+            }
+        });
+        filter.addUrlPatterns("/api/*");
+        return filter;
     }
 
     @Override
